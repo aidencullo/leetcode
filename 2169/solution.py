bf0c024898ccd586670db3da5735187d19f9e45c
Aiden Cullo <aidencullo@gmail.com>
2025-06-03 11:40:06 -0400

    2169. Count Operations to Obtain Zero
    k = min(num1, num2)
    
    logk time
    1 space

--- 2169/solution.py
+++ 2169/solution.py
@@ -3,8 +3,10 @@ class Solution:
         if num1 == 0 or num2 == 0:
             return 0
         if num1 >= num2:
-            num1 -= num2
-            return 1 + self.countOperations(num1, num2)
+            multiples = num1 // num2
+            num1 %= num2
+            return multiples + self.countOperations(num1, num2)
         else:
-            num2 -= num1
-            return 1 + self.countOperations(num1, num2)          
+            multiples = num2 // num1
+            num2 %= num1
+            return multiples + self.countOperations(num1, num2)
