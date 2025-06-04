d2a7dcdc80a07aac75e38ce81f33a289dffdc7a1
Aiden Cullo <aidencullo@gmail.com>
2025-06-04 12:58:54 -0400

    1952. Three Divisors
    sqrt(n) time
    1 space

--- 1952/solution.py
+++ 1952/solution.py
@@ -1,11 +1,8 @@
 class Solution:
     def isThree(self, n: int) -> bool:
-        def countDivisors(x):
-            divisors = set((1, x))
-            for i in range(2, int(math.sqrt(x)) + 1):
-                if x % i == 0:
-                    divisors.add(i)
-                    divisors.add(x // i)
-            return len(divisors)
-
-        return countDivisors(n) == 3
+        if n == 1 or n == 2:
+            return False
+        for i in range(2, int(math.sqrt(n))):
+            if n % i == 0:
+                return False
+        return int(math.sqrt(n)) == math.sqrt(n)
