197aca5b767c44183dc2ac48e6cd30672181b52a
Aiden Cullo <aidencullo@gmail.com>
2025-06-04 10:32:08 -0400

    1979. Find Greatest Common Divisor of Array
    time
    
    O(n + log(min(array)))
    
    space
    
    1
    
    n = len of array

--- 1979/solution.py
+++ 1979/solution.py
@@ -1,12 +1,11 @@
 class Solution:
     def findGCD(self, nums: List[int]) -> int:
         def getGCD(a, b):
-            greatest = 1
-            smallest = min(a, b)
-            for i in range(2, smallest + 1):
-                if a % i == 0 and b % i == 0:
-                    greatest = i
-            return greatest
+            while a and b:
+                smaller = min(a, b)
+                larger = max(a, b)
+                a, b = larger % smaller, smaller
+            return max(a, b)
 
         smallest = min(nums)
         largest = max(nums)
