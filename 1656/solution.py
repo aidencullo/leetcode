ce6e44fa22801c88946e4c7c21e24eb76da4e8a8
Aiden Cullo <aidencullo@gmail.com>
2025-06-05 16:24:46 -0400

    1656. Design an Ordered Stream
    init
    
    1 time
    1 space
    
    insert
    
    1 time
    n space

--- 1656/solution.py
+++ 1656/solution.py
@@ -1,13 +1,15 @@
 class OrderedStream:
 
     def __init__(self, n: int):
-        self.values = [None] * n
-        self.start = 0
+        self.values = {}
+        self.ptr = 1
 
     def insert(self, idKey: int, value: str) -> List[str]:
-        index = idKey - 1
-        self.values[index] = value
+        self.values[idKey] = value
+        result = []
 
-        while self.start < len(self.values) and self.values[self.start]:
-            self.start += 1
-        return self.values[index: self.start]
+        while self.ptr in self.values:
+            result.append(self.values[self.ptr])
+            self.ptr += 1
+            
+        return result
