5b2b55e3dc6bc0a67d15faeb17bf7c9ad20af1f1
Aiden Cullo <aidencullo@gmail.com>
2025-06-05 14:32:25 -0400

    703. Kth Largest Element in a Stream
    init
    
    time nlogk
    space k
    
    add
    
    time logk
    space 1
    
    simplify init

--- 703/solution.py
+++ 703/solution.py
@@ -3,16 +3,14 @@ import heapq
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.nums = []
+        self.heap = []
         self.k = k
         for num in nums:
-            heapq.heappush(self.nums, num)
-            if len(self.nums) > self.k:
-                heapq.heappop(self.nums)
+            self.add(num)
                 
 
     def add(self, val: int) -> int:
-        heapq.heappush(self.nums, val)
-        if len(self.nums) > self.k:
-            heapq.heappop(self.nums)
-        return self.nums[0]
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
