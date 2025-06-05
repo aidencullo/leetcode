55d5bc770ee3f4eb8dc19436ff9120e0ba733182
Aiden Cullo <aidencullo@gmail.com>
2025-06-05 16:59:57 -0400

    387. First Unique Character in a String
    n time
    1 space

--- 387/solution.py
+++ 387/solution.py
@@ -1,13 +1,9 @@
+from collections import Counter
+
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        common = set()
-        seen = set()
-        for c in s:
-            if c in seen:
-                common.add(c)
-            seen.add(c)
+        counts = Counter(s)
         for i, c in enumerate(s):
-            if c not in common:
+            if counts[c] == 1:
                 return i
         return -1
-
