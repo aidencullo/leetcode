bb1a8b33d786aa2864e3b3eddea02cbb1f489df4
Aiden Cullo <aidencullo@gmail.com>
2025-06-06 10:45:44 -0400

    96. Unique Binary Search Trees
    n time
    n space

--- 96/solution.py
+++ 96/solution.py
@@ -1,9 +1,15 @@
 class Solution:
+    def __init__(self):
+        self.memo = {}
+        self.memo[0] = 1
+        self.memo[1] = 1
+    
     def numTrees(self, n: int) -> int:
-        if n == 0 or n == 1:
-            return 1
+        if n in self.memo:
+            return self.memo[n]
         total = 0
         for i in range(1, n + 1):
             total += self.numTrees(i - 1) * self.numTrees(n - i)
-        return total
+        self.memo[n] = total
+        return self.memo[n]
         
