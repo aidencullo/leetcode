a22e32cad2ea6b65760639a47a8b0e0bb9577d29
Aiden Cullo <aidencullo@gmail.com>
2025-06-06 16:56:38 -0400

    933. Number of Recent Calls
    1 time
    n space

--- 933/solution.py
+++ 933/solution.py
@@ -1,8 +1,12 @@
+from collections import deque
+
 class RecentCounter:
 
     def __init__(self):
-        self.pings = []
+        self.pings = deque()
 
     def ping(self, t: int) -> int:
         self.pings.append(t)
-        return sum((1 for ping in self.pings if ping >= t - 3000))
+        while self.pings[0] < t - 3000:
+            self.pings.popleft()
+        return len(self.pings)
