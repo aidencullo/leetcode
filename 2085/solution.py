86d08400fe7b2d8a875a9834e65340ef2ad81026
Aiden Cullo <aidencullo@gmail.com>
2025-06-10 16:14:20 -0400

    2085. Count Common Words With One Occurrence
    n + m time/space

---
+++ 2085/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countWords(self, words1: List[str], words2: List[str]) -> int:
+        from collections import Counter
+        counter1 = Counter(words1)
+        unique1 = {k for k, v in counter1.items() if v == 1}
+        counter2 = Counter(words2)
+        unique2 = {k for k, v in counter2.items() if v == 1}
+        return len(unique1 & unique2)
+        
