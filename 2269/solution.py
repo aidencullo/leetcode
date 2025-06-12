c535055684729568ac97e72407283a76e474648c
Aiden Cullo <aidencullo@gmail.com>
2025-06-12 12:27:15 -0400

    2269. Find the K-Beauty of a Number
    klogn time
    k + log n = log n space

---
+++ 2269/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def divisorSubstrings(self, num: int, k: int) -> int:
+        k_beauty = 0
+        num_str = str(num)
+
+        def sliding_substrings(s, size):
+            for i in range(len(s) - size + 1):
+                yield s[i:i+size]
+
+        for x in sliding_substrings(num_str, k):
+            x = int(x)
+            if x == 0:
+                continue
+            if num % x == 0:
+                k_beauty += 1
+
+        return k_beauty
