ac5abe0950d080b88b994a7a0e3f7e7a38115bfe
Aiden Cullo <aidencullo@gmail.com>
2025-06-17 10:54:54 -0400

    1652. Defuse the Bomb
    kn time
    n space

--- 1652/solution.py
+++ 1652/solution.py
@@ -1,15 +1,20 @@
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
+        def circular_sum(start, end):
+            total = 0
+            for i in range(start, end):
+                total += code[i % len(code)]
+            return total
+
         n = len(code)
-        code = 3 * code
         decrypted = []
         if k > 0:
-            for i in range(n, 2 * n):
-                decrypted.append(sum(code[i + 1: i + 1 + k]))
+            for i in range(n):
+                decrypted.append(circular_sum(i + 1, i + 1 + k))
         if k < 0:
-            for i in range(n, 2 * n):
-                decrypted.append(sum(code[i + k: i])) 
+            for i in range(n):
+                decrypted.append(circular_sum(i + k, i))
         if k == 0:
-            for i in range(n, 2 * n):
+            for i in range(n):
                 decrypted.append(0)                   
         return decrypted
