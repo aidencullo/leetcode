caee9a85a8e95b81c63885eeeceeed3151c12451
Aiden Cullo <aidencullo@gmail.com>
2025-06-17 11:07:21 -0400

    1652. Defuse the Bomb
    n time
    n space

--- 1652/solution.py
+++ 1652/solution.py
@@ -9,11 +9,19 @@ class Solution:
         n = len(code)
         decrypted = []
         if k > 0:
+            initial_sum = circular_sum(0, k)
+            running_sum = initial_sum
             for i in range(n):
-                decrypted.append(circular_sum(i + 1, i + 1 + k))
+                running_sum -= code[i % n]
+                running_sum += code[(i + k) % n]
+                decrypted.append(running_sum)
         if k < 0:
+            initial_sum = circular_sum(k - 1, -1)
+            running_sum = initial_sum
             for i in range(n):
-                decrypted.append(circular_sum(i + k, i))
+                running_sum += code[(i - 1) % n]
+                running_sum -= code[(i + k - 1) % n]
+                decrypted.append(running_sum)
         if k == 0:
             for i in range(n):
                 decrypted.append(0)                   
