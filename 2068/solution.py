210ca98c3cab4ebabf623bada88260836ed081ef
Aiden Cullo <aidencullo@gmail.com>
2025-06-18 11:11:27 -0400

    2068. Check Whether Two Strings are Almost Equivalent
    n + m + 26 + 26 = n + m time
    26 + 26 = 1 space

---
+++ 2068/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
+        from collections import Counter
+        
+        count1 = Counter(word1)
+        count2 = Counter(word2)
+
+        for key in count1:
+            if abs(count1[key] - count2[key]) > 3:
+                return False
+        for key in count2:
+            if abs(count1[key] - count2[key]) > 3:
+                return False
+        return True
