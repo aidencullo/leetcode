7a8dd194bd7dd0c1b1ba3be4c5c9d16ae4066596
Aiden Cullo <aidencullo@gmail.com>
2025-06-18 09:15:20 -0400

    594. Longest Harmonious Subsequence
    n time
    n space

--- 594/solution.py
+++ 594/solution.py
@@ -1,15 +1,11 @@
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        nums.sort()
-        l, r = 0, 0
-        n = len(nums)
+        from collections import Counter
+        counter = Counter(nums)
         lhs = 0
-        while r < n:
-            while r < n and nums[r] - nums[l] <= 1:
-                r += 1
-            if nums[r - 1] - nums[l] == 1:
-               lhs = max(lhs, r - l)
-            current = nums[l]
-            while l < n and nums[l] == current:
-                l += 1
+        for key in counter:
+            if key + 1 in counter:
+                lhs = max(lhs, counter[key] + counter[key + 1])
+            if key - 1 in counter:
+                lhs = max(lhs, counter[key] + counter[key - 1])
         return lhs
