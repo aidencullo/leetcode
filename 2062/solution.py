94c3f38aae09e0ec6572153b04e0376580db9358
Aiden Cullo <aidencullo@gmail.com>
2025-06-18 10:32:56 -0400

    2062. Count Vowel Substrings of a String
    passed example test cases
    
    78 / 88 testcases passed

--- 2062/solution.py
+++ 2062/solution.py
@@ -1,12 +1,24 @@
 class Solution:
     def countVowelSubstrings(self, word: str) -> int:
-        def is_vowel_substring(substring):
-            return set(substring) == set('aeiou')
+        def is_vowel_substring(start, end):
+            return set(word[start: end + 1]) == set("aeiou")
 
-        vowel_substrings = 0
+        def is_vowel(index):
+            return word[index] in "aeiou"
+        
+        l, m, r = 0, 0, 0
         n = len(word)
-        for i in range(n):
-            for j in range(i + 1, n + 1):
-                if is_vowel_substring(word[i: j]):
-                    vowel_substrings += 1
+        vowel_substrings = 0
+        while m < n:
+            while m < n and not is_vowel(m):
+                m += 1
+                l = m
+                r = m
+            while m < n and not is_vowel_substring(l, m):
+                m += 1
+            r = m
+            while r < n and is_vowel(r):
+                vowel_substrings += 1
+                r += 1
+            l += 1
         return vowel_substrings
