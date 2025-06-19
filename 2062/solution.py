5f7e1933372ac110516a722114aff8e8648cd885
Aiden Cullo <aidencullo@gmail.com>
2025-06-19 16:52:58 -0400

    2062. Count Vowel Substrings of a String
    n^2 time
    n space

--- 2062/solution.py
+++ 2062/solution.py
@@ -5,20 +5,34 @@ class Solution:
 
         def is_vowel(index):
             return word[index] in "aeiou"
+
+        def count_vowel_substrings(start, end):
+            count = 0
+            l, r = start, start
+            while r < end + 1:
+                while r < end + 1 and not is_vowel_substring(l, r):
+                    r += 1
+                print(r)
+                if r == end + 1:
+                    break
+                count += 1 + end - r
+                l += 1
+            return count
+            
         
-        l, m, r = 0, 0, 0
         n = len(word)
         vowel_substrings = 0
-        while m < n:
-            while m < n and not is_vowel(m):
-                m += 1
-                l = m
-                r = m
-            while m < n and not is_vowel_substring(l, m):
-                m += 1
-            r = m
-            while r < n and is_vowel(r):
-                vowel_substrings += 1
-                r += 1
-            l += 1
-        return vowel_substrings
+        i = 0
+        total = 0
+        
+        while i < n:
+            while i < n and not is_vowel(i):
+                i += 1
+            if i == n:
+                break
+            start = i
+            while i < n and is_vowel(i):
+                i += 1
+            end = i - 1
+            total += count_vowel_substrings(start, end)
+        return total
