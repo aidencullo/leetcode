0308f34af4fc9313d576f52971a4c4e72fab8ab5
aiden cullo <aidencullo@gmail.com>
2025-08-06 14:52:58 -0400

    Add solution for problem 3477 - O(n²) worst case time complexity

--- 3477/solution.py
+++ 3477/solution.py
@@ -2,7 +2,9 @@ class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
         used = set()
         for fruit in fruits:
+            found = False
             for j, basket in enumerate(baskets):
-                if j not in used and fruit <= basket:
+                if j not in used and fruit <= basket and not found:
                     used.add(j)
+                    found = True
         return len(fruits) - len(used)
