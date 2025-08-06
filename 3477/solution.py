b692a4bff60821187f6a5428242774a65d56511f
aiden cullo <aidencullo@gmail.com>
2025-08-06 15:03:28 -0400

    Add solution for problem 3477

--- 3477/solution.py
+++ 3477/solution.py
@@ -2,9 +2,8 @@ class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
         used = set()
         for fruit in fruits:
-            found = False
             for j, basket in enumerate(baskets):
-                if j not in used and fruit <= basket and not found:
+                if j not in used and fruit <= basket:
                     used.add(j)
-                    found = True
+                    break
         return len(fruits) - len(used)
