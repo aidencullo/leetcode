bb44b091dbf11cbe6c8f6a0ce6335472aefb8bc6
aidencullo <aidencullo@gmail.com>
2025-08-21 12:14:19 -0400

    Add n³ time/space solution - alternative approach

--- 3/solution.py
+++ 3/solution.py
@@ -2,29 +2,29 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         from collections import defaultdict
         
-        seen = defaultdict(int)
+        seen = defaultdict(int) # 1
         cleaned = []
-        for num in nums:
+        for num in nums: # n
             if seen[num] < 3:
                 seen[num] += 1
-                cleaned.append(num)
+                cleaned.append(num) # 1
             
         nums = cleaned
-        nums.sort()
+        nums.sort() # nlgn
         
 
         indexes = defaultdict(int)
 
-        for i, v in enumerate(nums):
+        for i, v in enumerate(nums): # n
             indexes[v] += 1
 
         n = len(nums)
         result = []
         indexes_i = dict(indexes)
-        for i in range(n):
+        for i in range(n): # n
             indexes_i[nums[i]] -= 1
             indexes_j = dict(indexes_i)
-            for j in range(i + 1, n):
+            for j in range(i + 1, n): # n
                 indexes_j[nums[j]] -= 1
                 target = nums[i] + nums[j]
                 target *= -1
@@ -33,7 +33,7 @@ class Solution:
                     result.append([nums[i], nums[j], target])
         seen = set()
         result_unique = []
-        for triple in result:
+        for triple in result: # n^3
             if tuple(triple) not in seen:
                 seen.add(tuple(triple))
                 result_unique.append(triple)
