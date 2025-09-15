c051f74d56da9f84963622e348c30d1da38db787
Aiden Cullo <aidencullo@gmail.com>
2025-09-15 16:59:06 -0400

    Update LeetCode 2379 solution - passed sample test cases

--- 2379/solution.py
+++ 2379/solution.py
@@ -1,12 +1,21 @@
-# LeetCode Problem 2379 - Solution Template
-# Add your solution here
-
-def solution():
-    """
-    Add your solution implementation here
-    """
-    pass
-
-if __name__ == "__main__":
-    # Add test cases here
-    pass
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        white = 0
+        min_white = k
+        l, r = 0, k - 1
+        n = len(blocks)
+        for i in range(k):
+            if blocks[i] == 'W':
+                white += 1
+        l += 1
+        r += 1
+        min_white = min(min_white, white)
+        while r < n:      
+            if blocks[r] == 'W':
+                white += 1
+            if blocks[l - 1] == 'W':
+                white -= 1
+            min_white = min(min_white, white)
+            l += 1
+            r += 1     
+        return min_white
