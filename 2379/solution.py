a513b9ef4c4dff52b832ef3d2bc086a7cbddd7c9
Aiden Cullo <aidencullo@gmail.com>
2025-09-15 16:44:08 -0400

    Add LeetCode problem 2379 solution template

---
+++ 2379/solution.py
@@ -0,0 +1,12 @@
+# LeetCode Problem 2379 - Solution Template
+# Add your solution here
+
+def solution():
+    """
+    Add your solution implementation here
+    """
+    pass
+
+if __name__ == "__main__":
+    # Add test cases here
+    pass
