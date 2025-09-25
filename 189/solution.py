87f542918aeae7d0db45b8db256eb12fbc081082
Aiden Cullo <aidencullo@gmail.com>
2025-09-25 16:00:18 -0400

    7 / 39 testcases passed

--- 189/solution.py
+++ 189/solution.py
@@ -2,7 +2,20 @@
 from typing import List
 
 class Solution:
+
     def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if k == 0:
+            return
+        if len(nums) % 2 == 0:
+            self.rotateEven(nums, k)
+        else:
+            self.rotateOdd(nums, k)
+
+
+    def rotateOdd(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
@@ -15,6 +28,23 @@ class Solution:
             nums[next_index] = el
             el = next_el
             pos = next_index
+
+    def rotateEven(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        cycles = k
+        cycle_len = n // k
+        for start in range(k):
+            pos = start
+            el = nums[start]
+            for _ in range(cycle_len):
+                next_index = (pos + k) % n
+                next_el = nums[next_index]
+                nums[next_index] = el
+                el = next_el
+                pos = next_index
             
             
             
