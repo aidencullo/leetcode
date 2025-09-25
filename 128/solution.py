f1864b0c259abb4c21d207a76566e7dea1f40e4f
Aiden Cullo <aidencullo@gmail.com>
2025-09-25 09:42:46 -0400

    128

--- 128/solution.py
+++ 128/solution.py
@@ -4,4 +4,14 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        
+        seen = set(nums)
+        longest = 0
+        for x in seen:
+            if x - 1 not in seen:
+                total = 0
+                while x in seen:
+                    total += 1
+                    x += 1
+                longest = max(longest, total)
+        return longest
+            
