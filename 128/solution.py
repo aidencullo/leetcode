3c7b8e7640f8736737292885ef8e92ea20d686a4
Aiden Cullo <aidencullo@gmail.com>
2025-09-25 09:45:35 -0400

    feat: add comments to 128/solution.py explaining complexity

--- 128/solution.py
+++ 128/solution.py
@@ -4,10 +4,10 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        seen = set(nums)
+        seen = set(nums) # n time n space
         longest = 0
-        for x in seen:
-            if x - 1 not in seen:
+        for x in seen: # n + n = O(n) time
+            if x - 1 not in seen: # 1 time
                 total = 0
                 while x in seen:
                     total += 1
