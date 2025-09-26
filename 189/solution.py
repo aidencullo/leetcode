e07bfe46ae8ca8badf703f163e679ce6d2ca5472
Aiden Cullo <aidencullo@gmail.com>
2025-09-26 11:46:40 -0400

    189 passes test cases but tle

--- 189/solution.py
+++ 189/solution.py
@@ -2,49 +2,19 @@
 from typing import List
 
 class Solution:
-
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if k == 0:
-            return
-        if len(nums) % 2 == 0:
-            self.rotateEven(nums, k)
-        else:
-            self.rotateOdd(nums, k)
+        def swap(left, right):
+            nums[left], nums[right] = nums[right], nums[left]
 
+        def shift_one():
+            el = nums[-1]
+            for i in range(n):
+                el, nums[i] = nums[i], el
 
-    def rotateOdd(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         n = len(nums)
-        pos = 0
-        el = nums[pos]
-        for _ in range(n):
-            next_index = (pos + k) % n
-            next_el = nums[next_index]
-            nums[next_index] = el
-            el = next_el
-            pos = next_index
-
-    def rotateEven(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        n = len(nums)
-        cycles = k
-        cycle_len = n // k
-        for start in range(k):
-            pos = start
-            el = nums[start]
-            for _ in range(cycle_len):
-                next_index = (pos + k) % n
-                next_el = nums[next_index]
-                nums[next_index] = el
-                el = next_el
-                pos = next_index
-            
-            
-            
+        k %= n
+        for _ in range(k):
+            shift_one()
