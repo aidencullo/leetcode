a77e64ef543f5d281e3487e7f0027a26855e91e4
Aiden Cullo <aidencullo@gmail.com>
2025-09-26 17:37:45 -0400

    35 / 39 testcases passed

--- 189/solution.py
+++ 189/solution.py
@@ -7,18 +7,37 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        def swap(i, j):
-            nums[i], nums[j] = nums[j], nums[i]
-
         n = len(nums)
         k %= n
+
         if k == 0:
             return
-        
-        l, r = 0, k
-        for i in range(n - 1):
-            swap(l, r)
-            l += 1
-            r += 1
-            l %= k
-            r %= n
+
+        if n % k == 0:
+            self.rotateModulo(nums, k)
+        else:
+            self.rotateNonModulo(nums, k)            
+
+    def rotateModulo(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        cycle_len = n // k
+        for i in range(k):
+            pos = i
+            el = nums[pos]
+            for j in range(cycle_len):
+                pos += k
+                pos %= n
+                next_el = nums[pos]
+                nums[pos] = el
+                el = next_el
+
+    def rotateNonModulo(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        pos = 0
+        el = nums[pos]
+        for i in range(n):
+            pos += k
+            pos %= n
+            next_el = nums[pos]
+            nums[pos] = el
+            el = next_el
