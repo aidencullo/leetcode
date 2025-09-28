79341c1a7d6c8c6a2aa3c4b49c0f47a2c8096f69
Aiden Cullo <aidencullo@gmail.com>
2025-09-28 14:32:52 -0400

    Add complexity analysis comments to 796 solution

--- 796/solution.py
+++ 796/solution.py
@@ -1,11 +1,11 @@
 class Solution:
-    def rotateString(self, s: str, goal: str) -> bool:
-        rotate_one = lambda s: s[-1] + s[:-1]
+    def rotateString(self, s: str, goal: str) -> bool: # n^2 time n space
+        rotate_one = lambda s: s[-1] + s[:-1] # n time, n space
         
-        n = len(s)
-        current = s
-        for i in s:
-            if current == goal:
+        n = len(s) # 1 time space
+        current = s # 1 time space
+        for i in s: # n iterations 
+            if current == goal: # n time 1 space
                 return True
-            current = rotate_one(current)
+            current = rotate_one(current) # n time space
         return False
