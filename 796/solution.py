88cc1a67c631c59d0c68686dd7519dc1091a4214
Aiden Cullo <aidencullo@gmail.com>
2025-09-28 18:35:13 -0400

    796 n^2 worst case time, n space

--- 796/solution.py
+++ 796/solution.py
@@ -1,11 +1,6 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        rotate_one = lambda s: s[-1] + s[:-1]
-        
-        n = len(s)
-        current = s
-        for i in s: 
-            if current == goal:
-                return True
-            current = rotate_one(current)
-        return False
+        if len(s) != len(goal):
+            return False
+        goal += goal
+        return s in goal
