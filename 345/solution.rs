2d962064c29129043ba9826d68da1e7199137c64
Aiden Cullo <aidencullo@gmail.com>
2025-09-30 19:23:16 -0400

    feat: update solution and gitignore

--- 345/solution.rs
+++ 345/solution.rs
@@ -1,5 +1,15 @@
+struct Solution;
+
 impl Solution {
     pub fn reverse_vowels(s: String) -> String {
-        ""
+        for c in s.chars() {
+            println!("{}", c);
+        }
+        s // return the original string to satisfy type
     }
+}
+
+fn main() {
+    let s = "hello".to_string();
+    Solution::reverse_vowels(s);
 }
