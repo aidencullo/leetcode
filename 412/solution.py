3151cbfd0341641bd1454012c6eff3136032bc8e
Aiden Cullo <aidencullo@gmail.com>
2025-09-30 16:17:17 -0400

    refactor(412): simplify FizzBuzz solution with direct approach

--- 412/solution.py
+++ 412/solution.py
@@ -1,35 +1,16 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
 
-        def buzz(x):
-            try: 
-                if x % 5 == 0:
-                    return "Buzz"
-            except:
-                pass
-            return x
-
-        def fizz(x):
-            try: 
-                if x % 3 == 0:
-                    return "Fizz"
-            except:
-                pass
-            return x
-
-
-        def fizz_buzz(x):
-            try: 
-                if x % 3 == 0 and x % 5 == 0:
-                    return "FizzBuzz"
-            except:
-                pass
-            return x
-
+        res = []
         
-        fizz_buzzed = map(fizz_buzz, range(1, n + 1))
-        fizzed = map(fizz, fizz_buzzed)
-        buzzed = map(buzz, fizzed)
-        result = list(map(str, buzzed))
-        
-        return result
+        for i in range(1, n + 1):
+            if i % 5 == 0 and i % 3 == 0:
+                res.append("FizzBuzz")
+            elif i % 3 == 0:
+                res.append("Fizz")                
+            elif i % 5 == 0:
+                res.append("Buzz")
+            else:
+                res.append(str(i))
+
+        return res
