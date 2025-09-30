7a1ed1330be5ed72eecb7f1ec13ae03771594fdb
Aiden Cullo <aidencullo@gmail.com>
2025-09-30 18:42:24 -0400

    feat: update solution

--- 205/solution.py
+++ 205/solution.py
@@ -2,20 +2,19 @@ from collections import Counter
 
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        def isIsomorphic(s: str, t: str) -> bool:
 
-        if len(s) != len(t):
-            return False
-        
-        seen = {}
-        seen_t = set()
-        for x, y in zip(s, t):
-            if x in seen:
-                if y != seen[x]:
-                    return False
-            else:
-                if y in seen_t:
-                    return False
-                seen[x] = y
-            seen_t.add(y)
-        return True
-                    
+            if len(s) != len(t):
+                return False
+            
+            seen = {}
+            for x, y in zip(s, t):
+                if x in seen:
+                    if y != seen[x]:
+                        return False
+                else:
+                    seen[x] = y
+            return True
+
+        return isIsomorphic(s, t) and isIsomorphic(t, s)
+    
