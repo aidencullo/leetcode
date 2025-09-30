83335b082b5a6257f01186f05a33adfbb53019aa
Aiden Cullo <aidencullo@gmail.com>
2025-09-30 19:02:52 -0400

    feat: update solution

---
+++ 290/solution.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        return self.isIsomorphic(pattern, s.split())
+
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return len(set(s)) == len(set(zip(s, t))) == len(set(t))
