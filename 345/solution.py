f4044bba93135beb200af81af40b84321c24d725
Aiden Cullo <aidencullo@gmail.com>
2025-09-30 20:35:59 -0400

    list comp and generator

--- 345/solution.py
+++ 345/solution.py
@@ -1,21 +1,10 @@
 VOWELS = set('aeiouAEIOU')
 
-
 class Solution:
-    def reverseVowels(self, s: str) -> str:
-
-        def is_vowel(c):
+    def reverse_vowels(self, s: str) -> str:
+        def is_vowel(c: str) -> bool:
             return c in VOWELS
-        
-        vowels = []
-        for c in s:
-            if is_vowel(c):
-                vowels.append(c)
 
-        res = []
-        for c in s:
-            if is_vowel(c):
-                res.append(vowels.pop())
-            else:
-                res.append(c)
-        return ''.join(res)
+        vowels = [c for c in s if is_vowel(c)]
+
+        return ''.join(vowels.pop() if is_vowel(c) else c for c in s)
