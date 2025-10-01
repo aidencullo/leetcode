7c8c8e4e79280b8eae71596f3fc2406cf2238df9
Aiden Cullo <aidencullo@gmail.com>
2025-10-01 16:38:09 -0400

    calculate rotated coords instead of creating a new list

--- 1886/solution.py
+++ 1886/solution.py
@@ -1,16 +1,18 @@
-def rotate(mat):
-    n = len(mat)
-    return [
-        [mat[-(j + 1)][i] for j in range(n)]
-        for i in range(n)
-    ]
-
 class Solution:
-    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
-        current = mat
+    def findRotation(self, mat: list[list[int]], target: list[list[int]]) -> bool:
+        def rotate(row, col, rotations):
+            for _ in range(rotations):
+                row, col = col, -(row + 1)
+            return row, col
 
-        for _ in range(4):
-            current = rotate(current)
-            if current == target:
+        n = len(mat)
+        for rotation in range(4):
+            is_equal = True
+            for row in range(n):
+                for col in range(n):
+                    rotated_row, rotated_col = rotate(row, col, rotation)
+                    if target[row][col] != mat[rotated_row][rotated_col]:
+                        is_equal = False
+            if is_equal:
                 return True
         return False
