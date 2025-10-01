f22a990dcdab8833ea3eccf9a6645a4f5dbd6385
Aiden Cullo <aidencullo@gmail.com>
2025-09-30 20:35:47 -0400

    make vowel lookup 1

--- 345/solution.py
+++ 345/solution.py
@@ -1,13 +1,20 @@
+VOWELS = set('aeiouAEIOU')
+
+
 class Solution:
     def reverseVowels(self, s: str) -> str:
+
+        def is_vowel(c):
+            return c in VOWELS
+        
         vowels = []
         for c in s:
-            if c in 'aeiouAEIOU':
+            if is_vowel(c):
                 vowels.append(c)
 
         res = []
         for c in s:
-            if c in 'aeiouAEIOU':
+            if is_vowel(c):
                 res.append(vowels.pop())
             else:
                 res.append(c)
