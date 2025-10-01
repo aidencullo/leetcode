c1286bf94058adf99f626ca6c837b107cf48d51f
Aiden Cullo <aidencullo@gmail.com>
2025-10-01 12:59:16 -0400

    take a different approach, just translate values

--- 1886/solution.py
+++ 1886/solution.py
@@ -1,37 +1,19 @@
-def create_mat(n):
-    return [[0 for j in range(n)] for i in range(n)]
+import copy
 
+def rotate(mat):
+    new_mat = copy.deepcopy(mat)
+    n = len(mat)
+    for i in range(n):
+        for j in range(n):
+            pass
+    return mat
 
-class Matrix:
-    def __init__(self, lst):
-        self.n = len(lst)
-        self.first_quadrant = create_mat(self.n)
-        self.second_quadrant = create_mat(self.n)
-        self.third_quadrant = create_mat(self.n)
-        self.fourth_quadrant = create_mat(self.n)
-
-    def __eq__(self, other):
-        if not isinstance(other, Matrix):
-            return False
-        return (
-            self.first_quadrant == other.first_quadrant and
-            self.second_quadrant == other.second_quadrant and
-            self.third_quadrant == other.third_quadrant and
-            self.fourth_quadrant == other.fourth_quadrant
-        )
-
-    def rotate(self):
-        return self
-
-    
 class Solution:
     def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
-        start = Matrix(mat)
-        final = Matrix(target)
-        current = start
+        current = mat
 
         for _ in range(4):
-            current = current.rotate()
-            if current == final:
+            current = rotate(current)
+            if current == target:
                 return True
         return False
