bff4809d7e8799372c22858d4f220c6032e2fe85
Aiden Cullo <aidencullo@gmail.com>
2025-10-01 11:58:24 -0400

    taking a new approach -- Matrix class w quadrants (double lists)

--- 1886/solution.py
+++ 1886/solution.py
@@ -1,8 +1,14 @@
+def create_mat(n):
+    return [[0 for j in range(n)] for i in range(n)]
+
 class Solution:
     def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
-        def flipx(x):
-            pass
-        
-        new = flipx(target)
-        print(new)
-        return True
+
+        class Matrix:
+            def __init__(self, n):
+                self.first_quadrant = create_mat(n)
+                self.second_quadrant = create_mat(n)
+                self.third_quadrant = create_mat(n)
+                self.fourth_quadrant = create_mat(n)
+
+        return False
