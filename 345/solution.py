293941b024d316bbaa748ce15d9af8f2a8c62b67
Aiden Cullo <aidencullo@gmail.com>
2025-09-30 20:30:11 -0400

    big o annotations

--- 345/solution.py
+++ 345/solution.py
@@ -1,15 +1,15 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
 
-        vowels = []
-        for c in s:
-            if c in 'aeiouAEIOU':
-                vowels.append(c)
+        vowels = [] # 1 time space
+        for c in s: # n time
+            if c in 'aeiouAEIOU': # 1 time space
+                vowels.append(c) # 1 time, agg n space
 
         res = []
-        for c in s:
-            if c in 'aeiouAEIOU':
-                res.append(vowels.pop())
+        for c in s: # n time
+            if c in 'aeiouAEIOU': # 1 time space
+                res.append(vowels.pop()) # 1 time n space agg
             else:
-                res.append(c)
-        return ''.join(res)
+                res.append(c) # n space agg, 1 time
+        return ''.join(res) # n time space
