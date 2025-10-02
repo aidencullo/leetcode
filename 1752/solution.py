0fc472a0bf0f7e1d7e480ddcbbbb1cf9bb1ddd41
Aiden Cullo <aidencullo@gmail.com>
2025-10-02 15:55:48 -0400

    check for peaks and wrap around from last two first

--- 1752/solution.py
+++ 1752/solution.py
@@ -1,19 +1,11 @@
 class Solution:
     def check(self, nums: list[int]) -> bool:
-        el = nums[0]
-        pivot = 0
-        for i, x in enumerate(nums):
+        count = 0
+        el = nums[-1]
+        for x in nums:
             if x < el:
-                pivot = i
-                break
+                count += 1
+                if count > 1:
+                    return False
             el = x
-
-        new_nums = nums[pivot:] + nums[:pivot]
-
-        el = new_nums[0]
-        for x in new_nums:
-            if x < el:
-                return False
-            el = x
-            
         return True
