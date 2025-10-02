08bb44cd21e8f3fc1795e4f2cda91b7037e13cca
Aiden Cullo <aidencullo@gmail.com>
2025-10-02 17:24:09 -0400

    reverse whole list then subarrays

--- 189/solution.py
+++ 189/solution.py
@@ -3,46 +3,14 @@ from typing import List
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        def reverse(lst, l, r):
+            while l < r:
+                lst[l], lst[r] = lst[r], lst[l]
+                l += 1
+                r -= 1
 
         n = len(nums)
         k %= n
-
-        if k == 0:
-            return
-
-        import math
-        
-        gcd = math.gcd(k, n)
-        if gcd == 1:
-            self.rotateNonModulo(nums, k)            
-        else:
-            self.rotateModulo(nums, k)
-
-    def rotateModulo(self, nums: List[int], k: int) -> None:
-        n = len(nums)
-        import math
-        lcm = math.lcm(k, n)
-        cycle_len = lcm // k
-        for i in range(k):
-            pos = i
-            el = nums[pos]
-            for j in range(cycle_len):
-                pos += k
-                pos %= n
-                next_el = nums[pos]
-                nums[pos] = el
-                el = next_el
-
-    def rotateNonModulo(self, nums: List[int], k: int) -> None:
-        n = len(nums)
-        pos = 0
-        el = nums[pos]
-        for i in range(n):
-            pos += k
-            pos %= n
-            next_el = nums[pos]
-            nums[pos] = el
-            el = next_el
+        reverse(nums, 0, n - 1)
+        reverse(nums, 0, k - 1)
+        reverse(nums, k, n - 1)
