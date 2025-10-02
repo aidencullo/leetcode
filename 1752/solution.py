b8105b75311ec099b4b9d5fcef1dbeb78a7b49d5
Aiden Cullo <aidencullo@gmail.com>
2025-10-02 16:39:42 -0400

    count peaks after

--- 1752/solution.py
+++ 1752/solution.py
@@ -1,11 +1,8 @@
 class Solution:
     def check(self, nums: list[int]) -> bool:
         count = 0
-        el = nums[-1]
-        for x in nums:
-            if x < el:
+        n = len(nums)
+        for i in range(n):
+            if nums[i] > nums[(i + 1) % n]:
                 count += 1
-                if count > 1:
-                    return False
-            el = x
-        return True
+        return count < 2
