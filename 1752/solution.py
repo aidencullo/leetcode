003b2c8347b991eeca2f27f597c5c37c6e83fbea
Aiden Cullo <aidencullo@gmail.com>
2025-10-02 15:32:02 -0400

    build new array from first descending element

--- 1752/solution.py
+++ 1752/solution.py
@@ -1,28 +1,19 @@
-from typing import List
-
 class Solution:
-    def check(self, nums: List[int]) -> bool:
-        new_nums = []
-        seen = set()
-        import math
-        el = math.inf
-        for x in nums:
-            if x != el:
-                new_nums.append(x)
+    def check(self, nums: list[int]) -> bool:
+        el = nums[0]
+        pivot = 0
+        for i, x in enumerate(nums):
+            if x < el:
+                pivot = i
+                break
             el = x
-        new_new_nums = new_nums[-1:] + new_nums[:-1]
 
-        min_val = min(new_new_nums)
-        min_pos = new_new_nums.index(min_val)
+        new_nums = nums[pivot:] + nums[:pivot]
 
-        n = len(new_new_nums)
-        for i in range(min_pos, n + min_pos - 1):
-            i %= n
-            j = (i + 1) % n
-            if new_new_nums[i] > new_new_nums[j]:
+        el = new_nums[0]
+        for x in new_nums:
+            if x < el:
                 return False
+            el = x
+            
         return True
-
-s = Solution()
-result = s.check([1] * 10)
-print(result)
