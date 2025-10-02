6dec06712dc57fe8f7b4042ffb904845182e5eef
Aiden Cullo <aidencullo@gmail.com>
2025-10-01 20:15:13 -0400

    implement custom srted test

--- 1752/solution.py
+++ 1752/solution.py
@@ -1,8 +1,18 @@
+from typing import List
+
 class Solution:
     def check(self, nums: List[int]) -> bool:
+        def is_sorted(arr: List[int]) -> bool:
+            prev = float('-inf')
+            for x in arr:
+                if x < prev:
+                    return False
+                prev = x
+            return True
+        
         n = len(nums)
         for i in range(n):
-            new_nums = nums[i:] + nums[:i]
-            if sorted(new_nums) == new_nums:
+            rotated = nums[i:] + nums[:i]
+            if is_sorted(rotated):
                 return True
         return False
