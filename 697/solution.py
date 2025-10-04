898c880210046fb74924c4d63492440ff602d2a1
Aiden Cullo <aidencullo@gmail.com>
2025-10-04 15:13:16 -0400

    another linear solution keeping track of first_seen

--- 697/solution.py
+++ 697/solution.py
@@ -1,30 +1,22 @@
 from collections import Counter
+from collections import defaultdict
 
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
-        nums_counter = Counter(nums)
-        nums_degree = max(nums_counter.values())
-
+        first_seen = dict()
+        counts = defaultdict(int)
         degree = 0
-        counter = Counter()
-        i = 0
-        j = 0
-        import math
-        shortest_subarray = math.inf
-        n = len(nums)
-        total = 0
-        while i < n:
-            while degree < nums_degree and i < n:
-                counter[nums[i]] += 1
-                degree = max(counter[nums[i]], degree)
-                i += 1
-                total += 1
-            while degree == nums_degree:
-                if degree == counter[nums[j]]:
-                    degree -= 1
-                counter[nums[j]] -= 1
-                j += 1
-                total -= 1
-            shortest_subarray = min(shortest_subarray, total + 1)
-        return shortest_subarray
-        
+        shortest = 0
+
+        for i, x in enumerate(nums):
+            if x not in first_seen:
+                first_seen[x] = i
+            counts[x] += 1
+            if counts[x] > degree:
+                degree = counts[x]
+                shortest = i - first_seen[x] + 1
+            if counts[x] == degree:
+                shortest = min(shortest, i - first_seen[x] + 1)
+        return shortest
+            
+            
