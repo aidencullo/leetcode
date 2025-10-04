3235cba5d62059f31b08e4579c9a06096c961b0d
Aiden Cullo <aidencullo@gmail.com>
2025-10-04 11:08:13 -0400

    add type annotations

--- 748/solution.py
+++ 748/solution.py
@@ -3,13 +3,12 @@ from collections import Counter
 
 class Solution:
     def shortestCompletingWord(self, licensePlate: str, words: list[str]) -> str:
-
-        def remove_non_letters(word):
+        def remove_non_letters(word: str):
             lowercase_word = word.lower()
             only_letters = [c for c in lowercase_word if c.isalpha()]
             return only_letters
 
-        def is_subset(a, b):
+        def is_subset(a: Counter[str], b: Counter[str]) -> bool:
             return not (a - b)
 
         licensePlate_letters = remove_non_letters(licensePlate)
