f6bf570b8d4e5ecfb5dee54f6b5e4a3825a7acfd
Aiden Cullo <aidencullo@gmail.com>
2025-10-04 14:58:50 -0400

    697 linear solution

--- 697/solution.py
+++ 697/solution.py
@@ -12,15 +12,19 @@ class Solution:
         import math
         shortest_subarray = math.inf
         n = len(nums)
+        total = 0
         while i < n:
             while degree < nums_degree and i < n:
                 counter[nums[i]] += 1
-                degree = max(counter.values())
+                degree = max(counter[nums[i]], degree)
                 i += 1
+                total += 1
             while degree == nums_degree:
+                if degree == counter[nums[j]]:
+                    degree -= 1
                 counter[nums[j]] -= 1
-                degree = max(counter.values())
                 j += 1
-            shortest_subarray = min(shortest_subarray, counter.total() + 1)
+                total -= 1
+            shortest_subarray = min(shortest_subarray, total + 1)
         return shortest_subarray
         
