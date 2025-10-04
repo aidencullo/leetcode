1102dda762f3ffb02336c5b5b187715b24dd719c
Aiden Cullo <aidencullo@gmail.com>
2025-10-04 11:35:18 -0400

    inline subset and filtering out non-alphabetic characters
    we don't have to remake the str at all, just pass back the list of chars

--- 748/solution.py
+++ 748/solution.py
@@ -1,17 +1,9 @@
 from collections import Counter
 
-
 class Solution:
     def shortestCompletingWord(self, licensePlate: str, words: list[str]) -> str:
-        def remove_non_letters(word: str):
-            lowercase = word.lower()
-            letters = list(filter(str.isalpha, lowercase))
-            return letters
-
-        def is_subset(a: Counter[str], b: Counter[str]) -> bool:
-            return a <= b
-
-        licensePlate_letters = remove_non_letters(licensePlate)
-        licensePlate_counter = Counter(licensePlate_letters)
-        res = [word for word in words if is_subset(licensePlate_counter, Counter(word))]
-        return min(res, key=len)
+        license_counter = Counter(c.lower() for c in licensePlate if c.isalpha())
+        return min(
+            (word for word in words if license_counter <= Counter(word.lower())),
+            key=len
+        )
