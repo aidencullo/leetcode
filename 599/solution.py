8a144292316c98d1e0e671dd415eb67f0c0591ed
Aiden Cullo <aidencullo@gmail.com>
2025-10-04 15:45:03 -0400

    clean it up a tiny bit so that we only use min(n, m) space

--- 599/solution.py
+++ 599/solution.py
@@ -1,20 +1,27 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        if len(list1) < len(list2):
+            shorter_list = list1
+            longer_list = list2
+        else:
+            shorter_list = list2
+            longer_list = list1
+            
         first_seen = dict()
         minimum_restaurants = []
         import math
         least_index_sum = math.inf
-        for i, item in enumerate(list1):
-            if item not in first_seen:
-                first_seen[item] = i
-        for i, item in enumerate(list2):
-            if item in first_seen:
-                index_sum = i + first_seen[item]
+        for i, restaurant in enumerate(shorter_list):
+            if restaurant not in first_seen:
+                first_seen[restaurant] = i
+        for i, restaurant in enumerate(longer_list):
+            if restaurant in first_seen:
+                index_sum = i + first_seen[restaurant]
                 if index_sum < least_index_sum:
                     least_index_sum = index_sum
-                    minimum_restaurants = [item]
+                    minimum_restaurants = [restaurant]
                 elif index_sum == least_index_sum:
-                    minimum_restaurants.append(item)
+                    minimum_restaurants.append(restaurant)
         return minimum_restaurants
 
             
