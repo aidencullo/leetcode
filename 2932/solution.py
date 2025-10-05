8f3111d4a09cef3b680293bc5e7111bd84428d1d
Aiden Cullo <aidencullo@gmail.com>
2025-10-05 19:53:42 -0400

    simplify

--- 2932/solution.py
+++ 2932/solution.py
@@ -1,12 +1,3 @@
 class Solution:
     def maximumStrongPairXor(self, nums: List[int]) -> int:
-        def is_strong(x, y):
-            return abs(x - y) <= min(x, y)
-        
-        n = len(nums)
-        xor = 0
-        for x in nums:
-            for y in nums:
-                if is_strong(x, y):
-                    xor = max(xor, x ^ y)
-        return xor
+        return max(x^y for x in nums for y in nums if abs(x - y) <= min(x, y))
