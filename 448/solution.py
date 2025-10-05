86a7a239c4f20f9c03a5af2d42b0d8801f945e3f
Aiden Cullo <aidencullo@gmail.com>
2025-10-04 20:48:36 -0400

    ingenious solution from claude

--- 448/solution.py
+++ 448/solution.py
@@ -1,6 +1,7 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        all_nums = set(range(1, len(nums) + 1))
-        given_nums = set(nums)
-        diff = all_nums - given_nums
-        return list(diff)
+        for i, x in enumerate(nums):
+            ind = abs(x) - 1
+            nums[ind] = -abs(nums[ind])
+
+        return [i + 1 for i, num in enumerate(nums) if num > 0]
