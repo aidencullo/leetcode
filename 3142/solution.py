a54cc6d014bee170c5ae559bcf2396054197e11a
Aiden Cullo <aidencullo@gmail.com>
2025-10-05 11:11:26 -0400

    Update solution.py

--- 3142/solution.py
+++ 3142/solution.py
@@ -1,19 +1,8 @@
 class Solution:
     def satisfiesConditions(self, grid: List[List[int]]) -> bool:
-        def is_equal_below(i, j):
-            if i == rows - 1:
-                return True
-            return grid[i][j] == grid[i + 1][j]
-
-        def is_different_right(i, j):
-            if j == cols - 1:
-                return True
-            return grid[i][j] != grid[i][j + 1]
-
-        rows = len(grid)
-        cols = len(grid[0])
+        rows, cols = len(grid), len(grid[0])
         return all(
-            is_equal_below(i, j) and is_different_right(i, j)
-            for i in range(rows)
-            for j in range(cols)
+            (i == rows - 1 or grid[i][j] == grid[i + 1][j]) and
+            (j == cols - 1 or grid[i][j] != grid[i][j + 1])
+            for i in range(rows) for j in range(cols)
         )
