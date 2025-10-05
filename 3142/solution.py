4362d1a3d9029e1a7f6a122bf5b2b4a137f6fdcd
Aiden Cullo <aidencullo@gmail.com>
2025-10-05 12:01:04 -0400

    check if rows are the same by turning them into tuples and then sets

--- 3142/solution.py
+++ 3142/solution.py
@@ -1,8 +1,13 @@
 class Solution:
     def satisfiesConditions(self, grid: List[List[int]]) -> bool:
         rows, cols = len(grid), len(grid[0])
-        return all(
-            (i == rows - 1 or grid[i][j] == grid[i + 1][j]) and
-            (j == cols - 1 or grid[i][j] != grid[i][j + 1])
-            for i in range(rows) for j in range(cols)
-        )
+        for i in range(rows):
+            for j in range(cols - 1):
+                if grid[i][j] == grid[i][j + 1]:
+                    return False
+        # grid_transpose = [[grid[col][row] for col in range(n)] for row in range(n)]
+        list_of_tuples = list(map(tuple, grid))
+        print(list_of_tuples)
+        if len(set(list_of_tuples)) != 1:
+            return False
+        return True
