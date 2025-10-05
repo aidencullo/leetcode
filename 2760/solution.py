61b8ca1fcbba45a545c53bad44c32b6425307152
Aiden Cullo <aidencullo@gmail.com>
2025-10-05 16:48:49 -0400

    getting tle still, but I think I'm close

--- 2760/solution.py
+++ 2760/solution.py
@@ -1,13 +1,21 @@
 class Solution:
     def longestAlternatingSubarray(self, nums: List[int], threshold: int) -> int:
         import math
-        longest = -math.inf
+        longest = 0
         l, r = 0, 0
         n = len(nums)
         while r < n:
-            if nums[r] > threshold:
-                l, r = r + 1, r + 1
-            else:
+            while l < n and nums[l] % 2 and nums[l] < threshold:
+                l += 1
+            r = l
+            while r < n and nums[r] <= threshold:
+                if l != r:
+                    if nums[r] % 2 == nums[r - 1] % 2:
+                        l += 1
+                        break
                 r += 1
                 longest = max(longest, r - l)
         return longest
+        
+            
+                
