55618e197a7ffb04bbb0351f0341c96c6a56ead1
Aiden Cullo <aidencullo@gmail.com>
2025-10-06 11:50:03 -0400

    use for loop instead -- this is actually quadratic time and 26 constant space

--- 3090/solution.py
+++ 3090/solution.py
@@ -2,18 +2,14 @@ class Solution:
     def maximumLengthSubstring(self, s: str) -> int:
         from collections import Counter
         counter = Counter()
+
         n = len(s)
+        left = 0
         longest = 0
-        l, r = 0, -1
-        while r < n:
-            while r < n and (not counter or max(counter.values()) < 3):
-                longest = max(longest, r - l + 1)                
-                r += 1
-                if r == n:
-                    return longest
-                counter[s[r]] += 1
+        for i in range(n):
+            counter[s[i]] += 1
             while max(counter.values()) == 3:
-                counter[s[l]] -= 1
-                l += 1
+                counter[s[left]] -= 1
+                left += 1
+            longest = max(longest, i - left + 1)
         return longest
-            
