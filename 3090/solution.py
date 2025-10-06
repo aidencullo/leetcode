cd5d7cd68e316ca83fe4b3ccf3e2751818edd969
Aiden Cullo <aidencullo@gmail.com>
2025-10-06 11:23:53 -0400

    3090 n time 1 space solution using sliding window method

--- 3090/solution.py
+++ 3090/solution.py
@@ -1,17 +1,17 @@
 class Solution:
     def maximumLengthSubstring(self, s: str) -> int:
-        l, r = 0, 0
         from collections import Counter
         counter = Counter()
         n = len(s)
         longest = 0
+        l, r = 0, -1
         while r < n:
-            while r < n and (not counter or max(counter.values()) <= 2):
-                counter[s[r]] += 1
+            while r < n and (not counter or max(counter.values()) < 3):
+                longest = max(longest, r - l + 1)                
                 r += 1
-            longest = max(longest, r - l)
-            if r == n:
-                return longest
+                if r == n:
+                    return longest
+                counter[s[r]] += 1
             while max(counter.values()) == 3:
                 counter[s[l]] -= 1
                 l += 1
