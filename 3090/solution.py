0c324159db549341907b0206964e3c82b0641218
Aiden Cullo <aidencullo@gmail.com>
2025-10-06 11:54:34 -0400

    3090 only need to check the last el most recently incremented
    linear time
    linear or constant space -- depends if you want to consider the constraint of the input to only lowercase english letters (so then we will not use more than 26x space)

--- 3090/solution.py
+++ 3090/solution.py
@@ -5,10 +5,10 @@ class Solution:
 
         n = len(s)
         left = 0
-        longest = 0
+        longest = 2
         for i in range(n):
             counter[s[i]] += 1
-            while max(counter.values()) == 3:
+            while counter[s[i]] == 3:
                 counter[s[left]] -= 1
                 left += 1
             longest = max(longest, i - left + 1)
