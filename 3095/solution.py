8b60a3b636ff1bda72b5171c644be0a9273ae86b
Aiden Cullo <aidencullo@gmail.com>
2025-10-08 18:00:01 -0400

    3095 working on linear solution with bit frequency counting

--- 3095/solution.py
+++ 3095/solution.py
@@ -2,12 +2,35 @@ import math
 
 class Solution:
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
-        n = len(nums)
+
+        def subtract_bits(x):
+            for i in range(32):
+                shield  = 1 << i
+                if shield & x:
+                    bit_count[i] -= 1
+
+        def add_bits(x):
+            for i in range(32):
+                shield  = 1 << i
+                if shield & x:
+                    bit_count[i] += 1
+
+        def bits_to_dec():
+            for i in range(32):
+                bit = bit_count[i]
+                bit
+
+        bit_count = [0] * 32
         shortest = math.inf
-        for start in range(n):
-            bitwise_or = 0
-            for end in range(start, n):
-                bitwise_or |= nums[end]
-                if bitwise_or >= k:
-                    shortest = min(shortest, end - start + 1)
+        bitwise_or = 0
+        for i in range(n):
+            x = nums[i]
+            add_bits(x)
+            bitwise_or = bits_to_dec()
+            while bitwise_or >= k:
+                shortest = min(shortest, i - l + 1)
+                subtract_bits(nums[l])
+                bitwise_or = bits_to_dec()
+                l += 1
         return shortest if shortest != math.inf else -1
+            
