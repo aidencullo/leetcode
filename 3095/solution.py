714058babca5f96ad001f164caa6f7f4df8822f9
Aiden Cullo <aidencullo@gmail.com>
2025-10-08 15:00:00 -0400

    3095 accepted, quadratic time

--- 3095/solution.py
+++ 3095/solution.py
@@ -1,3 +1,13 @@
+import math
+
 class Solution:
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
-        pass
+        n = len(nums)
+        shortest = math.inf
+        for start in range(n):
+            bitwise_or = 0
+            for end in range(start, n):
+                bitwise_or |= nums[end]
+                if bitwise_or >= k:
+                    shortest = min(shortest, end - start + 1)
+        return shortest if shortest != math.inf else -1
