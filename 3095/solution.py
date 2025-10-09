d7c8628cb3a5e0c765b467131aa16a83a1682849
Aiden Cullo <aidencullo@gmail.com>
2025-10-09 13:17:43 -0400

    3095 linear time constant space

--- 3095/solution.py
+++ 3095/solution.py
@@ -16,21 +16,25 @@ class Solution:
                     bit_count[i] += 1
 
         def bits_to_dec():
+            dec = 0
             for i in range(32):
-                bit = bit_count[i]
-                bit
+                bit = (1 << i)
+                dec += bit * (1 if bit_count[i] else 0)
+            return dec
 
         bit_count = [0] * 32
         shortest = math.inf
         bitwise_or = 0
-        for i in range(n):
-            x = nums[i]
+        l = 0
+        n = len(nums)
+        for i, x in enumerate(nums):
             add_bits(x)
             bitwise_or = bits_to_dec()
-            while bitwise_or >= k:
+            while bitwise_or >= k and l <= i:
                 shortest = min(shortest, i - l + 1)
                 subtract_bits(nums[l])
                 bitwise_or = bits_to_dec()
                 l += 1
-        return shortest if shortest != math.inf else -1
-            
+        return shortest if shortest != 0 and shortest != math.inf else -1
+
+
