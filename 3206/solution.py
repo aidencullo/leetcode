2d70ad2f2b96cf92f1df54e5b4fc7fac60bcc478
Aiden Cullo <aidencullo@gmail.com>
2025-10-09 13:34:35 -0400

    3206 n time 1 space

---
+++ 3206/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int]) -> int:
+        def get_element(i):
+            return colors[i % n]
+        
+        alternating_groups = 0
+        n = len(colors)
+        for i in range(n):
+            left = get_element(i)
+            middle = get_element(i + 1)
+            right = get_element(i + 2)
+            if left != middle and middle != right:
+                alternating_groups += 1
+        return alternating_groups
+            
