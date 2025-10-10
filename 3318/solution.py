5c3b15e8f78bc9080a36ef2485668403ac0e5564
Aiden Cullo <aidencullo@gmail.com>
2025-10-10 10:30:07 -0400

    3318 return placeholder array for now

--- 3318/solution.py
+++ 3318/solution.py
@@ -1,3 +1,9 @@
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        pass
+        
+        def findXSum(nums: List[int], k: int, x: int) -> List[int]:
+            from collections import Counter
+            c = Counter(nums)
+
+        n = len(nums)
+        return (n - k + 1) * [0]
