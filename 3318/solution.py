da6a9aee0dd403e29770815ada7a3fcc25bc7eea
Aiden Cullo <aidencullo@gmail.com>
2025-10-10 11:28:24 -0400

    3318 n * klgk time n space -- i think

--- 3318/solution.py
+++ 3318/solution.py
@@ -1,9 +1,11 @@
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
         
-        def findXSum(nums: List[int], k: int, x: int) -> List[int]:
+        def findXSum(nums: List[int], x: int) -> List[int]:
             from collections import Counter
+            nums.sort(reverse=True)
             c = Counter(nums)
-
+            return sum(k * v for k, v in c.most_common(x))
+            
         n = len(nums)
-        return (n - k + 1) * [0]
+        return [findXSum(nums[i: i + k - 1 + 1], x) for i in range(n - k + 1)]
