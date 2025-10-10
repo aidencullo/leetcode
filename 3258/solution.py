feeb886f058b9ec36ca08dcb033e546792a9c156
Aiden Cullo <aidencullo@gmail.com>
2025-10-10 08:58:05 -0400

    extract incrementing counter to helper

--- 3258/solution.py
+++ 3258/solution.py
@@ -4,22 +4,30 @@ class Solution:
         def is_k_constrained(ones, zeroes, threshold):
             return ones <= threshold or zeroes <= threshold
 
+        def increment(ones, zeroes, c):
+            if c == "0":
+                zeroes += 1
+            else:
+                ones += 1
+            return ones, zeroes
+
+        def decrement(ones, zeroes, c):
+            if c == "0":
+                zeroes -= 1
+            else:
+                ones -= 1
+            return ones, zeroes
+
         n = len(s)
         total_substrings = (n * (n + 1)) // 2
         not_k_constraint_substrings = 0
         zeroes = 0
         ones = 0
-        l = 0
-        for i, c in enumerate(s):
-            if c == "0":
-                zeroes += 1
-            else:
-                ones += 1
+        left = 0
+        for right, c in enumerate(s):
+            ones, zeroes = increment(ones, zeroes, c)
             while not is_k_constrained(ones, zeroes, k):
-                not_k_constraint_substrings += n - i
-                if s[l] == "0":
-                    zeroes -= 1
-                else:
-                    ones -= 1
-                l += 1
+                not_k_constraint_substrings += n - right
+                ones, zeroes = decrement(ones, zeroes, s[left])
+                left += 1
         return total_substrings - not_k_constraint_substrings
