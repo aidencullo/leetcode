21ea979afd79f2523fa40e12935aba75618a7f4e
Aiden Cullo <aidencullo@gmail.com>
2025-10-10 08:46:31 -0400

    3258 n time 1 space sliding window

--- 3258/solution.py
+++ 3258/solution.py
@@ -1,15 +1,25 @@
 class Solution:
     def countKConstraintSubstrings(self, s: str, k: int) -> int:
+
+        def is_k_constrained(ones, zeroes, threshold):
+            return ones <= threshold or zeroes <= threshold
+        
         n = len(s)
-        k_constraint_substrings = 0
-        for i in range(n):
-            ones = 0
-            zeroes = 0
-            for j in range(i, n):
-                if s[j] == '0':
-                    zeroes += 1
+        total_substrings = (n  * (n + 1)) // 2
+        not_k_constraint_substrings = 0
+        zeroes = 0
+        ones = 0
+        l = 0
+        for i, c in enumerate(s):
+            if c == '0':
+                zeroes += 1
+            else:
+                ones += 1
+            while not is_k_constrained(ones, zeroes, k):
+                not_k_constraint_substrings += n - i
+                if s[l] == '0':
+                    zeroes -= 1
                 else:
-                    ones += 1
-                if ones <= k or zeroes <= k:
-                    k_constraint_substrings += 1
-        return k_constraint_substrings
+                    ones -= 1
+                l += 1
+        return total_substrings - not_k_constraint_substrings
