6f4a5a824725192a4d28d08fcbdbf74a79ba5001
Aiden Cullo <aidencullo@gmail.com>
2025-10-11 11:00:37 -0400

    3364, n^2 time n space

--- 3364/solution.py
+++ 3364/solution.py
@@ -4,8 +4,21 @@ class Solution:
         import math
         minimum_sum = math.inf
         for subarray_length in range(l, r + 1):
-            for i in range(n - (subarray_length - 1)):
-                current_sum = sum(nums[i: i + subarray_length])
+            if subarray_length > n:
+                break
+
+            current_sum = sum(nums[:subarray_length])
+            if current_sum > 0:
+                minimum_sum = min(minimum_sum, current_sum)
+
+            i = 1
+            while i + subarray_length - 1 < n:
+                last_i = i - 1
+                next_i = (i + subarray_length - 1)
+                current_sum -= nums[last_i]
+                current_sum += nums[next_i]
                 if current_sum > 0:
                     minimum_sum = min(minimum_sum, current_sum)
+                i += 1
+            
         return minimum_sum if minimum_sum != math.inf else -1
