6beb78adc7a3303100db19b81cfab98ee2bc0f11
Aiden Cullo <aidencullo@gmail.com>
2025-10-11 16:01:36 -0400

    refactor(leetcode): Refactor solution for problem 141
    Time: O(n), Space: O(1)

--- 141/solution.py
+++ 141/solution.py
@@ -6,12 +6,11 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        pointer = head
-        skip_pointer = head
-        while pointer and skip_pointer:
-            pointer = pointer.next if pointer else None
-            skip_pointer = skip_pointer.next if skip_pointer else None
-            skip_pointer = skip_pointer.next if skip_pointer else None
-            if pointer and skip_pointer and pointer == skip_pointer:
+        slow = head
+        fast = head
+        while fast and fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
                 return True
         return False
