cc889785ada94bf58778d08f1e14d94cef59d696
Aiden Cullo <aidencullo@gmail.com>
2025-10-11 10:19:03 -0400

    3364, n^2 time 1 space

--- 3364/solution.py
+++ 3364/solution.py
@@ -4,10 +4,10 @@ class Solution:
         import math
         minimum_sum = math.inf
         for i in range(n):
+            current_sum = 0
             for j in range(i, n):
-                subarray = nums[i: j + 1]
-                m = len(subarray)
-                m_sum = sum(subarray)
-                if m >= l and m <= r and m_sum > 0:
-                    minimum_sum = min(minimum_sum, m_sum)
+                current_sum += nums[j]
+                width = j - i + 1
+                if width >= l and width <= r and current_sum > 0:
+                    minimum_sum = min(minimum_sum, current_sum)
         return minimum_sum if minimum_sum != math.inf else -1
