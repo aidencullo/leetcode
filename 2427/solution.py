5c378e31c8e1089486427695508a9f75596ff973
Aiden Cullo <aidencullo@gmail.com>
2025-10-11 17:08:10 -0400

    sqrt(n) time and space

--- 2427/solution.py
+++ 2427/solution.py
@@ -1,7 +1,13 @@
+import math
+
 class Solution:
     def commonFactors(self, a: int, b: int) -> int:
-        factors = 0
-        for x in range(1, a + 1):
-            if a % x == 0 and b % x == 0:
-                factors += 1
-        return factors
+        factors = set()
+        for x in range(1, math.ceil(math.sqrt(a)) + 1):
+            if a % x == 0:
+                y = a // x
+                if b % x == 0:
+                    factors.add(x)
+                if b % y == 0:
+                    factors.add(y)
+        return len(factors)
