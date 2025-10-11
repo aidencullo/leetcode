528b75617a25c39056a74b286f1a71f281f5302f
Aiden Cullo <aidencullo@gmail.com>
2025-10-11 10:32:55 -0400

    3364, n^3 time n space
    slight simplification

--- 3364/solution.py
+++ 3364/solution.py
@@ -4,9 +4,8 @@ class Solution:
         import math
         minimum_sum = math.inf
         for subarray_length in range(l, r + 1):
-            for i in range(n):
-                if i + subarray_length <= n:
-                    current_sum = sum(nums[i: i + subarray_length])
-                    if current_sum > 0:
-                        minimum_sum = min(minimum_sum, current_sum)
+            for i in range(n - (subarray_length - 1)):
+                current_sum = sum(nums[i: i + subarray_length])
+                if current_sum > 0:
+                    minimum_sum = min(minimum_sum, current_sum)
         return minimum_sum if minimum_sum != math.inf else -1
