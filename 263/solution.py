4ab352e4d80486dfb14804d07ce16ebcd7274ace
Aiden Cullo <aidencullo@gmail.com>
2025-10-11 16:43:41 -0400

    lgn time 1 space

--- 263/solution.py
+++ 263/solution.py
@@ -1,12 +1,12 @@
-import math
-
-
 class Solution:
     def isUgly(self, n: int) -> bool:
-        prime_factors = [2, 3, 5]
         if n <= 0:
             return False
-        for factor in prime_factors:
-            while n % factor == 0:
-                n //= factor
-        return n == 1
+        x = n
+        while x % 2 == 0:
+            x //= 2
+        while x % 3 == 0:
+            x //= 3
+        while x % 5 == 0:
+            x //= 5
+        return x == 1
