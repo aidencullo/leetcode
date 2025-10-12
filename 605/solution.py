782af9442a743abd5b11f279fe86d451f1902de1
Aiden Cullo <aidencullo@gmail.com>
2025-10-12 10:00:46 -0400

    Update LeetCode 605 solution with improved algorithm using sum_beds helper function

--- 605/solution.py
+++ 605/solution.py
@@ -18,20 +18,26 @@ Approach:
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        def sum_beds(start, end):
+            beds = 0
+            if start < 0:
+                start += 1
+            if end == len(flowerbed):
+                end -= 1
+
+            for i in range(start, end + 1):
+                beds += flowerbed[i]
+
+            return beds
+        
         spaces = 0
-        flowerbed  = [0] + flowerbed + [0]
-        beds= len(flowerbed)
+        flowerbed  = flowerbed
+        num_flowerbeds = len(flowerbed)
         i = 0
-        while i < beds- 2:
-            if flowerbed[i] != 0:
+        while i < num_flowerbeds:
+            if sum_beds(i - 1, i + 1) != 0:
                 i += 1
-                continue
-            if flowerbed[i + 1] != 0:
-                i += 1
-                continue
-            if flowerbed[i + 2] != 0:
-                i += 1
-                continue
-            spaces += 1
-            i += 2
+            else:
+                spaces += 1
+                i += 2
         return spaces >= n
