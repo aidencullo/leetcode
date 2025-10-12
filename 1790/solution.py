ec096e21d6db7b5fa6433b401f9f96f8c89c81a0
Aiden Cullo <aidencullo@gmail.com>
2025-10-12 12:47:48 -0400

    short and sweet n timespace

--- 1790/solution.py
+++ 1790/solution.py
@@ -1,16 +1,18 @@
+from collections import Counter
+
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        from collections import Counter
         if len(s1) != len(s2):
             return False
-        diffs = []
+        if Counter(s1) != Counter(s2):
+            return False
+
+        count = 0
         for i in range(len(s1)):
             if s1[i] != s2[i]:
-                diffs.append(i)
-        count = len(diffs)
-        if count == 0:
-            return True
-        if count == 2:
-            return s1[diffs[0]] == s2[diffs[1]] and s1[diffs[1]] == s2[diffs[0]]
-        return False
+                count += 1
+
+        if count > 2:
+            return False
+        return True
         
