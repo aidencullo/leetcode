ed8d8d70cb6b50885039f8f1315e32d779b45925
Aiden Cullo <aidencullo@gmail.com>
2025-10-13 12:20:34 -0400

    Update 3264 solution

--- 3264/solution.py
+++ 3264/solution.py
@@ -1,6 +1,5 @@
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
-        for i in range(k):
-            min_i = nums.index(min(nums))
-            nums[min_i] *= multiplier
-        return nums
+        value_keys = [(v, i) for i, v in enumerate(nums)]
+        nsmall = heapq.nsmallest(value_keys)
+        return nsmall
