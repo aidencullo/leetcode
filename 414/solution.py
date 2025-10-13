dfe437cfee8daafae794b0d306aeef84d3ea3c89
Aiden Cullo <aidencullo@gmail.com>
2025-10-13 16:00:11 -0400

    nlgn time n space

--- 414/solution.py
+++ 414/solution.py
@@ -1,3 +1,7 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        pass
+        nums = list(set(nums))
+        nums.sort()
+        if len(nums) < 3:
+            return nums[-1]
+        return nums[-3]
