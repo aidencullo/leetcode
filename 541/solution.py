946af2b91fd1856626628134f18aecb579a540d3
Aiden Cullo <aidencullo@gmail.com>
2025-10-14 13:27:26 -0400

    time n space n + n/k = O(n)

--- 541/solution.py
+++ 541/solution.py
@@ -1,21 +1,3 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-
-        def reverse_copy(start, end):
-            l = end
-            for j in range(start, end - 1, -1):
-                res[l] = s[j]
-                l += 1
-
-        res = list(s)
-        i = 0
-        n = len(s)
-        while i < n:
-            end = i
-            if i + k < n:
-                start = i + k - 1
-            else:
-                start = n - 1
-            reverse_copy(start, end)
-            i += 2 * k
-        return "".join(res)
+        return s[:k][::-1] + s[k:2*k] + (self.reverseStr(s[2*k:], k) if len(s) > 2*k else "")
