ef19bd53c6e84d2613fa45105e68a482dc920fbd
Aiden Cullo <aidencullo@gmail.com>
2025-10-14 10:51:31 -0400

    n timespace

--- 414/solution.py
+++ 414/solution.py
@@ -1,7 +1,15 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         nums = list(set(nums))
-        nums.sort()
         if len(nums) < 3:
-            return nums[-1]
-        return nums[-3]
+            return max(nums)
+        first_max = max(nums)
+        second_max = -math.inf
+        third_max = -math.inf
+        for x in nums:
+            if x != first_max:
+                second_max = max(second_max, x)
+        for x in nums:
+            if x != first_max and x != second_max:
+                third_max = max(third_max, x)
+        return third_max
