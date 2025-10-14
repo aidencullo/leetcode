8cf040a933c9930e13648f96eee367c1800e5041
Aiden Cullo <aidencullo@gmail.com>
2025-10-14 13:32:26 -0400

    actually n^2 time i think n space

--- 541/solution.py
+++ 541/solution.py
@@ -1,3 +1,15 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        return s[:k][::-1] + s[k:2*k] + (self.reverseStr(s[2*k:], k) if len(s) > 2*k else "")
+        res = ""
+        i = 0
+        n = len(s)
+        while i < n:
+            end = i
+            if i + k < n:
+                start = i + k - 1
+            else:
+                start = n - 1
+            res += s[end: start + 1][::-1]
+            res += s[end + k: start + 1 + k]
+            i += 2 * k
+        return res
