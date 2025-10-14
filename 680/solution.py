6775c21629d5d2c36ffbd666d8a24e3fd28e23b2
Aiden Cullo <aidencullo@gmail.com>
2025-10-14 17:04:17 -0400

    n timespace?

--- 680/solution.py
+++ 680/solution.py
@@ -1,34 +1,16 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
 
-        def isPalindromeLeft(s):
-            different = False
-            l = 0
-            r = len(s) - 1
-            while l < r:
-                if s[l] != s[r] and different:
+        def isPalindrome(s):
+            for i in range(len(s) // 2):
+                if s[i] != s[len(s) - 1 - i]:
                     return False
-                if s[l] != s[r]:
-                    different = True
-                    l += 1
-                    continue
-                l += 1
-                r -= 1
             return True
 
-        def isPalindromeRight(s):
-            different = False
-            l = 0
-            r = len(s) - 1
-            while l < r:
-                if s[l] != s[r] and different:
-                    return False
-                if s[l] != s[r]:
-                    different = True
-                    r -= 1
-                    continue
-                l += 1
-                r -= 1
-            return True
-                
-        return isPalindromeLeft(s) or isPalindromeRight(s)
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                return isPalindrome(s[l: r]) or isPalindrome(s[l + 1: r + 1])
+            l += 1
+            r -= 1
+        return True
