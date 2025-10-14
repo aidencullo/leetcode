f3910fff4a3d0d771da21bfa6de1ff04e316495c
Aiden Cullo <aidencullo@gmail.com>
2025-10-14 11:49:10 -0400

    n timespace

--- 414/solution.py
+++ 414/solution.py
@@ -1,16 +1,5 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        first_max = max(nums)
-        second_max = -math.inf
-        third_max = -math.inf
-        for x in nums:
-            if x != first_max:
-                second_max = max(second_max, x)
-        if second_max == -math.inf:
-            return first_max
-        for x in nums:
-            if x != first_max and x != second_max:
-                third_max = max(third_max, x)
-        if third_max == -math.inf:
-            return first_max
-        return third_max
+        nums = list(set(nums))
+        max_three = heapq.nlargest(3, nums)
+        return max_three[2] if len(max_three) == 3 else max_three[0]
