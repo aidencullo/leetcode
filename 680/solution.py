bb2c8eb3e1a54e839afc020d9e1d341d4f94e3b9
Aiden Cullo <aidencullo@gmail.com>
2025-10-14 17:31:00 -0400

    n time 1 space

--- 680/solution.py
+++ 680/solution.py
@@ -1,16 +1,18 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
 
-        def isPalindrome(s):
-            for i in range(len(s) // 2):
-                if s[i] != s[len(s) - 1 - i]:
+        def isPalindrome(start, end):
+            length = end - start + 1
+            iterations = length // 2
+            for i in range(iterations):
+                if s[start + i] != s[end - i]:
                     return False
             return True
 
         l, r = 0, len(s) - 1
         while l < r:
             if s[l] != s[r]:
-                return isPalindrome(s[l: r]) or isPalindrome(s[l + 1: r + 1])
+                return isPalindrome(l + 1, r) or isPalindrome(l, r - 1)
             l += 1
             r -= 1
         return True
