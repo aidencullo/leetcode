c4253ca9e0ecfc8e74f9c4be544dbf1f108113c7
Aiden Cullo <aidencullo@gmail.com>
2025-10-14 12:47:03 -0400

    time n + klgn space 1

--- 1005/solution.py
+++ 1005/solution.py
@@ -1,3 +1,6 @@
 class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
-        
+        heapq.heapify(nums)
+        for _ in range(k):
+            heapq.heappush(nums, -heapq.heappop(nums))
+        return sum(nums)
