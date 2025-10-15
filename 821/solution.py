bc6df949d50446bfcd69b75cab3fad6b11d30745
Aiden Cullo <aidencullo@gmail.com>
2025-10-14 21:32:04 -0400

    time n + n + nlgn + n * lgn = O(nlgn) space n

--- 821/solution.py
+++ 821/solution.py
@@ -6,7 +6,21 @@ class Solution:
         for i, x in enumerate(s):
             if x == c:
                 cs.append(i)
+        cs.sort()
+        import bisect
+
+        def get_closest_index(index):
+            i = bisect.bisect(cs, index)
+            if i == 0:
+                return cs[0]
+            if i == len(cs):
+                return cs[len(cs) - 1]
+            if abs(cs[i] - index) < abs(cs[i - 1] - index):
+                return cs[i]
+            return cs[i - 1]
+        
         for i, x in enumerate(s):
-            answer[i] = min(abs(i - l) for l in cs)
+            c_i = get_closest_index(i)
+            answer[i] = abs(i - c_i)
         return answer
             
