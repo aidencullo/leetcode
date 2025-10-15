7e3b17a078225df617501d17380d8faad2f0a216
Aiden Cullo <aidencullo@gmail.com>
2025-10-15 15:00:03 -0400

    make previous solution more Pythonic

--- 71/solution.py
+++ 71/solution.py
@@ -1,12 +1,29 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        dirs = path.split('/')
-        stack = []
-        for dir in dirs:
-            if dir == '' or dir == '.':
-                continue
-            if dir == '..' and stack:
-                stack.pop()
-            if dir != '..':
-                stack.append(dir)
-        return '/' + '/'.join(stack)
+
+        def convert_double_periods(path):
+            dirs = path.split('/')
+            stack = []
+            for dir in dirs:
+                if dir == '..':
+                    if stack:
+                        stack.pop()
+                else:
+                    stack.append(dir)
+            return '/'.join(stack)
+
+        def convert_forward_slashes(path):
+            dirs = path.split('/')
+            dirs_no_extra_forward_slashes = [d for d in dirs if d]
+            return '/'.join(dirs_no_extra_forward_slashes)
+
+        def convert_single_periods(path):
+            dirs = path.split('/')
+            dirs_without_periods = [d for d in dirs if d != '.']
+            return '/'.join(dirs_without_periods)
+        
+        single_period_path = convert_single_periods(path)
+        without_slashes_path = convert_forward_slashes(single_period_path)
+        canonical_path = convert_double_periods(without_slashes_path)
+        
+        return '/' + canonical_path
