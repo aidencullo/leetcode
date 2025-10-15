d0aaf6fe7b8a21f4be2933cde7662638c67512bd
Aiden Cullo <aidencullo@gmail.com>
2025-10-15 12:57:12 -0400

    n time 1 space

--- 495/solution.py
+++ 495/solution.py
@@ -1,13 +1,13 @@
+import math
+import itertools
+
 class Solution:
     def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
         time_poisoned = 0
-        import math
         end_of_poison = -math.inf
         n = len(timeSeries)
-        for i, x in enumerate(timeSeries):
-            if i == n - 1:
-                time_poisoned += duration
-                continue
+        for i, x in enumerate(itertools.islice(timeSeries, 0, n - 1)):
             time_poisoned += min(duration, timeSeries[i + 1] - x)
+        time_poisoned += duration
         return time_poisoned
         
