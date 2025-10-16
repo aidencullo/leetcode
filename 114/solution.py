42b2be4c7d9b645d59a938784d567715686e7732
Aiden Cullo <aidencullo@gmail.com>
2025-10-16 14:06:26 -0400

    n timespace

--- 114/solution.py
+++ 114/solution.py
@@ -7,11 +7,21 @@
 class Solution:
     def flatten(self, root: Optional[TreeNode]) -> None:
         
-        def traverse(root: Optional[TreeNode]) -> None:
-            if root:
-                left = traverse(root.left)
-                left.right = root.right
-                traverse(root.right)
-                root.right = root.left
+        def traverse(node: Optional[TreeNode]) -> None:
+            if node:
+                left = node.left
+                right = node.right
+                rightmost_left = traverse(left)
+                if left:
+                    node.left = None
+                    node.right = left
+                    rightmost_left.right = right
+                rightmost_right = traverse(right)
+                if right:
+                    return rightmost_right
+                if left:
+                    return rightmost_left
+                return node
+
         traverse(root)
         return root
