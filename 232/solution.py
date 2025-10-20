07cdd866dd666c2aca74d70d9ca7065054126762
Aiden Cullo <aidencullo@gmail.com>
2025-10-20 16:03:55 -0400

    queue w stacks
    push n timespace
    rest 1 timespace

--- 232/solution.py
+++ 232/solution.py
@@ -1,18 +1,28 @@
 class MyQueue:
 
     def __init__(self):
-        
+        self.main = []
+        self.aux = []
 
     def push(self, x: int) -> None:
-        
+        if not self.main:
+            self.main.append(x)
+            return
+
+        while self.main:
+            self.aux.append(self.main.pop())
+        self.main.append(x)
+        while self.aux:
+            self.main.append(self.aux.pop())
 
     def pop(self) -> int:
+        return self.main.pop()
         
-
     def peek(self) -> int:
-        
+        return self.main[-1]
 
     def empty(self) -> bool:
+        return not self.main
         
 
 
