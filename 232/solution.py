d084967274206d82b178ae320ffbdfdf657a568e
Aiden Cullo <aidencullo@gmail.com>
2025-10-20 15:12:05 -0400

    template

--- 232/solution.py
+++ 232/solution.py
@@ -1,24 +1,24 @@
 class MyQueue:
 
     def __init__(self):
-        self.front = []
-        self.end = []
+        
 
     def push(self, x: int) -> None:
-        self.end.append(x)
+        
 
     def pop(self) -> int:
-        self._fix_stack()
-        return self.front.pop()
+        
 
     def peek(self) -> int:
-        self._fix_stack()
-        return self.front[-1]
-
-    def _fix_stack(self) -> None:
-        if not self.front:
-            while self.end:
-                self.front.append(self.end.pop())
+        
 
     def empty(self) -> bool:
-        return not self.front and not self.end
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
