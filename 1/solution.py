fca559c9b989f69ba4c421715b004bc8dc4946be
Aiden Cullo <aidencullo@gmail.com>
2025-10-28 13:12:14 -0400

    n timespace

--- 1/solution.py
+++ 1/solution.py
@@ -1,3 +1,7 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        others = {v: i for i, v in enumerate(nums)}
+        for i, num in enumerate(nums):
+            pair = target - num
+            if pair in others and others[pair] != i:
+                return [i, others[pair]]
