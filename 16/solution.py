54706fb3c60829a5eeec3c09787c4d457a419c89
Aiden Cullo <aidencullo@gmail.com>
2025-10-28 15:21:52 -0400

    past sample testcases n^3 time

--- 16/solution.py
+++ 16/solution.py
@@ -1,3 +1,15 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        pass
+        min_diff = math.inf
+        min_sum = math.inf
+        n = len(nums)
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                for k in range(j + 1, n):
+                    current_sum = nums[i] + nums[j] + nums[k]
+                    diff = abs(current_sum - target)
+                    if diff < min_diff:
+                        min_diff = diff
+                        min_sum = current_sum
+        return min_sum
+            
