9abf837db2a078aa13170904fb7afa5642dd2421
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 12:58:14 -0400

    n^2 time n space

--- 2367/solution.py
+++ 2367/solution.py
@@ -2,9 +2,10 @@ class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
         triplets = 0
         n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                for k in range(j + 1, n):
-                    if nums[j] - nums[i] == diff and nums[k] - nums[j] == diff:
-                        triplets += 1
+        seen = set()
+        for j in range(n):
+            for k in range(j + 1, n):
+                if nums[k] - nums[j] == diff and nums[j] - diff in seen:
+                    triplets += 1
+            seen.add(nums[j])
         return triplets
