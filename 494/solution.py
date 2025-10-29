52fb4364c7f349ab3f88f3a6a116e6158c358c1b
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 17:48:04 -0400

    2^n time n space tle

--- 494/solution.py
+++ 494/solution.py
@@ -1,3 +1,11 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        pass
+        def findTargetSumWaysRecursive(index: int, running: int) -> int:
+            if index == n and running == target:
+                return 1
+            if index == n and running != target:
+                return 0
+            return findTargetSumWaysRecursive(index + 1, running + nums[index]) + findTargetSumWaysRecursive(index + 1, running - nums[index])
+
+        n = len(nums)
+        return findTargetSumWaysRecursive(0, 0)
