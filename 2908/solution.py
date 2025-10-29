52554bab786cc44e8ff628eb5dbc1a897ccb8406
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 15:27:51 -0400

    n timespace

--- 2908/solution.py
+++ 2908/solution.py
@@ -2,10 +2,21 @@ class Solution:
     def minimumSum(self, nums: List[int]) -> int:
         min_sum = math.inf
         n = len(nums)
+        prefix_sum_left = []
+        prefix_sum_right = []
+        minn = math.inf
+        for num in nums:
+            minn = min(minn, num)
+            prefix_sum_left.append(minn)
+        minn = math.inf
+        for num in reversed(nums):
+            minn = min(minn, num)
+            prefix_sum_right.append(minn)
+        prefix_sum_right = prefix_sum_right[::-1]
         for i in range(n):
             middle = nums[i]
-            min_left = min(nums[:i]) if i > 0 else math.inf
-            min_right = min(nums[i + 1:]) if i < n - 1 else math.inf
+            min_left = prefix_sum_left[i - 1] if i > 0 else math.inf
+            min_right = prefix_sum_right[i + 1] if i < n - 1 else math.inf
             if min_left < middle and min_right < middle:
                 min_sum = min(min_sum, min_left + middle + min_right)
         return min_sum if min_sum != math.inf else -1
