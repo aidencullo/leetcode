4622c373d1c0905dfad0239ad3ce5fdb070fdb32
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 18:20:06 -0400

    I wanna say n time and n space?

--- 494/solution.py
+++ 494/solution.py
@@ -1,11 +1,15 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         def findTargetSumWaysRecursive(index: int, running: int) -> int:
+            if (index, running) in mem:
+                return mem[(index, running)]
             if index == n and running == target:
                 return 1
             if index == n and running != target:
                 return 0
-            return findTargetSumWaysRecursive(index + 1, running + nums[index]) + findTargetSumWaysRecursive(index + 1, running - nums[index])
+            mem[(index, running)] = findTargetSumWaysRecursive(index + 1, running + nums[index]) + findTargetSumWaysRecursive(index + 1, running - nums[index])
+            return mem[(index, running)]
 
         n = len(nums)
+        mem = {}
         return findTargetSumWaysRecursive(0, 0)
