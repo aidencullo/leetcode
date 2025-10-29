5dfd4f3fb53019aaca4d70abea1b7d40a71cf671
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 15:12:14 -0400

    n^3

--- 2908/solution.py
+++ 2908/solution.py
@@ -1,3 +1,13 @@
 class Solution:
     def minimumSum(self, nums: List[int]) -> int:
-        pass
+        min_sum = math.inf
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                for k in range(j + 1, n):
+                    left = nums[i]
+                    middle = nums[j]
+                    right = nums[k]
+                    if left < middle and right < middle:
+                        min_sum = min(min_sum, left + middle + right)
+        return min_sum if min_sum != math.inf else -1
