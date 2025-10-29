90d6e905ca7303b591845e22659671e67712e1b4
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 15:21:03 -0400

    n^2

--- 2908/solution.py
+++ 2908/solution.py
@@ -3,11 +3,9 @@ class Solution:
         min_sum = math.inf
         n = len(nums)
         for i in range(n):
-            for j in range(i + 1, n):
-                for k in range(j + 1, n):
-                    left = nums[i]
-                    middle = nums[j]
-                    right = nums[k]
-                    if left < middle and right < middle:
-                        min_sum = min(min_sum, left + middle + right)
+            middle = nums[i]
+            min_left = min(nums[:i]) if i > 0 else math.inf
+            min_right = min(nums[i + 1:]) if i < n - 1 else math.inf
+            if min_left < middle and min_right < middle:
+                min_sum = min(min_sum, min_left + middle + min_right)
         return min_sum if min_sum != math.inf else -1
