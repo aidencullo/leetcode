be052d94649c8dcd1f671d6aaba951828aca2408
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 17:43:03 -0400

    n timespace

--- 2395/solution.py
+++ 2395/solution.py
@@ -1,3 +1,10 @@
 class Solution:
     def findSubarrays(self, nums: List[int]) -> bool:
-        pass
+        seen = set()
+        n = len(nums)
+        for i in range(n - 1):
+            pairSum = nums[i] + nums[i + 1]
+            if pairSum in seen:
+                return True
+            seen.add(pairSum)
+        return False
