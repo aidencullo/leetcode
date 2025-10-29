ccb56a2c4dc8bbcc05f33c9cad3b61fb98875054
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 19:54:19 -0400

    use functools caching

--- 494/solution.py
+++ 494/solution.py
@@ -1,13 +1,15 @@
+from functools import cache
+
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        s = sum(nums)
+
+        @cache
+        def findTargetSumWaysRecursive(index: int, running: int) -> int:
+            if index == n and running == target:
+                return 1
+            if index == n and running != target:
+                return 0
+            return findTargetSumWaysRecursive(index + 1, running + nums[index]) + findTargetSumWaysRecursive(index + 1, running - nums[index])
+
         n = len(nums)
-        sums = defaultdict(int)
-        sums[0] = 1
-        for num in nums:
-            new_sums = defaultdict(int)
-            for value, frequency in sums.items():
-                new_sums[value + num] += frequency
-                new_sums[value - num] += frequency
-            sums = new_sums
-        return sums[target]
+        return findTargetSumWaysRecursive(0, 0)
