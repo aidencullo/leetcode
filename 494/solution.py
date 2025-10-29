ddd82e23e717528845b08185a59eea0dab867f2e
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 17:43:49 -0400

    template

--- 494/solution.py
+++ 494/solution.py
@@ -1,16 +1,3 @@
-from typing import List
-from collections import defaultdict
-
-
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        total = sum(nums)
-        n = len(nums)
-        dp = {0: 1}
-        for i in range(1, n + 1):
-            new_dp = defaultdict(int)
-            for key in dp:
-                new_dp[key + nums[i - 1]] += dp[key]
-                new_dp[key - nums[i - 1]] += dp[key]
-            dp = new_dp
-        return dp[target]
+        pass
