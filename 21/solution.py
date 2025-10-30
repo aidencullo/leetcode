e8df041c51ba9de63e1e43a1bff0767b50101f87
Aiden Cullo <aidencullo@gmail.com>
2025-10-29 21:50:57 -0400

    template

--- 21/solution.py
+++ 21/solution.py
@@ -1,27 +1,8 @@
-from typing import Optional
-
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def mergeTwoLists(
-        self, list1: Optional[ListNode], list2: Optional[ListNode]
-    ) -> Optional[ListNode]:
-        head = ListNode()
-        runner = head
-        runner1 = list1
-        runner2 = list2
-        while runner1 and runner2:
-            if runner1.val <= runner2.val:
-                runner.next = runner1
-                runner1 = runner1.next
-            else:
-                runner.next = runner2
-                runner2 = runner2.next
-            runner = runner.next
-        runner.next = runner1 or runner2
-        return head.next
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        pass
