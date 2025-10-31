935ff0886c04660688937eee2e41ee5589d0833a
Aiden Cullo <aidencullo@gmail.com>
2025-10-31 11:44:19 -0400

    83 -- n time 1 space

--- 83/solution.py
+++ 83/solution.py
@@ -5,4 +5,10 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        runner = head
+        while runner:
+            while runner.next and runner.val == runner.next.val:
+                runner.next = runner.next.next
+            runner = runner.next
+        return head
+        
