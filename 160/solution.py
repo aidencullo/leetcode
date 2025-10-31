bc6001fca020f9c368ad88532e52d2c39f607c31
Aiden Cullo <aidencullo@gmail.com>
2025-10-31 12:43:54 -0400

    160 -- back to template

--- 160/solution.py
+++ 160/solution.py
@@ -1,34 +1,9 @@
-from typing import Optional
-
-
-class ListNode:
-    def __init__(self, x, next=None):
-        self.val = x
-        self.next = next
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
-    def getIntersectionNode(
-        self, headA: ListNode, headB: ListNode
-    ) -> Optional[ListNode]:
-        a = self.count(headA)
-        b = self.count(headB)
-        if a > b:
-            for _ in range(a - b):
-                headA = headA.next
-        else:
-            for _ in range(b - a):
-                headB = headB.next
-        while headA and headB:
-            if headA == headB:
-                return headA
-            headA = headA.next
-            headB = headB.next
-        return None
-
-    def count(self, head: ListNode):
-        cnt = 0
-        while head:
-            head = head.next
-            cnt += 1
-        return cnt
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        pass
