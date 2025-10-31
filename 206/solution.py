10853e8e1b4ed9bad7c526a64eb0334daaea4ac5
Aiden Cullo <aidencullo@gmail.com>
2025-10-31 17:40:14 -0400

    206 template

--- 206/solution.py
+++ 206/solution.py
@@ -1,18 +1,8 @@
-from typing import Optional
-
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        last = None
-        while head:
-            next = head.next
-            head.next = last
-            last = head
-            head = next
-        return last
+        pass
