d8bf8ae1a9b55552c5959cd6fc64adf3c9d10eda
Aiden Cullo <aidencullo@gmail.com>
2025-11-03 12:57:01 -0500

    206 -- mle

--- 206/solution.py
+++ 206/solution.py
@@ -5,4 +5,13 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        stack = []
+        while head:
+            stack.append(head.val)
+        reversed_head = ListNode()
+        runner = reversed_head
+        while val := stack.pop():
+            cur = ListNode(val)
+            runner.next = cur
+            runner = runner.next
+        return reversed_head.next
