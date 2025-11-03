9afcec3548e7f883c0f766a36e7a46f22d0d3049
Aiden Cullo <aidencullo@gmail.com>
2025-11-03 17:38:20 -0500

    206 -- n timespace

--- 206/solution.py
+++ 206/solution.py
@@ -5,11 +5,10 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def reverseList(last: ListNode, next: Optional[ListNode]) -> Optional[ListNode]:
-            if not head or not head.next:
-                return head
-            reverse_head = self.reverseList(head.next)
-            head.next.next = head
-            head.next = None
-            return reverse_head
-        return reverseList(head, head)
+        if not head or not head.next:
+            return head
+        reverse_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return reverse_head
+        
