e95a1c94dddf3b629da88afc9d1064bea870671a
Aiden Cullo <aidencullo@gmail.com>
2025-11-05 13:31:59 -0500

    917 -- n timespace

--- 917/solution.py
+++ 917/solution.py
@@ -1,3 +1,13 @@
 class Solution:
     def reverseOnlyLetters(self, s: str) -> str:
-        pass
+        stack = []
+        res = []
+        for char in s:
+            if char.isalpha():
+                stack.append(char)
+        for char in s:
+            if char.isalpha():
+                res.append(stack.pop())
+            else:
+                res.append(char)
+        return ''.join(res)
