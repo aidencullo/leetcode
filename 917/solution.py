07a3f9addb568ff02b6654473646d23383a1e063
Aiden Cullo <aidencullo@gmail.com>
2025-11-05 17:38:55 -0500

    917 -- n time 1 space

--- 917/solution.py
+++ 917/solution.py
@@ -1,13 +1,14 @@
 class Solution:
     def reverseOnlyLetters(self, s: str) -> str:
-        stack = []
-        res = []
-        for char in s:
-            if char.isalpha():
-                stack.append(char)
-        for char in s:
-            if char.isalpha():
-                res.append(stack.pop())
-            else:
-                res.append(char)
-        return ''.join(res)
+        chars = list(s)
+        n = len(s)
+        l, r = 0, n - 1
+        while l < r:
+            while l < r and not chars[l].isalpha():
+                l += 1
+            while l < r and not chars[r].isalpha():
+                r -= 1
+            chars[l], chars[r] =  chars[r], chars[l]
+            l += 1
+            r -= 1
+        return ''.join(chars)
