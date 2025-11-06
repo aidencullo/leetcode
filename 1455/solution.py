91297a84181118da811bd13e43dc4737a186c0a8
Aiden Cullo <aidencullo@gmail.com>
2025-11-06 14:27:09 -0500

    1455 n timespace

--- 1455/solution.py
+++ 1455/solution.py
@@ -1,3 +1,6 @@
 class Solution:
     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
-        pass
+        for i, word in enumerate(sentence.split()):
+            if word.startswith(searchWord):
+                return i + 1
+        return -1
