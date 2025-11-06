9cd2ea0060b85775c30acdbe4c80325ba6c7bef0
Aiden Cullo <aidencullo@gmail.com>
2025-11-06 14:12:42 -0500

    1346 n^2 time 1 space

--- 1346/solution.py
+++ 1346/solution.py
@@ -1,3 +1,8 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        pass
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if arr[i] == 2 * arr[j] and j != i:
+                    return True
+        return False
+            
