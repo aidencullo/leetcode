7516d1bbec2c09073d9c46cba2e3353b44cad6d2
Aiden Cullo <aidencullo@gmail.com>
2025-11-06 18:49:06 -0500

    628 tle

--- 628/solution.py
+++ 628/solution.py
@@ -1,3 +1,9 @@
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
-        pass
+        maximum_product = -math.inf
+        for i, x in enumerate(nums):
+            for j, y in enumerate(nums):
+                for k, z in enumerate(nums):
+                    if i != j and j != k and i != k:
+                        maximum_product = max(maximum_product, x * y * z)
+        return maximum_product
