5b3680fd1542a29c7749c71c88f9b047748055c2
Aiden Cullo <aidencullo@gmail.com>
2025-11-07 18:02:34 -0500

    1030  n^2 time and space

--- 1030/solution.py
+++ 1030/solution.py
@@ -1,9 +1,29 @@
 class Solution:
     def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
-        grid  = [[0] * cols for i in range(rows)]
-        for row in range(rows):
-            for col in range(cols):
-                grid[row][col] = abs(row - rCenter) + abs(col - cCenter)
-        distances  = [(grid[row][col], [row, col]) for row in range(rows) for col in range(cols)]
-        distances.sort()
-        return [pair for distance, pair in distances]
+        from collections import deque
+
+        dq = deque()
+
+        r_pair = (rCenter, cCenter)
+
+        dq.append(r_pair)
+        
+
+        seen = set()
+        seen.add(r_pair)
+        cells = []
+
+        while dq:
+            pair = dq.popleft()
+            r, c = pair
+            cells.append(pair)
+            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                new_r = r + dr
+                new_c = c + dc
+                new_pair = (new_r, new_c)
+                if new_r >= 0 and new_r < rows and new_c >= 0 and new_c < cols and new_pair not in seen:
+                    seen.add(new_pair)
+                    dq.append(new_pair)
+                        
+        return cells
+            
