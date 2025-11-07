e4f5d5cb46cd7d18ef48f0fb0a6b854ad71eb2b4
Aiden Cullo <aidencullo@gmail.com>
2025-11-06 19:02:57 -0500

    628 nlgn time n space

--- 628/solution.py
+++ 628/solution.py
@@ -1,9 +1,7 @@
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
-        maximum_product = -math.inf
-        for i, x in enumerate(nums):
-            for j, y in enumerate(nums):
-                for k, z in enumerate(nums):
-                    if i != j and j != k and i != k:
-                        maximum_product = max(maximum_product, x * y * z)
+        nums.sort()
+        maximum_product = prod(nums[-3:])
+        maximum_product = max(prod(nums[-2:]) * prod[-1])
         return maximum_product
+        
