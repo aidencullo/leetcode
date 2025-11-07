5df28143ce96f44579a405aa086f13b96a6c4f75
Aiden Cullo <aidencullo@gmail.com>
2025-11-07 13:46:23 -0500

    1030 n^2 lg n^2 time n^2 space

--- 1030/solution.py
+++ 1030/solution.py
@@ -1,3 +1,9 @@
 class Solution:
     def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
-        pass
+        grid  = [[0] * cols for i in range(rows)]
+        for row in range(rows):
+            for col in range(cols):
+                grid[row][col] = abs(row - rCenter) + abs(col - cCenter)
+        distances  = [(grid[row][col], [row, col]) for row in range(rows) for col in range(cols)]
+        distances.sort()
+        return [pair for distance, pair in distances]
