1dd035f2f4bda1ba4beeb8f7c02a5e600c80cfc4
Aiden Cullo <aidencullo@gmail.com>
2025-11-08 18:25:50 -0500

    70 2^n tle

--- 70/solution.py
+++ 70/solution.py
@@ -1,14 +1,7 @@
 class Solution:
-
-    def __init__(self):
-        self.mem = {}
-
     def climbStairs(self, n: int) -> int:
-        if n in self.mem:
-            return self.mem[n]
-        if n == 0:
+        if n == 1:
             return 1
-        if n < 0:
-            return 0
-        self.mem[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
-        return self.mem[n]
+        if n == 2:
+            return 2
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
