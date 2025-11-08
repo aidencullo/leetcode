a6bf3f07f93446523283deae483be24bef1a2dcc
Aiden Cullo <aidencullo@gmail.com>
2025-11-08 11:33:09 -0500

    661 n^2 time and space

--- 661/solution.py
+++ 661/solution.py
@@ -6,12 +6,13 @@ class Solution:
         cols = len(img[0])
         for row in range(len(img)):
             for col in range(len(img[0])):
-                count = 0
-                total = 0
-                for i in range(row - 1, row + 2):
-                    for j in range(col - 1, col + 2):
-                        if 0 <= i < rows and 0 <= j < cols:
-                            total += img[i][j]
-                            count += 1
+                count = sum(1 for i in range(row - 1, row + 2) for j in range(col - 1, col + 2) if 0 <= i < rows and 0 <= j < cols)
+
+                total = sum(
+                    img[r][c]
+                    for r in range(row-1, row+2)
+                    for c in range(col-1, col+2)
+                    if 0 <= r < rows and 0 <= c < cols
+                )
                 smoother[row][col] = total // count
         return smoother
