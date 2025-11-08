efd7155f0cb55ef5d818e0bbbae2d4f8101b82ad
Aiden Cullo <aidencullo@gmail.com>
2025-11-08 11:25:25 -0500

    show that deepcopy works on 3-level nested array

--- 661/copy.py
+++ 661/copy.py
@@ -1,4 +1,4 @@
-from copy import deepcopy
+from copy import deepcopy, copy
 
 lst = dir(1)
 cpy = deepcopy(lst)
@@ -15,3 +15,20 @@ list1 = lst
 list2 = cpy
 # False
 assert not all(x is y for x, y in zip(list1, list2))
+
+a = [0]
+lst = [[a]]
+lst
+cpy = deepcopy(lst)
+a[0] = 1
+assert lst[0][0][0] == 1
+assert cpy[0][0][0] == 0
+
+
+a = [0]
+lst = [[a]]
+lst
+cpy = copy(lst)
+a[0] = 1
+assert lst[0][0][0] == 1
+assert cpy[0][0][0] == 1
