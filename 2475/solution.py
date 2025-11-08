dd84e1dd6f9f5047d0a2cc55d6e19e6494f5058f
Aiden Cullo <aidencullo@gmail.com>
2025-11-08 14:35:00 -0500

    2475 n^3 time n space

--- 2475/solution.py
+++ 2475/solution.py
@@ -1,15 +1,13 @@
+from itertools import combinations
+from collections import Counter
+from math import prod
+from typing import List
+
 class Solution:
     def unequalTriplets(self, nums: List[int]) -> int:
-        n = len(nums)
+        nums_unique = set(nums)
+        counter = Counter(nums)
         count = 0
-        for i in range(n):
-            for j in range(i + 1, n):
-                for k in range(j + 1, n):
-                    if nums[i] == nums[j]:
-                        continue
-                    if nums[i] == nums[k]:
-                        continue
-                    if nums[j] == nums[k]:
-                        continue
-                    count += 1
+        for combo in combinations(nums_unique, 3):
+            count += prod(counter[key] for key in combo)
         return count
