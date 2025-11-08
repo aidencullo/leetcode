dd42fb60b723d11cdb1158700fb8577c111504f0
Aiden Cullo <aidencullo@gmail.com>
2025-11-08 18:27:49 -0500

    70 n time n space

--- 70/solution.py
+++ 70/solution.py
@@ -1,7 +1,13 @@
 class Solution:
+    def __init__(self):
+        self.mem = {}
+    
     def climbStairs(self, n: int) -> int:
+        if n in self.mem:
+            return self.mem[n]
         if n == 1:
             return 1
         if n == 2:
             return 2
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        self.mem[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.mem[n]
