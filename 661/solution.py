f4d4183f4d66f2032af6b8f599d8a83159815dbd
Aiden Cullo <aidencullo@gmail.com>
2025-11-08 11:51:22 -0500

    661 instantiate new matrix myself

--- 661/solution.py
+++ 661/solution.py
@@ -1,28 +1,16 @@
-#for now let's just assume all objects are immutable or lists
-def deepcopy(obj):
-    if isinstance(obj, list):
-        res = []
-        for sub_obj in obj:
-            res.append(deepcopy(sub_obj))
-        return res
-    # immutable
-    return obj
-
-
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        smoother = deepcopy(img)
         rows = len(img)
         cols = len(img[0])
+        smoother = [[0] * cols for row in range(rows)]
         for row in range(len(img)):
             for col in range(len(img[0])):
-                count = sum(1 for i in range(row - 1, row + 2) for j in range(col - 1, col + 2) if 0 <= i < rows and 0 <= j < cols)
-
-                total = sum(
-                    img[r][c]
-                    for r in range(row-1, row+2)
-                    for c in range(col-1, col+2)
-                    if 0 <= r < rows and 0 <= c < cols
-                )
+                count = 0
+                total = 0
+                for i in range(row - 1, row + 2):
+                    for j in range(col - 1, col + 2):
+                        if 0 <= i < rows and 0 <= j < cols:
+                            total += img[i][j]
+                            count += 1
                 smoother[row][col] = total // count
         return smoother
