dc530b5d89eefdea08b461fa7a242521115d44b8
Aiden Cullo <aidencullo@gmail.com>
2025-11-08 13:58:45 -0500

    724 n time n space

--- 724/solution.py
+++ 724/solution.py
@@ -1,9 +1,24 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        total_sum = sum(nums)
-        running_sum = 0
-        for i, x in enumerate(nums):
-            if running_sum == total_sum - running_sum - x:
+
+        def get_left_sum(index):
+            if i == 0:
+                return 0
+            return left_prefix[index - 1]
+
+        def get_right_sum(index):
+            if i == n - 1:
+                return 0
+            return right_prefix[index + 1]
+        
+        if len(nums) == 1:
+            return 0
+        
+        from itertools import accumulate
+        left_prefix = list(accumulate(nums))
+        right_prefix = list(reversed(list(accumulate(reversed(nums)))))
+        n = len(nums)
+        for i in range(n):
+            if get_left_sum(i) == get_right_sum(i):
                 return i
-            running_sum += x
         return -1
