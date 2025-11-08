27b157034dd64ea203c32e9c17f641e5c863c87e
Aiden Cullo <aidencullo@gmail.com>
2025-11-08 14:51:28 -0500

    2475 n timespace

--- 2475/solution.py
+++ 2475/solution.py
@@ -5,9 +5,12 @@ from typing import List
 
 class Solution:
     def unequalTriplets(self, nums: List[int]) -> int:
-        nums_unique = set(nums)
         counter = Counter(nums)
+        left_sum = 0
+        right_sum = sum(counter.values())
         count = 0
-        for combo in combinations(nums_unique, 3):
-            count += prod(counter[key] for key in combo)
+        for key in counter.keys():
+            right_sum -= counter[key]
+            count += left_sum * counter[key] * right_sum
+            left_sum += counter[key]
         return count
