43056357d51e00cdfb8178e644497b021f123a99
Aiden Cullo <aidencullo@gmail.com>
2025-11-08 13:37:37 -0500

    717 n time 1 space

--- 717/solution.py
+++ 717/solution.py
@@ -1,9 +1,12 @@
 class Solution:
     def isOneBitCharacter(self, bits: List[int]) -> bool:
-        if not bits:
-            return False
-        if len(bits) == 1:
-            return True
-        if bits[0] == 0:
-            return self.isOneBitCharacter(bits[1:])
-        return self.isOneBitCharacter(bits[2:])
+        i = 0
+        n = len(bits)
+        while i < n:
+            if i == n - 1:
+                return True
+            if bits[i] == 0:
+                i += 1
+                continue
+            i += 2
+        return False
