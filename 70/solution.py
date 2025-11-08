6f7a22b246cba28567bbe6a59b5aadd5a0904129
Aiden Cullo <aidencullo@gmail.com>
2025-11-08 18:36:23 -0500

    70 n time n space using dp

--- 70/solution.py
+++ 70/solution.py
@@ -1,11 +1,7 @@
-from functools import cache
-
 class Solution:
 
-    @cache
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        a = b = 1
+        for i in range(n):
+            a, b = b, a + b
+        return a
