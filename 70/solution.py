c5aa47efd91ef5c0575335f0ab930d4a8bb98358
Aiden Cullo <aidencullo@gmail.com>
2025-11-08 18:31:51 -0500

    70 n time n space using functool cache

--- 70/solution.py
+++ 70/solution.py
@@ -1,13 +1,11 @@
+from functools import cache
+
 class Solution:
-    def __init__(self):
-        self.mem = {}
-    
+
+    @cache
     def climbStairs(self, n: int) -> int:
-        if n in self.mem:
-            return self.mem[n]
         if n == 1:
             return 1
         if n == 2:
             return 2
-        self.mem[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
-        return self.mem[n]
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
