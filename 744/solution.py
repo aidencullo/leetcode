82f2af72cb5b0936e48a8b8ac5b134a2828462b1
Aiden Cullo <aidencullo@gmail.com>
2025-11-09 13:23:31 -0500

    744 n timespace

--- 744/solution.py
+++ 744/solution.py
@@ -1,11 +1,15 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        n = len(letters)
-        l = bisect.bisect_right(letters, target)
+        unique_letters = set(letters)
+        import string
 
+        alphabet = string.ascii_lowercase  # 'abcdefghijklmnopqrstuvwxyz'
+        target_index = alphabet.index(target)
 
-        if l >= n:
-            return letters[0]
-        return letters[l]
+        for i in range(target_index + 1, len(alphabet)):
+            if alphabet[i] in unique_letters:
+                return alphabet[i]
+        return letters[0]
+        
         
 
