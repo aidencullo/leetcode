8c1ef8e7e69d86a9c7b7f1604d0c66dc2f3e29a5
Aiden Cullo <aidencullo@gmail.com>
2025-11-09 12:27:31 -0500

    use chain instead of +

--- 744/concat.py
+++ 744/concat.py
@@ -1,7 +1,7 @@
 from functools import reduce
 from itertools import batched, chain
 batches = batched(range(10), 2)
-reduced = reduce(lambda x, y: x + y, batches)
+reduced = reduce(chain, batches)
 batches = batched(range(10), 2)
 chained = chain.from_iterable(batches)
 assert list(reduced) == list(chained)
