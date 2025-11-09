3d1687701a2c3f3de7b08f4867f9fa8f9ccbc35c
Aiden Cullo <aidencullo@gmail.com>
2025-11-09 11:18:08 -0500

    566 n^2 time and 1 space

--- 566/solution.py
+++ 566/solution.py
@@ -2,16 +2,17 @@ class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
         rows = len(mat)
         cols = len(mat[0])
+
         if r * c != rows * cols:
             return mat
+
         data = []
+        reshaped = [[0] * c for _ in range(r)]
+
         for row in range(rows):
             for col in range(cols):
-                data.append(mat[row][col])
-        data_index = 0
-        reshaped = [[0] * c for _ in range(r)]
-        for i in range(r):
-            for j in range(c):
-                reshaped[i][j] = data[data_index]
-                data_index += 1
+                data_index = row * cols + col
+                new_row = data_index // c
+                new_col = data_index % c
+                reshaped[new_row][new_col] = mat[row][col]
         return reshaped
