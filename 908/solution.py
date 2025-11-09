30286bf89aeb80e75cd58986ec2a7ca8e4204ba6
Aiden Cullo <aidencullo@gmail.com>
2025-11-09 17:26:25 -0500

    908 constant timespace

--- 908/solution.py
+++ 908/solution.py
@@ -1,6 +1,4 @@
 class Solution:
     def smallestRangeI(self, nums: List[int], k: int) -> int:
         diff = max(nums) - min(nums)
-        if diff > 2 * k:
-            return diff - 2 * k
-        return 0
+        return max(diff - 2 * k, 0)
