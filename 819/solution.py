2ca40d8b16629c0520223211ab80d910f6e36343
Aiden Cullo <aidencullo@gmail.com>
2025-11-09 17:59:02 -0500

    819 O(pb) time O(b + p) space

--- 819/solution.py
+++ 819/solution.py
@@ -1,9 +1,8 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        banned_unique = set(banned)
         paragraph = paragraph.lower()
         words = re.findall(r'\b\w+\b', paragraph)
-        words = [word for word in words if word not in banned_unique]
+        words = [word for word in words if word not in banned]
         counter = Counter(words)
         max_word = max(counter.items(), key=lambda x: x[1])[0]
         return max_word
