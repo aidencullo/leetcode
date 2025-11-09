7edf094c233d888ec6d644cc554850df9ffe7158
Aiden Cullo <aidencullo@gmail.com>
2025-11-09 13:18:14 -0500

    744 lgn time 1 space -- stdlib functions

--- 744/solution.py
+++ 744/solution.py
@@ -1,16 +1,8 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
         n = len(letters)
-        l, r = 0, n - 1
+        l = bisect.bisect_right(letters, target)
 
-        while l <= r:
-            k = (l + r) // 2
-            el = letters[k]
-
-            if el <= target:
-                l = k + 1
-            else:
-                r = k - 1
 
         if l >= n:
             return letters[0]
