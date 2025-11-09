a8421218341be0246949afe66a83043300919eaf
Aiden Cullo <aidencullo@gmail.com>
2025-11-09 18:10:26 -0500

    819 O(pb) time O(b + p) space

--- 819/solution.py
+++ 819/solution.py
@@ -1,11 +1,9 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         import string
-        for punc in string.punctuation:
-            paragraph = paragraph.replace(punc, ' ')
         paragraph = paragraph.lower()
+        paragraph = ''.join(map(lambda x: x if x.isalpha() else ' ', paragraph))
         words = paragraph.split()
-        # words = re.findall(r'\b\w+\b', paragraph)
-        words = [word for word in words if word not in banned and word]
+        words = [word for word in words if word not in banned]
         counter = Counter(words)
         return max(counter, key=counter.get)
