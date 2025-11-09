4283a9db81999c67dd186ad539a9f81bf586c923
Aiden Cullo <aidencullo@gmail.com>
2025-11-09 11:52:25 -0500

    598 n time 1 space where n = len(ops)

--- 598/solution.py
+++ 598/solution.py
@@ -1,12 +1,7 @@
 class Solution:
     def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
-        M = [[0] * n for _ in range(m)]
-        for r in range(m):
-            for c in range(n):
-                for a, b in ops:
-                    if r < a and c < b:
-                        M[r][c] += 1
-        all_data = [item for row in M for item in row]
-        max_item = max(all_data)
-        return all_data.count(max_item)
-                        
+        if not ops:
+            return m * n
+        min_cols = min(m, min(a for a, b in ops))
+        min_rows = min(n, min(b for a, b in ops))
+        return min_cols * min_rows
