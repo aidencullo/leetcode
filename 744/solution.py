67a7fd807a5e37f12b2046ddb5b4feee0aa3169c
Aiden Cullo <aidencullo@gmail.com>
2025-11-09 13:13:01 -0500

    744 lgn time 1 space

--- 744/solution.py
+++ 744/solution.py
@@ -1,6 +1,19 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        for letter in letters:
-            if letter > target:
-                return letter
-        return letters[0]
+        n = len(letters)
+        l, r = 0, n - 1
+
+        while l <= r:
+            k = (l + r) // 2
+            el = letters[k]
+
+            if el <= target:
+                l = k + 1
+            else:
+                r = k - 1
+
+        if l >= n:
+            return letters[0]
+        return letters[l]
+        
+
