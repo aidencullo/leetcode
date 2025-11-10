87d122dd311a1773a6035363a5e3dc1094f70924
Aiden Cullo <aidencullo@gmail.com>
2025-11-10 14:09:03 -0500

    872 n timespace

--- 872/solution.py
+++ 872/solution.py
@@ -1,21 +1,24 @@
-from typing import Optional
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        def dfs(root: Optional[TreeNode]) -> list[int]:
-            if not root:
-                return
-            if not root.left and not root.right:
-                yield root.val
-            yield from dfs(root.left)
-            yield from dfs(root.right)
+        def get_leaf_value_sequence(root):
+            def helper(node):
+                if not node.left and not node.right:
+                    sequence.append(node.val)
+                if node.left:
+                    helper(node.left)
+                if node.right:
+                    helper(node.right)
+            sequence = []
+            helper(root)
+            return sequence
 
-        return list(dfs(root1)) == list(dfs(root2))
+        
+        tree1_leaf_value_sequence = get_leaf_value_sequence(root1)
+        tree2_leaf_value_sequence = get_leaf_value_sequence(root2)
+        return tree1_leaf_value_sequence == tree2_leaf_value_sequence
