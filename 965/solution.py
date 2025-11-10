94e02d9f49d239041260f21dacab466de73906ac
Aiden Cullo <aidencullo@gmail.com>
2025-11-10 17:27:22 -0500

    965 n timespace

--- 965/solution.py
+++ 965/solution.py
@@ -1,22 +1,19 @@
-from typing import Optional
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
-        def helper(root: Optional[TreeNode], prev) -> bool:
-            if not root:
-                return
-            self.univalue = self.univalue and root.val == prev
-            helper(root.left, prev)
-            helper(root.right, prev)
+        if not root:
+            return True
+        
+        def helper(node, x):
+            if not node:
+                return True
+            
+            return node.val == x and helper(node.left, x) and helper(node.right, x)
 
-        self.univalue = True
-        helper(root, root.val)
-        return self.univalue
+        c = root.val
+        return helper(root, c)
