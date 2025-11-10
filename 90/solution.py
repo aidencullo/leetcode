d7fb7f48513870c0f57625dbf6074cf08c3c8ef1
Aiden Cullo <aidencullo@gmail.com>
2025-11-10 13:59:03 -0500

    90 n 2^n time n2^n space

--- 90/solution.py
+++ 90/solution.py
@@ -2,7 +2,7 @@ class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         def subsets(index, subset):
             if index == n:
-                res.add(tuple(sorted(subset)))
+                res.add(tuple(subset))
                 return
 
             subset.append(nums[index])
@@ -12,5 +12,6 @@ class Solution:
 
         n = len(nums)
         res = set()
+        nums.sort()
         subsets(0, [])
         return [list(subset) for subset in res]
