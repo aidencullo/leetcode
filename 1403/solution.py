c10c3610492cfbfe51e5ec357109acbea02d74d9
Aiden Cullo <aidencullo@gmail.com>
2025-11-10 12:06:18 -0500

    1403 nlgn time n space

--- 1403/solution.py
+++ 1403/solution.py
@@ -1,10 +1,8 @@
 class Solution:
     def minSubsequence(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        total = 0
-        n = len(nums)
-        i = n - 1
-        while i >= 0 and total <= sum(nums) / 2:
-            total += nums[i]
-            i -= 1
-        return list(reversed(nums[i + 1:]))
+        nums.sort(reverse=True)
+        acc = list(accumulate(nums))
+        from bisect import bisect_right
+        i = bisect_right(acc, sum(nums) / 2)
+        return nums[:i + 1]
+        
