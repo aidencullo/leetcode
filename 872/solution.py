90f16f1b7af96fbcfda1f5c500af956f42a0d6a6
Aiden Cullo <aidencullo@gmail.com>
2025-11-10 14:19:40 -0500

    872 n timespace simplify

--- 872/solution.py
+++ 872/solution.py
@@ -6,19 +6,15 @@
 #         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        def get_leaf_value_sequence(root):
-            def helper(node):
-                if not node.left and not node.right:
-                    return [node.val]
-                res = []
-                if node.left:
-                    res.extend(helper(node.left))
-                if node.right:
-                    res.extend(helper(node.right))
-                return res
-            return helper(root)
+        def dfs(node):
+            if not node.left and not node.right:
+                return [node.val]
+            res = []
+            if node.left:
+                res.extend(dfs(node.left))
+            if node.right:
+                res.extend(dfs(node.right))
+            return res
 
         
-        tree1_leaf_value_sequence = get_leaf_value_sequence(root1)
-        tree2_leaf_value_sequence = get_leaf_value_sequence(root2)
-        return tree1_leaf_value_sequence == tree2_leaf_value_sequence
+        return dfs(root1) == dfs(root)
