f48152b3fbb53aed45ff1b52b0e553dce2cf3103
Aiden Cullo <aidencullo@gmail.com>
2025-11-09 19:33:47 -0500

    867 n^2 time 1 space

--- 867/solution.py
+++ 867/solution.py
@@ -1,11 +1,3 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        n = len(matrix)
-        m = len(matrix[0])
-
-        transposed = [[0] * n for _ in range(m)]
-
-        for r in range(n):
-            for c in range(m):
-                transposed[c][r] = matrix[r][c]
-        return transposed
+        return [list(items) for items in zip(*matrix)]
