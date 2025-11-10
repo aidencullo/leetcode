e5128c7f3399c76bc25f5ec6c8050cb3856d17ba
Aiden Cullo <aidencullo@gmail.com>
2025-11-10 11:37:28 -0500

    1403 nlgn time

--- 1403/solution.py
+++ 1403/solution.py
@@ -1,34 +1,10 @@
 class Solution:
     def minSubsequence(self, nums: List[int]) -> List[int]:
-        def findSubsequence(i, sub_sequence):
-            nonlocal min_subsequence
-            if i == n:
-                if sum(sub_sequence) <= sum(nums) / 2:
-                    return
-                if len(sub_sequence) > len(min_subsequence):
-                    return
-                if len(sub_sequence) == len(min_subsequence) and sum(sub_sequence) <= sum(min_subsequence):
-                    return
-                min_subsequence = sorted(sub_sequence, reverse=True)
-                return
-            
-            num = nums[i]
-
-            # if not sub_sequence:
-            #     findSubsequence(i + 1, sub_sequence + [num])
-            #     findSubsequence(i + 1, sub_sequence)
-
-            # last = sub_sequence[-1]
-
-            # if last >= num:
-            #     return
-
-            findSubsequence(i + 1, sub_sequence + [num])
-            findSubsequence(i + 1, sub_sequence)
-            
-
+        nums.sort()
+        total = 0
         n = len(nums)
-        min_subsequence = [0] * n
-        findSubsequence(0, [])
-        return min_subsequence
-        
+        i = n - 1
+        while i >= 0 and total <= sum(nums) / 2:
+            total += nums[i]
+            i -= 1
+        return list(reversed(nums[i + 1:]))
