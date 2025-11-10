6a75c4343a2ad654a9a24f98ae102ec091bb0e8f
Aiden Cullo <aidencullo@gmail.com>
2025-11-10 10:26:30 -0500

    1217 linear time constant space simpler

--- 1217/solution.py
+++ 1217/solution.py
@@ -1,6 +1,3 @@
 class Solution:
     def minCostToMoveChips(self, position: List[int]) -> int:
-        n = len(position)
-        evens = sum(1 if x % 2 == 0 else 0 for x in position)
-        odds = sum(1 if x % 2 == 1 else 0 for x in position)
-        return min(evens, odds)
+        return min(sum(x % 2 == 0 for x in position), sum(x % 2 == 1 for x in position))
