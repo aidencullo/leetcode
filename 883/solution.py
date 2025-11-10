847d933bee4e009b7d7690fb2a3355f3a81fc065
Aiden Cullo <aidencullo@gmail.com>
2025-11-09 19:59:27 -0500

    883 quadratic time constant space

--- 883/solution.py
+++ 883/solution.py
@@ -1,14 +1,7 @@
 class Solution:
     def projectionArea(self, grid: List[List[int]]) -> int:
 
-        z_area = 0
-        n = len(grid)
-
-        for i in range(n):
-            for j in range(n):
-                z_area += 1 if grid[i][j] else 0
-
-
+        z_area = sum(1 if item else 0 for row in grid for item in row)
         x_area = sum(max(row) for row in grid)
         y_area = sum(max(col) for col in zip(*grid))
 
