24e534b15e103d759f7c31f2d3e12f896de47802
Aiden Cullo <aidencullo@gmail.com>
2025-11-10 17:32:43 -0500

    965 n timespace stack

--- 965/solution.py
+++ 965/solution.py
@@ -6,13 +6,39 @@
 #         self.right = right
 class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
-        
-        def helper(node):
-            if not node:
-                return True
-            
-            return node.val == root.val and helper(node.left) and helper(node.right)
+        stack = [root]
 
-        return helper(root)
+        while stack:
+            node = stack.pop()
+            while node:
+                if node.val != root.val:
+                    return False
+                if node.right:
+                    stack.append(node.right)
+                node = node.left
+
+        return True
+
+
+#     # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+#         stack = [root]
+#         values = []
+
+#         while stack:
+#             node = stack.pop()
+#             while node:
+#                 values.append(node.val)
+#                 if node.right:
+#                     stack.append(node.right)
+#                 node = node.left
+
+#         return sum(x for x in values if low <= x <= high)
+
+        
