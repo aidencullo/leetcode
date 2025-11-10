bab85488b249eb63055ef46cbae128183445e5f5
Aiden Cullo <aidencullo@gmail.com>
2025-11-10 13:27:21 -0500

    78 2^n timespace

--- 78/solution.py
+++ 78/solution.py
@@ -1,15 +1,14 @@
 class Solution:
-    def subsets(self, nums: list[int]) -> list[list[int]]:
-        def dfs(i):
-            if i == len(nums):
-                res.append(subset.copy())
-            else:
-                dfs(i + 1)
-                subset.append(nums[i])
-                dfs(i + 1)
-                subset.pop()
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def subsets(index, subset):
+            if index == n:
+                res.append(subset)
+                return
 
+            subsets(index + 1, subset + [nums[index]])
+            subsets(index + 1, subset)
+
+        n = len(nums)
         res = []
-        subset = []
-        dfs(0)
+        subsets(0, [])
         return res
