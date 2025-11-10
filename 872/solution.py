facba8c513a4691ade84284ff7879dc51dfd523a
Aiden Cullo <aidencullo@gmail.com>
2025-11-10 14:22:17 -0500

    872 n^2 time n space

--- 872/solution.py
+++ 872/solution.py
@@ -7,14 +7,11 @@
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         def dfs(node):
+            if not node:
+                return []
             if not node.left and not node.right:
                 return [node.val]
-            res = []
-            if node.left:
-                res.extend(dfs(node.left))
-            if node.right:
-                res.extend(dfs(node.right))
-            return res
 
+            return dfs(node.left) + dfs(node.right)
         
-        return dfs(root1) == dfs(root)
+        return dfs(root1) == dfs(root2)
