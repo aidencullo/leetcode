ccf9cf7006e00aa90b7e27331c8dcea2aff99358
Aiden Cullo <aidencullo@gmail.com>
2025-11-11 16:52:25 -0500

    797 not sure the time or space -- gotta figure this one out

--- 797/solution.py
+++ 797/solution.py
@@ -1,3 +1,26 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        pass
+        def dfs(node):
+            if node in seen:
+                return
+
+            if node == n - 1:
+                seen.append(node)
+                paths.append(list(seen))
+                seen.pop()
+                return
+
+            seen.append(node)
+
+            for neighbor in graph[node]:
+                dfs(neighbor)
+
+            seen.pop()
+
+
+        paths = []
+        seen = []
+        n = len(graph)
+
+        dfs(0)
+        return paths
