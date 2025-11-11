4b06dc898abb1a70efc17a38bdef03661658cd1e
Aiden Cullo <aidencullo@gmail.com>
2025-11-11 16:09:44 -0500

    797 template

--- 797/solution.py
+++ 797/solution.py
@@ -1,18 +1,3 @@
-from collections import defaultdict
-
-
 class Solution:
-    def allPathsSourceTarget(self, graph_input: list[list[int]]) -> list[list[int]]:
-        graph = defaultdict(list)
-        for i, nodes in enumerate(graph_input):
-            graph[i] = nodes
-        src, dest = 0, len(graph) - 1
-        stack = [[src]]
-        paths = []
-        while stack:
-            cur = stack.pop()
-            if cur[-1] == dest:
-                paths.append(cur)
-            for node in graph[cur[-1]]:
-                stack.append(cur + [node])
-        return paths
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        pass
