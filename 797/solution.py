ec7e844651ae21a847a2c2f4ce85626a42949f75
Aiden Cullo <aidencullo@gmail.com>
2025-11-12 12:38:36 -0500

    797 n * 2^n timespace bfs

--- 797/solution.py
+++ 797/solution.py
@@ -1,26 +1,42 @@
+"""
+Input: graph = [[1,2],[3],[3],[]]
+Output: [[0,1,3],[0,2,3]]
+Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+
+
+dq        
+[
+    (3, [0, 2, 3]),
+    (3, [0, 1, 3]),
+]
+
+node     path
+2        [0, 2]
+
+"""
+
+from collections import deque
+
+
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        def dfs(node):
-            if node in seen:
-                return
+        def bfs(root): # root = 0
+            dq = deque()
+            dq.appendleft((root, [root]))
 
-            seen.append(node)
+            while dq:
+                node, path = dq.pop() # node = 1, path = [0, 1]
 
-            if node == n - 1:
-                paths.append(list(seen))
-                seen.pop()
-                return
+                if node == n - 1: # node = 1, n - 1 = 3 => False
+                    paths.append(path)
+                    continue
 
-
-            for neighbor in graph[node]:
-                dfs(neighbor)
-
-            seen.pop()
-
+                for neighbor in graph[node]: # graph[1] = [3], path = [0, 1]
+                    if neighbor not in path: # neighbor = 3
+                        dq.appendleft((neighbor, path + [neighbor]))
 
         paths = []
-        seen = []
         n = len(graph)
 
-        dfs(0)
+        bfs(0)
         return paths
