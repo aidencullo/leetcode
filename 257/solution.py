ea970e619ca0f7e1e032ab3360e982f9685a872f
Aiden Cullo <aidencullo@gmail.com>
2025-11-14 14:56:30 -0500

    257 n 2 ^ n timespace

--- 257/solution.py
+++ 257/solution.py
@@ -6,4 +6,31 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        pass
+        if not root:
+            return
+        
+        def is_leaf(node):
+            return not node.left and not node.right
+
+        paths = []
+        path = []
+        
+        def dfs(node):
+
+            path.append(node.val)
+            
+            if is_leaf(node):
+                paths.append(path.copy())
+                path.pop()
+                return
+
+            if node.left:
+                dfs(node.left)
+            if node.right:
+                dfs(node.right)
+
+            path.pop()
+            
+
+        dfs(root)
+        return ['->'.join(str(node) for node in path) for path in paths]
