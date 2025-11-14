d65d49be87fbc834e6b9a543608997a3d4d9e365
Aiden Cullo <aidencullo@gmail.com>
2025-11-14 14:48:18 -0500

    257 template

--- 257/solution.py
+++ 257/solution.py
@@ -6,18 +6,4 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        paths = []
-
-        def follow_paths(node, path):
-            new_path = path + [node.val]
-            if node and not node.left and not node.right:
-                paths.append(new_path)
-                return
-            if node.left:
-                follow_paths(node.left, new_path)
-            if node.right:
-                follow_paths(node.right, new_path)
-
-        follow_paths(root, [])
-        pretty_paths = ['->'.join([str(i) for i in path]) for path in paths]
-        return pretty_paths
+        pass
