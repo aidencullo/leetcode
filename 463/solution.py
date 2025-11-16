90d45b71f3d548276fa33ece975873db88ab8996
Aiden Cullo <aidencullo@gmail.com>
2025-11-16 14:56:54 -0500

    463 n timespace

--- 463/solution.py
+++ 463/solution.py
@@ -1,25 +1,53 @@
 class Solution:
-    def islandPerimeter(self, grid: list[list[int]]) -> int:
-        def dfs(grid, row, col):
-            if not (0 <= row < rows and 0 <= col < cols):
-                return 1
-            if (row, col) in seen:
-                return 0
-            if grid[row][col] == 0:
-                return 1
-            seen.add((row, col))
-            return (
-                dfs(grid, row + 1, col)
-                + dfs(grid, row - 1, col)
-                + dfs(grid, row, col + 1)
-                + dfs(grid, row, col - 1)
-            )
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        def is_water(r, c):
+            if not 0 <= r < len(grid):
+                return True
+            if not 0 <= c < len(grid[0]):
+                return True
 
+            return not grid[r][c]
+        
+        def dfs(r, c):
+            if not 0 <= r < len(grid):
+                return
+            if not 0 <= c < len(grid[0]):
+                return
+
+            if not grid[r][c]:
+                return
+            
+            if (r, c) in seen:
+                return
+
+            seen.add((r, c))
+            
+            nonlocal perimeter
+
+            if is_water(r + 1, c):
+                perimeter += 1
+            if is_water(r - 1, c):
+                perimeter += 1
+            if is_water(r, c + 1):
+                perimeter += 1
+            if is_water(r, c - 1):
+                perimeter += 1
+
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+
+
+        perimeter = 0
         seen = set()
+        # from collections import product
+        # pairs = product(range(rows), range(cols))
+        # for i, j in pairs:
+        #         dfs(i, j)
         rows = len(grid)
         cols = len(grid[0])
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == 1:
-                    return dfs(grid, i, j)
-        return 0
+                dfs(i, j)
+        return perimeter
