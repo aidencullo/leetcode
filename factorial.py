f93416ee4e9b78bf27a117b91500f3c748aa29da
Aiden Cullo <aidencullo@gmail.com>
2025-11-18 11:59:11 -0500

    timing unoptimized factorial fn

--- factorial.py
+++ factorial.py
@@ -1,3 +1,9 @@
+import time
+
+import sys
+sys.setrecursionlimit(10**5)  # default ~1000
+
+
 def factorial(n):
     if n < 0:
         raise Exception
@@ -9,3 +15,17 @@ assert factorial(1) == 1
 assert factorial(0) == 1
 assert factorial(5) == 120
 assert factorial(10) == 3628800
+
+
+def timed_factorial(n):
+
+    for i in range(5):
+        n = 10 ** i
+        start = time.time()
+        result = factorial(n)
+        end = time.time()
+        print(f'n={n:<10}: {end - start:>10.6f}s')
+
+
+timed_factorial(10)
+
