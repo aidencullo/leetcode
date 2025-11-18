dbc155fd7ba2271f2315b0761b3a1b9fc3e1da5b
Aiden Cullo <aidencullo@gmail.com>
2025-11-18 11:48:55 -0500

    1791 E + V time V space not sure if it's correct as leetcode(cloudfare) is down

--- 1791/solution.py
+++ 1791/solution.py
@@ -1,3 +1,17 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        pass
+        graph = defaultdict(int)
+
+        for edge in edges:
+            u, v = edge
+
+            graph[u] += 1
+            graph[v] += 1
+
+        n = len(graph)
+
+        for node in graph:
+            if graph[node] == n - 1:
+                return node
+
+        return -1
