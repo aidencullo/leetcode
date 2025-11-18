ef02918b839fe99f8a9bf1e6cde71693f5db39e2
Aiden Cullo <aidencullo@gmail.com>
2025-11-18 11:41:08 -0500

    463 n timespace

--- 463/solution.py
+++ 463/solution.py
@@ -5,16 +5,35 @@ class Solution:
         
         def is_water(r, c):
             return not in_grid(r, c) or not grid[r][c]
+
+        def is_land(r, c):
+            return not is_water(r, c)
         
         def dfs(r, c):
             if is_water(r, c):
                 return False
+
+            if (r, c) in seen:
+                return
+
+            seen.add((r, c))
+
+            nonlocal perimeter
+
+            perimeter += is_water(r + 1, c) + is_water(r - 1, c) + is_water(r, c + 1) + is_water(r, c - 1)
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
             
-            return is_water(r + 1, c) + is_water(r - 1, c) + is_water(r, c + 1) + is_water(r, c - 1)
 
         rows = len(grid)
         cols = len(grid[0])
         perimeter = 0
+        seen = set()
         for i, j in product(range(rows), range(cols)):
-                perimeter += dfs(i, j)
+            if is_land(i, j):
+                dfs(i, j)
+                break
         return perimeter
