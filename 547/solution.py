ecb6ea0746af5ed94b7b222436cc6cd103a4520c
Aiden Cullo <aidencullo@gmail.com>
2025-11-19 13:16:19 -0500

    547 n^2 time n space

--- 547/solution.py
+++ 547/solution.py
@@ -1,3 +1,20 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        pass
+        def dfs(node):
+            if node in seen:
+                return
+
+            seen.add(node)
+
+            for neighbor, connection in enumerate(isConnected[node]):
+                if neighbor != node and connection:
+                    dfs(neighbor)
+
+        seen = set()
+        nodes = len(isConnected)
+        provinces = 0
+        for node in range(nodes):
+            if node not in seen:
+                provinces += 1
+                dfs(node)
+        return provinces
