7fe57a534f09a7b4c9e4ebb7da9c4529ecf122ae
Aiden Cullo <aidencullo@gmail.com>
2025-11-21 16:51:00 -0500

    auto-pushed changes

--- 733/solution.py
+++ 733/solution.py
@@ -1,29 +1,27 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        def dfs(x, y, old_color):
-            if not (0 <= x < n and 0 <= y < m):
+        def dfs(r, c):
+            if not (0 <= r < n and 0 <= c < m):
+                return
+            
+            if (r, c) in seen:
                 return
 
-            if (x, y) in seen:
+            if image[r][c] != old_color:
                 return
 
-            if image[x][y] != old_color:
-                return
+            seen.add((r, c))
+            image[r][c] = color
 
-            image[x][y] = color
-            seen.add((x, y))
-
-            dfs(x + 1, y, old_color)
-            dfs(x - 1, y, old_color)
-            dfs(x, y + 1, old_color)
-            dfs(x, y - 1, old_color)
-
-            
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
             
 
-            
         n = len(image)
         m = len(image[0])
         seen = set()
-        dfs(sr, sc, image[sr][sc])
+        old_color = image[sr][sc]
+        dfs(sr, sc)
         return image
