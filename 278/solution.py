43d6e2b8b24ff31c489a104029a2cd10822b79f2
Aiden Cullo <aidencullo@gmail.com>
2025-11-21 16:38:13 -0500

    auto-pushed changes

--- 278/solution.py
+++ 278/solution.py
@@ -1,14 +1,12 @@
 # The isBadVersion API is already defined for you.
 # def isBadVersion(version: int) -> bool:
 
-
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        l, r = 0, n
+        l, r = 1, n
         while l <= r:
-            k = l + (r - l) // 2
-            is_bad = isBadVersion(k)
-            if is_bad:
+            k = (l + r) // 2
+            if isBadVersion(k):
                 r = k - 1
             else:
                 l = k + 1
