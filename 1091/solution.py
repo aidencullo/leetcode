20513b3e803fc968ee941779118bfe608ce231d3
Aiden Cullo <aidencullo@gmail.com>
2025-11-24 15:54:38 -0500

    pep8

--- 1091/solution.py
+++ 1091/solution.py
@@ -1,9 +1,14 @@
+from collections import deque
+from typing import List
+
+
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        from collections import deque
-
         seen = set()
-        directions = [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
+        directions = [
+            (-1, -1), (0, -1), (1, -1), (1, 0),
+            (1, 1), (0, 1), (-1, 1), (-1, 0)
+        ]
         n = len(grid)
         q = deque()
         q.appendleft((0, 0, 1))
@@ -12,6 +17,7 @@ class Solution:
             r, c, dist = q.pop()
             if not (0 <= r < n and 0 <= c < n and (r, c) not in seen and not grid[r][c]):
                 continue
+
             seen.add((r, c))
             if r == c == n - 1:
                 return dist
@@ -19,4 +25,5 @@ class Solution:
             for dr, dc in directions:
                 new_r, new_c = r + dr, c + dc
                 q.appendleft((new_r, new_c, dist + 1))
+
         return -1
