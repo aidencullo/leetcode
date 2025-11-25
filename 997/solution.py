db2bb15ec18b9a8f58b8082c41eed9c01612c750
Aiden Cullo <aidencullo@gmail.com>
2025-11-25 15:17:40 -0500

    auto-pushed changes

--- 997/solution.py
+++ 997/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        if n == 1:
+            return 1
         trusts = set()
         trusted = defaultdict(int)
-        for i in range(1, n + 1):
-            trusted[i] = 0
         for a, b in trust:
             trusts.add(a)
             trusted[b] += 1
