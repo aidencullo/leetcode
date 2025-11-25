15557e921a1006e6205f9ef5f77e85a9aa78c6f5
Aiden Cullo <aidencullo@gmail.com>
2025-11-25 15:15:18 -0500

    auto-pushed changes

--- 997/solution.py
+++ 997/solution.py
@@ -1,3 +1,11 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        pass
+        trusts = set()
+        trusted = defaultdict(int)
+        for a, b in trust:
+            trusts.add(a)
+            trusted[b] += 1
+            for person in trusted:
+                if trusted[person] == n - 1 and person not in trusts:
+                    return person
+        return -1
