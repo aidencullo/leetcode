17ac17e37c4ac6e679207b1e19b1df8704e00d69
Aiden Cullo <aidencullo@gmail.com>
2025-11-26 16:15:26 -0500

    auto-pushed changes

--- 1976/solution.py
+++ 1976/solution.py
@@ -1,34 +1,17 @@
 class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
         graph = defaultdict(list)
-        for u, v, time in roads:
-            graph[u].append((v, time))
-            graph[v].append((u, time))
+        for start, end, weight in roads:
+            graph[start].append((end, weight))
+        heap = [(0, 0)]
+        visited = set()
+        final_distances = {}
 
-        def dfs(node, running_time):
-            nonlocal shortest_time
-            nonlocal ways
-            
-            if node == n - 1:
-                if running_time < shortest_time:
-                    shortest_time = running_time
-                    ways = 1
-                elif running_time == shortest_time:
-                    ways += 1
-                return
-            
-            if node in seen:
-                return
 
-            seen.add(node)
+        while heap:
+            node, current_weight  = heapq.heappop(heap)
 
-            for neighbor, time_to in graph[node]:
-                dfs(neighbor, running_time + time_to)
-
-            seen.remove(node)
-
-        shortest_time = math.inf
-        ways = 0
-        seen = set()
-        dfs(0, 0)
-        return ways
+            for neighbor, weight in graph[node]:
+                if neighbor not in final_distances:
+                    final_distances[neighbor] = current_weight + weight
+                else:
--- 2006/solution.py
+++ 2006/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def countKDifference(self, nums: List[int], k: int) -> int:
-        total = 0
-        for x in nums:
-            for y in nums:
-                if x - y == k:
-                    total += 1
-        return total
+        seen = defaultdict(int)
+        res = 0
+        for num in nums:
+            res += seen[num + k]
+            res += seen[num - k]
+            seen[num] += 1
+        return res
