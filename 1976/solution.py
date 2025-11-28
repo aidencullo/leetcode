93d6480593e66fd879d2953511f3c9358b3d27bf
Aiden Cullo <aidencullo@gmail.com>
2025-11-28 17:00:15 -0500

    1976 still 1 off on test case...

--- 1976/solution.py
+++ 1976/solution.py
@@ -3,15 +3,39 @@ class Solution:
         graph = defaultdict(list)
         for start, end, weight in roads:
             graph[start].append((end, weight))
+            graph[end].append((start, weight))
+            
         heap = [(0, 0)]
         visited = set()
-        final_distances = {}
+        distances = {}
+        visited = set()
+        paths = {}
+        paths[0] = 1
 
 
         while heap:
-            node, current_weight  = heapq.heappop(heap)
+            weight, node  = heapq.heappop(heap)
 
-            for neighbor, weight in graph[node]:
-                if neighbor not in final_distances:
-                    final_distances[neighbor] = current_weight + weight
+            if node in distances and distances[node] < weight:
+                continue
+
+            for neighbor, neighbor_weight in graph[node]:
+                if neighbor in visited:
+                    continue
+                
+                other_path_to_neighbor = weight + neighbor_weight
+                if neighbor not in distances:
+                    distances[neighbor] = other_path_to_neighbor
+                    paths[neighbor] = paths[node]
+                    heapq.heappush(heap, (other_path_to_neighbor, neighbor))
+                elif distances[neighbor] > other_path_to_neighbor:
+                    distances[neighbor] = other_path_to_neighbor
+                    paths[neighbor] = paths[node]
+                    heapq.heappush(heap, (other_path_to_neighbor, neighbor))
                 else:
+                    paths[neighbor] += paths[node]
+                    
+
+            visited.add(node)
+
+        return paths[n - 1]
