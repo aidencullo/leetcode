ceeb9a38f117490b33715e64782c5236fb40877f
Aiden Cullo <aidencullo@gmail.com>
2025-11-29 16:12:58 -0500

    pep8ify

--- 1971/solution.py
+++ 1971/solution.py
@@ -1,34 +1,27 @@
-class Solution:
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = defaultdict(list)
+from collections import defaultdict, deque
+from typing import List
 
+
+class Solution:
+    def validPath(
+        self, n: int, edges: List[List[int]], source: int, destination: int
+    ) -> bool:
+        graph = defaultdict(list)
         for a, b in edges:
             graph[a].append(b)
             graph[b].append(a)
 
-        from collections import deque
-
         seen = set()
-        q = deque()
-
-        q.append(source)
+        q = deque([source])
 
         while q:
-            q_len = len(q)
-            for _ in range(q_len):
+            for _ in range(len(q)):
                 node = q.popleft()
-
                 if node == destination:
                     return True
-                
                 seen.add(node)
-                
                 for neighbor in graph[node]:
                     if neighbor not in seen:
                         q.append(neighbor)
 
         return False
-
-                        
-
-        
