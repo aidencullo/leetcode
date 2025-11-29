e4b6b330fe707d67b113dff849650263d2ad9478
Aiden Cullo <aidencullo@gmail.com>
2025-11-28 19:52:39 -0500

    template

--- 130/solution.py
+++ 130/solution.py
@@ -1,3 +1,6 @@
 class Solution:
-    def reverseBits(self, n: int) -> int:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
         pass
