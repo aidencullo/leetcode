0f6e6ddcedf9b44bb595295ecadef72a1be1bbb7
Aiden Cullo <aidencullo@gmail.com>
2025-11-30 21:06:00 -0500

    130 dfs n time 1 space

--- 130/solution.py
+++ 130/solution.py
@@ -4,30 +4,53 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
 
-        def fill(r, c):
-            def outside_grid(r, c):
-                return r < 0 or r >= len(board) or c < 0 or c >= len(board[0])
+        def dfs(r, c):
+
+            if not (0 <= r < len(board) and 0 <= c < len(board[0])):
+                return
 
             if (r, c) in seen:
-                return True
-            
-            if outside_grid(r, c):
-                return False
-
-            if board[r][c] == 'X':
-                return True
+                return
 
             seen.add((r, c))
 
-            if (fill(r, c - 1)
-                and fill(r, c + 1)
-                and fill(r - 1, c)
-                and fill(r + 1, c)):
-                board[r][c] = 'X'
-                return True
-            return False
-        
+            if board[r][c] == 'X':
+                return
+
+            if board[r][c] == 'O':
+                board[r][c] = '-'
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+
         seen = set()
+                
+        row = 0
+        for col in range(len(board[0])):
+            dfs(row, col)
+
+        row = len(board) - 1
+        for col in range(len(board[0])):
+            dfs(row, col)
+                
+        col = 0
+        for row in range(len(board)):
+            dfs(row, col)
+
+        col = len(board[0]) - 1
+        for row in range(len(board)):
+            dfs(row, col)
+
         for i in range(len(board)):
             for j in range(len(board[0])):
-                fill(i, j)                    
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == '-':
+                    board[i][j] = 'O'
+                   
