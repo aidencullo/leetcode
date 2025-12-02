568e0cbcbb765a24c12c4529d5c121f7443e29a5
Aiden Cullo <aidencullo@gmail.com>
2025-12-02 15:55:08 -0500

    getting max recursion error

--- fibo.py
+++ fibo.py
@@ -1,6 +1,14 @@
 from functools import cache
 from time import time
 
+@cache
+def fibo_cached(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibo_cached(n - 1) + fibo_cached(n - 2)
+
 def fibo(n):
     if n == 0:
         return 0
@@ -8,7 +16,6 @@ def fibo(n):
         return 1
     return fibo(n - 1) + fibo(n - 2)
 
-fibo_cached = cache(fibo)
 
 def test_fibonacci_times(fibo_impl, max_n=30, step=10):
     for n in range(step, max_n + 1, step):
@@ -16,5 +23,5 @@ def test_fibonacci_times(fibo_impl, max_n=30, step=10):
         fibo_impl(n)
         print(f"{n} {time() - start}")
 
-test_fibonacci_times(fibo_cached)
+test_fibonacci_times(fibo_cached, max_n=100000, step=1000)
 test_fibonacci_times(fibo)
