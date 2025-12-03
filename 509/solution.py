4d78185441eeae648bd88280ae0d9243e4dc7d34
Aiden Cullo <aidencullo@gmail.com>
2025-12-03 12:14:58 -0500

    509 n, iteration

--- 509/solution.py
+++ 509/solution.py
@@ -1,6 +1,11 @@
 class Solution:
-    @cache
     def fib(self, n: int) -> int:
-        if (n == 0 or n == 1):
-            return n
-        return self.fib(n - 1) + self.fib(n - 2)
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+
+        a, b = 0, 1
+        for _ in range(1, n):
+            a, b = b, a + b
+        return b
