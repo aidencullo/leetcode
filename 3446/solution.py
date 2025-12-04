e0ff14c567ff0751165e6ddcbca2c1add465d6d7
Aiden Cullo <aidencullo@gmail.com>
2025-12-04 15:56:51 -0500

    3446 n^2 lgn time n space

--- 3446/solution.py
+++ 3446/solution.py
@@ -2,21 +2,34 @@ class Solution:
     def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
         def process_bottom_left_triangle():
             n = len(grid)
-            for row_index in range(n):
-                seq = [grid[j][j] for j in range(row_index, n)]
+            for starting_row in range(n):
+                seq = []
+                col = 0
+                for row in range(starting_row, n):
+                    seq.append(grid[row][col])
+                    col += 1
                 seq_sorted = sorted(seq, reverse=True)
-                for j in range(row_index, n):
-                    grid[j][j] = seq_sorted[j - row_index]
+                col = 0
+                for row in range(starting_row, n):
+                    grid[row][col] = seq_sorted[col]
+                    col += 1
 
         process_bottom_left_triangle()
 
         def process_top_right_triangle():
             n = len(grid)
-            for col_index in range(1, n):
-                seq = [grid[j][j] for j in range(col_index, n)]
-                seq_sorted = sorted(seq, reverse=True)
-                for j in range(row_index, n):
-                    grid[j][j] = seq_sorted[j - row_index]
+            for starting_col in range(1, n):
+                seq = []
+                row = 0
+                for col in range(starting_col, n):
+                    seq.append(grid[row][col])
+                    row += 1
+                seq_sorted = sorted(seq)
+                col = 0
+                row = 0
+                for col in range(starting_col, n):
+                    grid[row][col] = seq_sorted[row]
+                    row += 1
 
         process_top_right_triangle()
 
