12583421a82cfe0c46171483dc7976a9eaf7e96f
Aiden Cullo <aidencullo@gmail.com>
2025-12-08 15:41:22 -0500

    3731 n + n + k + n + k = O(k) time

--- 3731/solution.py
+++ 3731/solution.py
@@ -2,7 +2,7 @@ class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
         low = min(nums)
         high = max(nums)
-        all_nums = set(range(low, high + 1))
+        all_nums = dict.fromkeys(range(low, high + 1))
         for num in nums:
-            all_nums.remove(num)
-        return sorted(all_nums)
+            all_nums.pop(num, None)  # does nothing, no error
+        return list(all_nums.keys())
