3c1ec78bea0188f80d8e8d5082302a5f9351e46f
Aiden Cullo <aidencullo@gmail.com>
2025-12-10 11:21:57 -0500

    1710 nlgn + k where k is the total number of boxes of all types

--- 1710/solution.py
+++ 1710/solution.py
@@ -1,21 +1,15 @@
-BOX_COUNT_INDEX = 0
-
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-        def maximize(remaining_box_slots, loaded_units):
-            nonlocal maximum_units
-            if remaining_box_slots < 0:
-                return
-            maximum_units = max(maximum_units, loaded_units)
+        boxTypes.sort(key=itemgetter(1), reverse=True)
+        units = 0
+        i = 0
+        n = len(boxTypes)
+        while truckSize > 0 and i < n:
+            if boxTypes[i][0] == 0:
+                i += 1
+                continue
+            units += boxTypes[i][1]
+            boxTypes[i][0] -= 1
+            truckSize -= 1
+        return units
 
-            for i, (box_count, current_box_units) in enumerate(boxTypes):
-                if box_count == 0:
-                    continue
-                boxTypes[i][BOX_COUNT_INDEX] -= 1
-                maximize(remaining_box_slots - 1, loaded_units + current_box_units)
-                boxTypes[i][BOX_COUNT_INDEX] += 1
-
-        maximum_units = -math.inf
-        initial_units_loaded = 0
-        maximize(truckSize, initial_units_loaded)
-        return maximum_units
