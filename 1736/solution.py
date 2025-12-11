9b77877ab9bbd1b887fd0b5fabcfaf4820d9b9a9
Aiden Cullo <aidencullo@gmail.com>
2025-12-11 08:30:32 -0500

    chatgpt soln

--- 1736/solution.py
+++ 1736/solution.py
@@ -1,28 +1,8 @@
 class Solution:
-    def maximumTime(self, time: str) -> str:
-        initial_h1 = time[0]
-        initial_h2 = time[1]
-        initial_m1 = time[3]
-        initial_m2 = time[4]
-        question_mark = '?'
-
-        if initial_h1 == question_mark:
-            if initial_h2 != question_mark and int(initial_h2) > 3:
-                h1 = 1
-            else:
-                h1 = 2
-        else:
-            h1 = int(initial_h1)
-            
-        if initial_h2 == question_mark:
-            if h1 == 2:
-                h2 = 3
-            else:
-                h2 = 9
-        else:
-            h2 = int(initial_h2)
-
-        m1 = 5 if initial_m1 == '?' else initial_m1
-        m2 = 9 if initial_m2 == '?' else initial_m2
-
+    def maximumTime(self, t: str) -> str:
+        h1, h2, _, m1, m2 = t
+        h1 = '2' if h1 == '?' and (h2 == '?' or h2 <= '3') else ('1' if h1 == '?' else h1)
+        h2 = '3' if h2 == '?' and h1 == '2' else ('9' if h2 == '?' else h2)
+        m1 = '5' if m1 == '?' else m1
+        m2 = '9' if m2 == '?' else m2
         return f"{h1}{h2}:{m1}{m2}"
