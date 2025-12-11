1a03299127322461c12304c65872f7ae4c5efaee
Aiden Cullo <aidencullo@gmail.com>
2025-12-11 10:30:17 -0500

    1827 n time 1 space -- more elegant

--- 1827/solution.py
+++ 1827/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        n = len(nums)
         ops = 0
-        for i in range(n - 1):
-            if nums[i] >= nums[i + 1]:
-                ops += nums[i] - nums[i + 1] + 1
-                nums[i + 1] = nums[i] + 1
+        prev = -math.inf
+
+        for curr in nums:
+            ops += max(0, prev - curr + 1)
+            prev = max(prev + 1, curr)
+
         return ops
