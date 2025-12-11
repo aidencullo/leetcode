159aac1e6be58685377a2b28801ea10d9e42671f
Aiden Cullo <aidencullo@gmail.com>
2025-12-11 08:34:19 -0500

    don't reassign variables'

--- 1736/solution.py
+++ 1736/solution.py
@@ -1,8 +1,10 @@
 class Solution:
     def maximumTime(self, t: str) -> str:
-        h1, h2, _, m1, m2 = t
-        h1 = '2' if h1 == '?' and (h2 == '?' or h2 <= '3') else ('1' if h1 == '?' else h1)
-        h2 = '3' if h2 == '?' and h1 == '2' else ('9' if h2 == '?' else h2)
-        m1 = '5' if m1 == '?' else m1
-        m2 = '9' if m2 == '?' else m2
-        return f"{h1}{h2}:{m1}{m2}"
+        h1_orig, h2_orig, _, m1_orig, m2_orig = t
+        
+        h1_computed = '2' if h1_orig == '?' and (h2_orig == '?' or h2_orig <= '3') else ('1' if h1_orig == '?' else h1_orig)
+        h2_computed = '3' if h2_orig == '?' and h1_computed == '2' else ('9' if h2_orig == '?' else h2_orig)
+        m1_computed = '5' if m1_orig == '?' else m1_orig
+        m2_computed = '9' if m2_orig == '?' else m2_orig
+        
+        return f"{h1_computed}{h2_computed}:{m1_computed}{m2_computed}"
