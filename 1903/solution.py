ed833500b5cf8381164c13e90a1f4af91ea8b798
Aiden Cullo <aidencullo@gmail.com>
2025-12-11 12:35:18 -0500

    n time 1 space

--- 1903/solution.py
+++ 1903/solution.py
@@ -1,12 +1,6 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
-        def gen_substrings(s):
-            return [s[i:j] for i in range(len(s)) for j in range(i+1, len(s)+1)]
-        
-        all_substrings = gen_substrings(num)
-        largest = -math.inf
-        for substring in all_substrings:
-            x = int(substring)
-            if x % 2 == 1:
-                largest = max(largest, x)
-        return "" if largest == -math.inf else str(largest)
+        for i in range(len(num) - 1, -1, -1):
+            if int(num[i]) % 2 == 1:
+                return num[:i + 1]
+        return ""
