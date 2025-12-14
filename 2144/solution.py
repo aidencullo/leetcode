cefba4e99f8b01653dc3ab502d010ecf20a287e0
Aiden Cullo <aidencullo@gmail.com>
2025-12-14 15:28:36 -0500

    pass start value to sum

--- 2144/solution.py
+++ 2144/solution.py
@@ -1,3 +1,4 @@
 class Solution:
     def minimumCost(self, cost: List[int]) -> int:
-        cost.sort(reverse=True); return sum(sum(cost[i:i+2]) for i in range(0, len(cost), 3))
+        cost.sort(reverse=True);
+        return sum((sum(cost[i:i+2]) for i in range(0, len(cost), 3)), 0);
