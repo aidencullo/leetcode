07d0fbf815e110a6bc5e11f44033cdb3f29016fa
Aiden Cullo <aidencullo@gmail.com>
2025-12-15 08:52:57 -0500

    367 accepted sqrt(n) time

--- 367/solution.py
+++ 367/solution.py
@@ -1,6 +1,7 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        for x in range(num):
+        for x in range(1, num + 1):
+            if x * x > num:
+                return False
             if num == x * x:
                 return True
-        return False
