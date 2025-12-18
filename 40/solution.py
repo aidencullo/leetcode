bd56f44c658965d504709c21165859cd3394dada
Aiden Cullo <aidencullo@gmail.com>
2025-12-18 08:45:40 -0500

    try to prematurely exit, but still got tle

--- 40/solution.py
+++ 40/solution.py
@@ -12,10 +12,17 @@ class Solution:
             if combination_sum > target:
                 return
 
+            possible_remaining_sum = suffix_sum[i]
+            
+            if combination_sum + possible_remaining_sum < target:
+                return
+
             backtrack(i + 1, combination + [candidates[i]])
             backtrack(i + 1, combination)
 
+
         candidates.sort()
+        suffix_sum = list(reversed(list(accumulate(reversed(candidates)))))
         combinations = set()
         backtrack(0, [])
         return [list(t) for t in combinations]
