c005fcca2ff723eb436cad428223bec5a241c4ed
Aiden Cullo <aidencullo@gmail.com>
2025-12-18 11:00:58 -0500

    first try -- fails on test cases

--- 15/solution.py
+++ 15/solution.py
@@ -1,3 +1,10 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        pass
+        res = []
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                for k in range(j + 1, n):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        res.append([nums[i], nums[j], nums[k]])
+        return res
