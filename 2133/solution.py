77da505a7308e5586b2dfea78cae37ac7b9abc92
Aiden Cullo <aidencullo@gmail.com>
2025-12-22 23:12:48 -0500

    auto-pushed changes

--- 2133/solution.py
+++ 2133/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
         def valid(mat):
-            return any(set(row) != one_to_n for row in mat)
+            return not any(set(row) != one_to_n for row in mat)
 
         one_to_n = set(range(1, len(matrix) + 1))
         return valid(matrix) and valid(zip(*matrix))
