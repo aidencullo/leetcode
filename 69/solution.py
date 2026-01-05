6d63837870ae396f2d6b55ddaa2974203f43f917
Aiden Cullo <aidencullo@gmail.com>
2026-01-05 08:57:02 -0500

    Add skeleton for problem 69

--- 69/solution.py
+++ 69/solution.py
@@ -1,14 +1,11 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        l = 0
-        r = x
-        while l <= r:
-            k = (l + r) // 2
-            target = k * k
-            if target < x:
-                l = k + 1
-            elif target > x:
-                r = k - 1
+        sqrt = 0
+        
+        for i in range(x + 1):
+            if i * i <= x:
+                sqrt = i
             else:
-                return k
-        return r
+                break
+
+        return sqrt
