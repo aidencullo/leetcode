f700d77456df6fecf1ea63b605e682476133ceda
Aiden Cullo <aidencullo@gmail.com>
2026-01-05 08:53:58 -0500

    Update solution for problem 35

--- 35/solution.py
+++ 35/solution.py
@@ -2,4 +2,15 @@ from typing import List
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        return bisect.bisect_left(nums, target)
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            k = (r + l) // 2
+            el = nums[k]
+            
+            if target <= el:
+                r = k - 1
+            else:
+                l = k + 1
+            
+        return l
