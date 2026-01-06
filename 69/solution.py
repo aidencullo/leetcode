0c7ac534372766f4311ed6ad9c302b4a762c995d
Aiden Cullo <aidencullo@gmail.com>
2026-01-06 08:20:46 -0500

    lgn

--- 69/solution.py
+++ 69/solution.py
@@ -1,13 +1,6 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        """
-        LeetCode 69: Sqrt(x)
-        Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
-        """
-        largest_sqrt = 0
-        
-        for y in range(0, x + 1):
-            if y ** 2 <= x:
-                largest_sqrt = y
-
-        return largest_sqrt
+        i = bisect.bisect_left(range(x + 1), x, key=lambda y: y ** 2)
+        if i ** 2 != x:
+            return i - 1
+        return i
