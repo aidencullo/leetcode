5117e11ad68ef4c6dd677557ce9fec25bef55c13
Aiden Cullo <aidencullo@gmail.com>
2026-01-06 08:52:02 -0500

    LeetCode 504: Base 7 skeleton

---
+++ 504/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        """
+        LeetCode 504: Base 7
+        Given an integer num, convert it to a base 7 string.
+        """
+        pass
