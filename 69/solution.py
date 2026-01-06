78e615e2453e5cd233e451a021cf03776734c8e9
Aiden Cullo <aidencullo@gmail.com>
2026-01-06 08:29:31 -0500

    clever manipulation to avoid selection statement

--- 69/solution.py
+++ 69/solution.py
@@ -1,6 +1,4 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        i = bisect.bisect_left(range(x + 1), x, key=lambda y: y ** 2)
-        if i ** 2 != x:
-            return i - 1
-        return i
+        i = bisect.bisect_left(range(x + 1), x + 1, key=lambda y: y ** 2)
+        return i - 1
