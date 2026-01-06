99b41e4329a60afe2b955ff473e2959f051163a0
Aiden Cullo <aidencullo@gmail.com>
2026-01-06 08:49:37 -0500

    manual

--- 69/solution.py
+++ 69/solution.py
@@ -1,4 +1,12 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        i = bisect.bisect_left(range(x + 1), x + 1, key=lambda y: y ** 2)
-        return i - 1
+        l, r = 0, x
+        while l <= r:
+            k = (l + r) // 2
+            y = k ** 2
+
+            if x >= y:
+                l = k + 1
+            else:
+                r = k - 1
+        return r
