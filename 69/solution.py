f6a7832f2ec7a2b4acac9d5d9f4c43c1f1cf0e9c
Aiden Cullo <aidencullo@gmail.com>
2026-01-06 08:50:52 -0500

    solution

--- 69/solution.py
+++ 69/solution.py
@@ -1,12 +1,3 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        l, r = 0, x
-        while l <= r:
-            k = (l + r) // 2
-            y = k ** 2
-
-            if x >= y:
-                l = k + 1
-            else:
-                r = k - 1
-        return r
+        return int(math.sqrt(x))
