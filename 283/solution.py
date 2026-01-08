63e9b22b0e28246b2fd598952217c6d2b7aaca8a
aidencullo <aidencullo@gmail.com>
2026-01-08 08:34:31 -0500

    two pointers, linear solution

--- 283/solution.py
+++ 283/solution.py
@@ -3,4 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+        l, r = 0, 0
+        n = len(nums)
+
+        while r < n:
+            while r < n and nums[r] == 0:
+                r += 1
+            if r == n:
+                break
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r += 1
