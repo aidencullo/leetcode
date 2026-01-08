c9812d959437bb1c3984fe81625d4cc829c73a20
aidencullo <aidencullo@gmail.com>
2026-01-08 08:00:16 -0500

    Add LeetCode 507 solution

---
+++ 507/solution.py
@@ -0,0 +1,12 @@
+"""
+LeetCode Problem 507: Perfect Number
+
+https://leetcode.com/problems/perfect-number/
+
+A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
+"""
+
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        divisors = [i for i in range(1, num) if num % i == 0]
+        return sum(divisors) == num
