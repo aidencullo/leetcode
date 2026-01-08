49e6aa3a6b7e2d92bfc3f45ca42eccc28e1caaa3
aidencullo <aidencullo@gmail.com>
2026-01-08 08:43:24 -0500

    Add LeetCode 414 boilerplate

--- 414/solution.py
+++ 414/solution.py
@@ -1,5 +1,3 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        nums = list(set(nums))
-        max_three = heapq.nlargest(3, nums)
-        return max_three[2] if len(max_three) == 3 else max_three[0]
+        pass
