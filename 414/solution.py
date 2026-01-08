c6dcce02c89bb436a20e96c021e14f303ef8d448
aidencullo <aidencullo@gmail.com>
2026-01-08 08:49:41 -0500

    faster using heap

--- 414/solution.py
+++ 414/solution.py
@@ -1,3 +1,6 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        pass
+        unique_nums = list(set(nums))
+        heapq.heapify(unique_nums)
+        three_max = heapq.nlargest(3, unique_nums)
+        return three_max[2] if len(three_max) == 3 else three_max[0]
