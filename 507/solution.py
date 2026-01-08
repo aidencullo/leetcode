54cc83aa8f269cf0a1102c045f427ec712eb201f
aidencullo <aidencullo@gmail.com>
2026-01-08 08:10:50 -0500

    sqrt n time complexity, n space?

--- 507/solution.py
+++ 507/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
         divisors = set()
-        for i in range(2, math.isqrt(num)):
+        for i in range(1, math.isqrt(num) + 1):
             if num % i == 0:
                 divisors.add(i)
                 divisors.add(num // i)
+        divisors.discard(num)
         return sum(divisors) == num
