41534b1aeaa4e625cd8dee9540322a5361d0c547
aidencullo <aidencullo@gmail.com>
2026-01-08 09:01:30 -0500

    save progress, unsubmitted

--- 892/solution.py
+++ 892/solution.py
@@ -1,3 +1,17 @@
 class Solution:
-    def solve(self, args):
-        pass
+    def surfaceArea(self, grid: List[List[int]]) -> int:
+        def surface_between(p1, p2):
+            if outside_grid(p2):
+                return grid[p1[0], p1[1]]
+            return 0 if grid[p1[0], p1[1]] < grid[p2[0], p2[1]] else grid[p2[0], p2[1]]
+        
+        def calculate(r, c):
+            surface_area = 0
+            surface_area += surface_between((r, c), (r + 1, c))
+            surface_area += surface_between((r, c), (r - 1, c))
+            surface_area += surface_between((r, c), (r, c + 1))
+            surface_area += surface_between((r, c), (r, c - 1))
+            return surface_area
+        
+        return sum(calculate(r, c) for r, c in product(range(len(grid)), range(len(grid[0]))))
+            
