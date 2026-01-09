51cf88e4dae866a30a09bdada2dc064179f00792
aidencullo <aidencullo@gmail.com>
2026-01-08 22:16:11 -0500

    still working out this soution, but I think it's viable...

--- 892/solution.py
+++ 892/solution.py
@@ -1,5 +1,12 @@
 class Solution:
     def surfaceArea(self, grid: List[List[int]]) -> int:
+        def inside_grid(pair):
+            return (0 <= pair[0] < len(grid)
+                    and 0 <= pair[1] < len(grid[0]))
+
+        def outside_grid(pair):
+            return not inside_grid(pair)
+        
         def surface_between(p1, p2):
             if outside_grid(p2):
                 return grid[p1[0], p1[1]]
@@ -13,5 +20,5 @@ class Solution:
             surface_area += surface_between((r, c), (r, c - 1))
             return surface_area
         
-        return sum(calculate(r, c) for r, c in product(range(len(grid)), range(len(grid[0]))))
+        return sum(print(r) for r, c in product(range(len(grid)), range(len(grid[0]))))
             
