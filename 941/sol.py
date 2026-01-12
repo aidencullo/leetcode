b9d84196afbecb8d739c34daa2576e3d6c38efd3
aidencullo <aidencullo@gmail.com>
2026-01-12 08:59:56 -0500

    optimize validMountainArray solution

--- 941/sol.py
+++ 941/sol.py
@@ -1,16 +1,19 @@
-"""
-LeetCode Problem 941: Valid Mountain Array
-
-https://leetcode.com/problems/valid-mountain-array/
-
-Given an array of integers arr, return true if and only if it is a valid mountain array.
-An array arr is a mountain array if and only if:
-- arr.length >= 3
-- There exists some i with 0 < i < arr.length - 1 such that:
-  - arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
-  - arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
-"""
-
 class Solution:
-    def validMountainArray(self, arr: list[int]) -> bool:
-        pass
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr) < 3:
+            return False
+        
+        # Climb up
+        i = 0
+        while i + 1 < len(arr) and arr[i] < arr[i + 1]:
+            i += 1
+        
+        # Peak can't be first or last
+        if i == 0 or i == len(arr) - 1:
+            return False
+        
+        # Climb down
+        while i + 1 < len(arr) and arr[i] > arr[i + 1]:
+            i += 1
+        
+        return i == len(arr) - 1
