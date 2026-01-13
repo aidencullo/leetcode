a563cb34aef5062b0d9a44b2e02e97ea8d5d98e8
aiden cullo <aidencullo@gmail.com>
2026-01-13 08:52:37 -0500

    Remove comments from solution.py

--- 941/solution.py
+++ 941/solution.py
@@ -1,30 +1,48 @@
 class Solution:
-    def validMountainArray(self, arr: List[int]) -> bool:
-        if len(arr) < 3: # 1
-            return False
-
-        peak = max(arr) # n 
-        peak_idx = arr.index(peak) # n
-
-        if peak_idx == 0 or peak_idx == len(arr) - 1: # 1
-            return False
-
-        ascent = arr[:peak_idx + 1] # n
-        descent = arr[peak_idx:] # n
+     def validMountainArray(self, arr: List[int]) -> bool:
+         if len(arr) < 3:
+             return False
+ 
+         peak = max(arr)
+         peak_idx = arr.index(peak)
+ 
+         if peak_idx == 0 or peak_idx == len(arr) - 1:
+             return False
+ 
+         ascent = arr[:peak_idx + 1]
+         descent = arr[peak_idx:]
 
         def is_unique(lst):
-            return len(set(lst)) == len(lst) # n
+            return len(set(lst)) == len(lst)
 
-        if not is_unique(descent): # n
+        if not is_unique(descent):
+            return False
+        
+        if not is_unique(ascent):
             return False
 
-        if not is_unique(ascent): # n
+        def is_ascending(lst):
+            prev = -math.inf
+            
+            for x in lst:
+                if x <= prev:
+                    return False
+                prev = x
+            return True
+
+        def is_descending(lst):
+            prev = math.inf
+            
+            for x in lst:
+                if x >= prev:
+                    return False
+                prev = x
+            return True
+
+        if not is_ascending(ascent):
             return False
 
-        if sorted(ascent) != ascent: # nlgn
-            return False
-
-        if sorted(descent, reverse=True) != descent: # nlgn
+        if not is_descending(descent):
             return False
 
         return True
