c2b3c8f085fe5024d770e7c060e64894aa99ed77
aiden cullo <aidencullo@gmail.com>
2026-01-13 08:55:34 -0500

    Fix indentation in solution.py

--- 941/solution.py
+++ 941/solution.py
@@ -1,29 +1,28 @@
 class Solution:
-     def validMountainArray(self, arr: List[int]) -> bool:
-         if len(arr) < 3:
-             return False
- 
-         peak = max(arr)
-         peak_idx = arr.index(peak)
- 
-         if peak_idx == 0 or peak_idx == len(arr) - 1:
-             return False
- 
-         ascent = arr[:peak_idx + 1]
-         descent = arr[peak_idx:]
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr) < 3:
+            return False
+
+        peak = max(arr)
+        peak_idx = arr.index(peak)
+
+        if peak_idx == 0 or peak_idx == len(arr) - 1:
+            return False
+
+        ascent = arr[:peak_idx + 1]
+        descent = arr[peak_idx:]
 
         def is_unique(lst):
             return len(set(lst)) == len(lst)
 
         if not is_unique(descent):
             return False
-        
+
         if not is_unique(ascent):
             return False
 
         def is_ascending(lst):
             prev = -math.inf
-            
             for x in lst:
                 if x <= prev:
                     return False
@@ -32,7 +31,6 @@ class Solution:
 
         def is_descending(lst):
             prev = math.inf
-            
             for x in lst:
                 if x >= prev:
                     return False
