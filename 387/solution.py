4dfbd923523ded5091b2775462b5067efd94ef2e
aiden cullo <aidencullo@gmail.com>
2026-01-15 11:20:02 -0500

    chore: add leetcode 387 scaffold

--- 387/solution.py
+++ 387/solution.py
@@ -1,10 +1,20 @@
-from collections import Counter
-
-
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        counts = Counter(s)
+        seen = set()
+        seen_more = set()
+
+        for c in s:
+            if c in seen:
+                seen_more.add(c)
+            seen.add(c)
+
+        all_chars = set(s)
+            
+        for c in seen_more:
+            all_chars.discard(c)
+
         for i, c in enumerate(s):
-            if counts[c] == 1:
+            if c in all_chars:
                 return i
         return -1
+        
