9c91fa949337dcb7b574ac4dff265b96106ad8f5
aiden cullo <aidencullo@gmail.com>
2026-01-15 18:16:38 -0500

    chore: update 2529 solution

--- 2529/solution.py
+++ 2529/solution.py
@@ -1,5 +1,5 @@
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
-        neg = [x for x in nums if x < 0]
-        pos = [x for x in nums if x > 0]
+        neg = list(filter(lambda x: x < 0, nums))
+        pos = list(filter(lambda x: x > 0, nums))
         return max(len(neg), len(pos))
