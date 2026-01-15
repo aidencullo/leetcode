9abcc3bc7818326ded17478cdaccac0bda6936c4
aiden cullo <aidencullo@gmail.com>
2026-01-15 11:47:25 -0500

    refactor: update solution

--- 387/solution.py
+++ 387/solution.py
@@ -2,7 +2,7 @@ class Solution:
     def firstUniqChar(self, s: str) -> int:
         counter = Counter(s)
         for i, c in enumerate(s):
-            if c in unique:
+            if counter[c] == 1:
                 return i
         return -1
         
