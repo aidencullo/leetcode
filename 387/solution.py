9bfaf220b7f535771edc6c83d43e0f7f4fbc954c
aiden cullo <aidencullo@gmail.com>
2026-01-15 11:29:51 -0500

    refactor: streamline unique char tracking

--- 387/solution.py
+++ 387/solution.py
@@ -1,18 +1,18 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         seen = set()
-        seen_more = set()
+        uniq = set()
+        chars = set(s)
+
+        uniq |= chars
 
         for c in s:
             if c in seen:
-                seen_more.add(c)
+                uniq.discard(c)
             seen.add(c)
 
-        all_chars = set(s)
-        all_chars -= seen_more
-
         for i, c in enumerate(s):
-            if c in all_chars:
+            if c in uniq:
                 return i
         return -1
         
