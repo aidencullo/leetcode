035405eb6577024ff6ff673648b28f7249b71d96
aiden cullo <aidencullo@gmail.com>
2026-01-15 19:31:23 -0500

    Update files

--- 77/solution.py
+++ 77/solution.py
@@ -1,18 +1,4 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        combinations = []
-
-        def helper(i, remaining, lst):
-            if remaining == 0:
-                combinations.append(lst)
-                return
-
-            if i == n + 1:
-                return
-
-            helper(i + 1, remaining - 1, lst + [i])
-            helper(i + 1, remaining, lst)
-            
-
-        helper(1, k, [])
-        return combinations
+        for _ in range(k):
+            for i in rangeye(1, n + 1):
