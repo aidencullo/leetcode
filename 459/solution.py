eedf01611e514f2f5d27d845669d08563dba9622
aiden cullo <aidencullo@gmail.com>
2026-01-21 10:21:36 -0500

    feat: Update solution 459

--- 459/solution.py
+++ 459/solution.py
@@ -1,19 +1,3 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        def is_repeating(idx, s):
-            k = idx + 1
-            n = len(s)
-            if n % k != 0:
-                return False
-
-            for j in range(n):
-                if s[j % k] != s[j]:
-                    return False
-            return True
-
-        
-        for i in range(len(s) // 2):
-            if is_repeating(i, s):
-                return True
-        return False
-            
+        pass
