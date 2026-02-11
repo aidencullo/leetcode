c67a1f9339c54cae45bd8ba6e1dbc670f9753290
aiden cullo <aidencullo@gmail.com>
2026-02-11 12:59:28 -0500

    feat: Solve problem 2833 (Furthest Point From Origin)

--- 2833/solution.py
+++ 2833/solution.py
@@ -1,3 +1,21 @@
 class Solution:
     def furthestDistanceFromOrigin(self, moves: str) -> int:
-        pass
+        m = -math.inf
+        left = 0
+        right = 0
+
+        for c in moves:
+            if c == 'L' or c == '-':
+                left += 1
+            else:
+                left -= 1
+            m = max(m, left)
+
+        for c in moves:
+            if c == 'R' or c == '-':
+                right += 1
+            else:
+                right -= 1
+            m = max(m, right)
+
+        return m
