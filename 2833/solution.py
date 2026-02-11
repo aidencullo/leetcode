e3d9d2777b06f076c5f1225a3e263897fdcc3991
aiden cullo <aidencullo@gmail.com>
2026-02-11 13:52:16 -0500

    refactor: Simplify problem 2833 solution with counting approach

--- 2833/solution.py
+++ 2833/solution.py
@@ -1,21 +1,6 @@
 class Solution:
-    def furthestDistanceFromOrigin(self, moves: str) -> int:
-        m = -math.inf
-        left = 0
-        right = 0
-
-        for c in moves:
-            if c == 'L' or c == '-':
-                left += 1
-            else:
-                left -= 1
-            m = max(m, left)
-
-        for c in moves:
-            if c == 'R' or c == '-':
-                right += 1
-            else:
-                right -= 1
-            m = max(m, right)
-
-        return m
+    def furthestDistanceFromOrigin(self, moves: str) -> int: 
+        left = moves.count('L') 
+        right = moves.count('R')
+        underscore = moves.count('_')
+        return max(left, right) - min(left, right) + underscore
