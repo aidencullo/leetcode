33af5e4b8a13705069d19ab7fd4f8f2496d14ae3
aiden cullo <aidencullo@gmail.com>
2026-02-18 12:56:08 -0500

    Solve 2540: Minimum Common Value

--- 2540/solution.py
+++ 2540/solution.py
@@ -1,6 +1,4 @@
-from typing import List
-
-
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-
+        common_values = set(nums1) & set(nums2)
+        return min(common_values) if common
