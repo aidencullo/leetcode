c43bd69013cb099a281716e8e6e4fcbc89be505e
aiden cullo <aidencullo@gmail.com>
2026-02-18 09:53:09 -0500

    feat: Solve problem 859 (Buddy Strings)

--- 859/solution.py
+++ 859/solution.py
@@ -1,3 +1,21 @@
 class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
-        pass
+        c1 = Counter(s)
+        c2 = Counter(goal)
+
+        if c1 != c2:
+            return False
+
+        differences = sum(1 for a, b in zip(s, goal) if a != b)
+
+        if differences > 2:
+            return False
+
+        max_occurrences = max(c1.values())
+
+        if differences == 0 and max_occurrences < 2:
+            return False
+
+        return True
+
+        
