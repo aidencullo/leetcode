b58cb28566a83a32f455b3073e9b540b572d0b83
aiden cullo <aidencullo@gmail.com>
2026-02-18 12:52:48 -0500

    Add stub for 2511: Maximum Enemy Forts That Can Be Captured

--- 2511/solution.py
+++ 2511/solution.py
@@ -1,50 +1,6 @@
+from typing import List
+
+
 class Solution:
-    def captureForts(self, forts: list[int]) -> int:
-        friendly = False
-        cnt = 0
-        captured = 0
-        i = 0
-        n = len(forts)
+    def maximumEnemyForts(self, forts: List[int]) -> int:
 
-        while i < n:
-
-            while i < n and forts[i] != 1:
-                i += 1
-            if i == n:
-                break
-
-            i += 1
-
-            cnt = 0
-            while i < n and forts[i] == 0:
-                i += 1
-                cnt += 1
-            if i == n:
-                break
-
-            if forts[i] == -1:
-                captured = max(captured, cnt)
-
-
-        forts = list(reversed(forts))
-        i = 0
-        while i < n:
-
-            while i < n and forts[i] != 1:
-                i += 1
-            if i == n:
-                break
-
-            i += 1
-
-            cnt = 0
-            while i < n and forts[i] == 0:
-                i += 1
-                cnt += 1
-            if i == n:
-                break
-
-            if forts[i] == -1:
-                captured = max(captured, cnt)
-
-        return captured
