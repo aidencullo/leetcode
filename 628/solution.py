fa798a7bbf3964172479b9f67a1d00e6c90ba78b
aiden cullo <aidencullo@gmail.com>
2026-02-18 14:50:32 -0500

    Add stub for 628 Maximum Product of Three Numbers

--- 628/solution.py
+++ 628/solution.py
@@ -1,7 +1,6 @@
+from typing import List
+
+
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
-        nums.sort()
-        maximum_product = prod(nums[-3:])
-        maximum_product = max(prod(nums[-2:]) * prod[-1])
-        return maximum_product
-        
+        pass
