61ee333c1e39361436aa54895b003f2f364ce635
aiden cullo <aidencullo@gmail.com>
2026-02-18 09:40:00 -0500

    feat: Solve problem 884 (Uncommon Words from Two Sentences)

--- 884/solution.py
+++ 884/solution.py
@@ -2,16 +2,6 @@ class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
         c1 = Counter(s1.split())
         c2 = Counter(s2.split())
-        res = []
-
-        for word in c1:
-            if c1.get(word) == 1 and word not in c2:
-                res.append(word)
-
-
-        for word in c2:
-            if c2.get(word) == 1 and word not in c1:
-                res.append(word)
-
-        return res
+        c3 = c1 + c2
+        return [k for k, v in c3.items() if v == 1]
 
