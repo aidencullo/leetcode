d8af3637c15caf160a818777e3c290f05cd7ad74
aiden cullo <aidencullo@gmail.com>
2026-02-17 21:49:06 -0500

    feat: Solve problem 884 (Uncommon Words from Two Sentences)

--- 884/solution.py
+++ 884/solution.py
@@ -1,8 +1,3 @@
-from collections import Counter
-from typing import List
-
-
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        counts = Counter((s1 + " " + s2).split())
-        return [word for word, freq in counts.items() if freq == 1]
+        pass
