517596716acd916454b2c9b9b7fa7686485e7eec
aiden cullo <aidencullo@gmail.com>
2026-02-18 10:03:38 -0500

    feat: Solve problem 1122 (Relative Sort Array)

--- 1122/solution.py
+++ 1122/solution.py
@@ -3,4 +3,22 @@ from typing import List
 
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        pass
+        from functools import cmp_to_key
+
+        def compare(a, b):
+            # return negative if a < b, zero if equal, positive if a > b
+
+            if a not in arr2 and b not in arr2:
+                return a - b
+
+            if a not in arr2:
+                return 1
+
+            if b not in arr2:
+                return -1
+
+            return arr2.index(a) - arr2.index(b)
+
+        return sorted(arr1, key=cmp_to_key(compare))
+
+
