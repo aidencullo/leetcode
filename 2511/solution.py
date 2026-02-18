37cd639a076ec83f23b0b25c1ba53a0f311d5493
aiden cullo <aidencullo@gmail.com>
2026-02-18 11:31:07 -0500

    feat: Problem 2511 incorrect attempt

--- 2511/solution.py
+++ 2511/solution.py
@@ -1,3 +1,46 @@
 class Solution:
     def captureForts(self, forts: list[int]) -> int:
+        friendly = False
+        cnt = 0
+        captured = 0
+        i = 0
+        n = len(forts)
 
+        while i < n:
+
+            while i < n and forts[i] != 1:
+                i += 1
+            if i == n:
+                break
+
+            i += 1
+
+            cnt = 0
+            while i < n and forts[i] == 0:
+                i += 1
+                cnt += 1
+            if i == n:
+                break
+            captured = max(captured, cnt)
+
+
+        forts = list(reversed(forts))
+        i = 0
+        while i < n:
+
+            while i < n and forts[i] != 1:
+                i += 1
+            if i == n:
+                break
+
+            i += 1
+
+            cnt = 0
+            while i < n and forts[i] == 0:
+                i += 1
+                cnt += 1
+            if i == n:
+                break
+            captured = max(captured, cnt)
+
+        return captured
