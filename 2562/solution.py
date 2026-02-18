243911d6736849f6ba51d17313396b37204c294f
aiden cullo <aidencullo@gmail.com>
2026-02-18 13:59:03 -0500

    Solve 2562: Find the Array Concatenation Value

--- 2562/solution.py
+++ 2562/solution.py
@@ -3,4 +3,19 @@ from typing import List
 
 class Solution:
     def findTheArrayConcVal(self, nums: List[int]) -> int:
-        pass
+        def concat_value(x, y):
+            return int(str(x) + str(y))
+            
+        n = len(nums)
+        l, r = 0, n - 1
+        res = 0
+
+        while l < r:
+            res += concat_value(nums[l], nums[r])
+            l += 1
+            r -= 1
+
+        if l == r:
+            res += nums[l]
+
+        return res
