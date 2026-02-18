0ae801958366e73f0ae6a06493387f355023fa2a
aiden cullo <aidencullo@gmail.com>
2026-02-18 09:43:06 -0500

    feat: Solve problem 859 (Buddy Strings)

--- 859/solution.py
+++ 859/solution.py
@@ -1,18 +1,3 @@
 class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
-        from collections import Counter
-        if len(s) != len(goal):
-            return False
-        diffs = []
-        for i in range(len(s)):
-            if s[i] != goal[i]:
-                diffs.append(i)
-        count = len(diffs)
-        if count == 0:
-            if max(Counter(s).values()) > 1:
-                return True
-            return False
-        if count == 2:
-            return s[diffs[0]] == goal[diffs[1]] and s[diffs[1]] == goal[diffs[0]]
-        return False
-        
+        pass
