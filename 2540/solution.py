54950321afa535056dce75611f7c34082cb8afc0
aiden cullo <aidencullo@gmail.com>
2026-02-18 12:57:20 -0500

    Refactor 2540: use early return instead of set intersection

--- 2540/solution.py
+++ 2540/solution.py
@@ -1,4 +1,7 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        common_values = set(nums1) & set(nums2)
-        return min(common_values) if common
+        set1 = set(nums1)
+        for x in nums2:
+            if x in set1:
+                return x
+        return -1
