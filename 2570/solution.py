414c8e3fb34655d9e3eb5d35e69d8f68e25c5c17
aiden cullo <aidencullo@gmail.com>
2026-02-19 13:00:10 -0500

    Refactor 2570: use itertools.chain to simplify loop

--- 2570/solution.py
+++ 2570/solution.py
@@ -1,25 +1,9 @@
 from collections import defaultdict
+from itertools import chain
 
 class Solution:
     def mergeArrays(self, nums1, nums2):
         d = defaultdict(int)
-        for arr in (nums1, nums2):
-            for k, v in arr:
-                d[k] += v
+        for k, v in chain(nums1, nums2):
+            d[k] += v
         return sorted(d.items())
-
-
-"""
-
-uhhhh
-
-lets see line by line
-
-constant
-constant (2) == allocate space for new tuple/iterable
-m+n to iterate
-constant assignment of value
-
-m+n lg m+n
-
-"""
