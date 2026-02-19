88a8e7f781faf0c8fc484367ece0b4f5588e3e67
aiden cullo <aidencullo@gmail.com>
2026-02-19 12:49:23 -0500

    Refactor 2570: simplify list construction

--- 2570/solution.py
+++ 2570/solution.py
@@ -4,5 +4,5 @@ from typing import List
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
         c = Counter(dict(nums1)) + Counter(dict(nums2))
-        lst = [[k, v] for k, v in c.items()]
+        lst = list(c.items())
         return sorted(lst)
