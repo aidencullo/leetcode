366c294f0521ced4e1cb563771e599595dcb287b
aiden cullo <aidencullo@gmail.com>
2026-02-19 12:48:52 -0500

    Solve 2570: Merge Two 2D Arrays by Summing Values

--- 2570/solution.py
+++ 2570/solution.py
@@ -3,4 +3,6 @@ from typing import List
 
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        pass
+        c = Counter(dict(nums1)) + Counter(dict(nums2))
+        lst = [[k, v] for k, v in c.items()]
+        return sorted(lst)
