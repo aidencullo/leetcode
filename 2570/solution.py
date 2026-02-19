4e177cf26ddabfd2a54d523b11c3059dddd95a57
aiden cullo <aidencullo@gmail.com>
2026-02-19 12:59:29 -0500

    Refactor 2570: use defaultdict instead of Counter

--- 2570/solution.py
+++ 2570/solution.py
@@ -1,3 +1,25 @@
+from collections import defaultdict
+
 class Solution:
     def mergeArrays(self, nums1, nums2):
-        return sorted((Counter(dict(nums1)) + Counter(dict(nums2))).items())
+        d = defaultdict(int)
+        for arr in (nums1, nums2):
+            for k, v in arr:
+                d[k] += v
+        return sorted(d.items())
+
+
+"""
+
+uhhhh
+
+lets see line by line
+
+constant
+constant (2) == allocate space for new tuple/iterable
+m+n to iterate
+constant assignment of value
+
+m+n lg m+n
+
+"""
