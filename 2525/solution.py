1053c6a255a0c90267aadccef99feabbceb5fa12
aiden cullo <aidencullo@gmail.com>
2026-02-21 16:17:55 -0500

    Solve 2525. Categorize Box According to Criteria

--- 2525/solution.py
+++ 2525/solution.py
@@ -1,3 +1,11 @@
 class Solution:
     def categorizeBox(self, length: int, width: int, height: int, mass: int) -> str:
-        pass
+        is_bulky = max(length, width, height, mass) >= 10 ** 4 or length * height * width >= 10 ** 9
+        is_heavy = mass >= 100
+        if is_heavy and is_bulky:
+            return "Both"
+        if not is_heavy and not is_bulky:
+            return "Neither"
+        if is_heavy:
+            return "Heavy"
+        return "Bulky"
