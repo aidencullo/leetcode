24072dceb1bc1bb52919161f60c9326f6c4453bd
aiden cullo <aidencullo@gmail.com>
2026-02-21 17:02:20 -0500

    Refactor 2363. Merge Similar Items

--- 2363/solution.py
+++ 2363/solution.py
@@ -1,18 +1,11 @@
 from typing import List
-
+from collections import defaultdict
 
 class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
-        d1 = dict(items1)
-        d2 = dict(items2)
-
         res = defaultdict(int)
 
-        for k, v in d2.items():
+        for k, v in items1 + items2:
             res[k] += v
 
-        for k, v in d1.items():
-            res[k] += v
-
-        return sorted(list(item) for item in res.items())
-
+        return sorted(res.items())
