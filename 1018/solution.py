607846b8027804a578a9be2965d41f102b7d26d2
aiden cullo <aidencullo@gmail.com>
2026-02-21 15:29:48 -0500

    Solve 1018. Binary Prefix Divisible By 5

--- 1018/solution.py
+++ 1018/solution.py
@@ -3,4 +3,12 @@ from typing import List
 
 class Solution:
     def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
-        pass
+        running = 0
+        res = []
+        for i, bit in enumerate(nums):
+            running *= 2
+            running += bit
+            res.append(running % 5 == 0)
+
+        return res
+            
