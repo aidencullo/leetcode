7608ff7944cd4d1182217f3e87b52190ea36be22
aiden cullo <aidencullo@gmail.com>
2026-02-21 13:10:37 -0500

    Solve 1260 - Shift 2D Grid (simplify)

--- 1260/solution.py
+++ 1260/solution.py
@@ -7,6 +7,7 @@ class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         
         def shift_first_row(old):
+            import copy
             new = copy.deepcopy(old)
             from itertools import product
 
@@ -22,17 +23,7 @@ class Solution:
 
 
         def shift(old):
-            new = make_double_list(len(old), len(old[0]))
-            from itertools import product
-
-            rows = len(new)
-            cols = len(new[0])
-            for row, col in product(range(rows), range(cols)):
-                old_row = row
-                old_col = (col - 1) % cols
-                new[row][col] = old[old_row][old_col]
-
-            return new
+            return [row[-1:] + row[:-1] for row in old]
 
         for i in range(k):
             grid = shift(grid)
