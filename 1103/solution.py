18c372087cba5529a2aaed2ee5216017cb9165bc
aiden cullo <aidencullo@gmail.com>
2026-02-21 11:23:18 -0500

    Add stub for 1103 - Distribute Candies to People

--- 1103/solution.py
+++ 1103/solution.py
@@ -1,16 +1,5 @@
 from typing import List
 
-
 class Solution:
     def distributeCandies(self, candies: int, num_people: int) -> List[int]:
-        res = [0] * num_people
-        i = 0
-
-        while candies:
-            print(res)
-            share = min(candies, i + 1)
-            candies -= share
-            res[i % num_people] += share
-            i += 1
-
-        return res
+        pass
