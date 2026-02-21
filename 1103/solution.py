3c970eadd3d1214ba55474a66c40cab2bd1b18c7
aiden cullo <aidencullo@gmail.com>
2026-02-20 20:45:38 -0500

    1103 - attempt (incorrect)

--- 1103/solution.py
+++ 1103/solution.py
@@ -3,4 +3,14 @@ from typing import List
 
 class Solution:
     def distributeCandies(self, candies: int, num_people: int) -> List[int]:
-        pass
+        res = [0] * num_people
+        i = 0
+
+        while candies:
+            share = min(candies, i + 1)
+            candies -= share
+            res[i] += share
+            i = (i + 1) % num_people
+            
+
+        return res
