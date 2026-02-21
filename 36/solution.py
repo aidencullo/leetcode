908e2d2e320cd104eadc5e42b57a364e8a01af6f
aiden cullo <aidencullo@gmail.com>
2026-02-21 17:23:52 -0500

    WIP: problem 36 - validate rows and cols, missing boxes

--- 36/solution.py
+++ 36/solution.py
@@ -2,4 +2,19 @@ from typing import List
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        pass
+
+        def valid_seq(seq):
+            nums = list(filter(str.isdigit, seq))
+            return len(set(nums)) == len(nums)
+
+        for row in board:
+            if not valid_seq(row):
+                return False
+
+        cols = [list(col) for col in zip(*board)]
+        for col in cols:
+            if not valid_seq(col):
+                return False
+            
+        
+        return True
