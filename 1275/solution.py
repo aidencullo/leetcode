62f2b168e54702f8b354440273a02f8fc413bd85
aiden cullo <aidencullo@gmail.com>
2026-02-21 16:13:17 -0500

    Refactor 1275. Find Winner on a Tic Tac Toe Game

--- 1275/solution.py
+++ 1275/solution.py
@@ -1,62 +1,22 @@
 from typing import List
 
-
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-
-        def check_diag(board):
-            if all(board[i][i] == 'X' for i in range(3)):
-                return True
-
-            if all(board[i][i] == 'O' for i in range(3)):
-                return True
-
-            if all(board[2 - i][i] == 'X' for i in range(3)):
-                return True
-
-            if all(board[2 - i][i] == 'O' for i in range(3)):
-                return True
-
-
-        def check_rows(board):
-            if any(all(el == 'X' for el in row) for row in board):
-                return True
-
-            if any(all(el == 'O' for el in row) for row in board):
-                return True
-
-        def transpose(matrix):
-            return [list(col) for col in zip(*matrix)]
-
-        def winner(board):
-            if check_rows(board):
-                return True
-
-            cols_as_rows = transpose(board)
-            if check_rows(cols_as_rows):
-                return True
-
-            if check_diag(board):
-                return True
-
-            return False
-
-        def get_winner():
-            return 'A' if len(moves) % 2 == 1 else 'B'
-
-        def complete():
-            return len(moves) == 9
-            
-        board = [[None] * 3 for i in range(3)]
-
+        board = [[None]*3 for _ in range(3)]
+        
         for i, (r, c) in enumerate(moves):
             board[r][c] = 'X' if i % 2 == 0 else 'O'
-
-        if winner(board):
-            return get_winner()
-
-        if not winner(board) and not complete():
-            return 'Pending'
-        else:
-            return 'Draw'
-            
+        
+        # check rows, columns, diagonals in one pass
+        for i in range(3):
+            if board[i][0] == board[i][1] == board[i][2] != None:
+                return 'A' if board[i][0] == 'X' else 'B'
+            if board[0][i] == board[1][i] == board[2][i] != None:
+                return 'A' if board[0][i] == 'X' else 'B'
+        
+        if board[0][0] == board[1][1] == board[2][2] != None:
+            return 'A' if board[0][0] == 'X' else 'B'
+        if board[0][2] == board[1][1] == board[2][0] != None:
+            return 'A' if board[0][2] == 'X' else 'B'
+        
+        return 'Draw' if len(moves) == 9 else 'Pending'
