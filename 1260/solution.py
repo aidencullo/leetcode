f23d83468ff4834824e638a4646fd138de2cff9f
aiden cullo <aidencullo@gmail.com>
2026-02-21 12:31:21 -0500

    Solve 1260 - Shift 2D Grid (failed attempt)

--- 1260/solution.py
+++ 1260/solution.py
@@ -4,9 +4,9 @@ class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         
         def shift(grid):
-            cols = list(zip(*matrix))
+            cols = [list(c) for c in zip(*grid)]
             last_col = cols[-1]
-            new_first_col = last_col[-1] + last_col[1:]
+            new_first_col = [last_col[-1]] + last_col[1:]
             new_remaining_cols = cols[1:]
             new_cols = new_first_col + new_remaining_cols
             new = [list(r) for r in zip(*cols)]
