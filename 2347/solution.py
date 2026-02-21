96d6400165536541c6d54c5e676166d4be4492b7
aiden cullo <aidencullo@gmail.com>
2026-02-21 16:20:29 -0500

    Solve 2347. Best Poker Hand

--- 2347/solution.py
+++ 2347/solution.py
@@ -3,4 +3,11 @@ from typing import List
 
 class Solution:
     def bestHand(self, ranks: List[int], suits: List[str]) -> str:
-        pass
+        if len(set(suits)) == 1:
+            return "Flush"
+        if len(set(ranks)) >= 3:
+            return "Three of a Kind"
+        if len(set(ranks)) == 2:
+            return "Pair"
+        return "High Card"
+        
