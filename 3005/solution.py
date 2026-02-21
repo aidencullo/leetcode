d1e38156af97d670a9b92bef0e99e745bff0f22b
aiden cullo <aidencullo@gmail.com>
2026-02-21 16:42:48 -0500

    Solve 3005. Count Elements With Maximum Frequency

--- 3005/solution.py
+++ 3005/solution.py
@@ -3,4 +3,6 @@ from typing import List
 
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
-        pass
+        cnt = Counter(nums)
+        max_freq = max(Counter(nums).values())
+        return sum(1 for x in nums if cnt[x] == max_freq)
