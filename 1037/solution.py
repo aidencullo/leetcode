87cb485581956b562097237e6dd4b6e6c57aad3e
aiden cullo <aidencullo@gmail.com>
2026-02-21 13:48:24 -0500

    Solve 1037 - Valid Boomerang (still false, but progress)

--- 1037/solution.py
+++ 1037/solution.py
@@ -1,5 +1,24 @@
-from typing import List
-
 class Solution:
     def isBoomerang(self, points: List[List[int]]) -> bool:
-        pass
+        if len(set(tuple(point) for point in points)) < len(points):
+            return False
+
+        def calculate_slope(p1, p2):
+            x1, y1 = p1
+            x2, y2 = p2
+
+            return (y2 - y1) / (x2 - x1)
+
+        from itertools import pairwise
+
+        p1, p2 = points[0], points[1]
+        slope = calculate_slope(p1, p2)
+
+        for a, b in pairwise(points):
+            if slope != calculate_slope(a, b):
+                return False
+
+        return True
+        
+
+        
