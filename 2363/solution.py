39a25e9c8557c89d6f97a74e1e200254ac1583b3
aiden cullo <aidencullo@gmail.com>
2026-02-21 16:55:41 -0500

    Solve 2363. Merge Similar Items

--- 2363/solution.py
+++ 2363/solution.py
@@ -3,4 +3,16 @@ from typing import List
 
 class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
-        pass
+        d1 = dict(items1)
+        d2 = dict(items2)
+
+        res = defaultdict(int)
+
+        for k, v in d2.items():
+            res[k] += v
+
+        for k, v in d1.items():
+            res[k] += v
+
+        return sorted(list(item) for item in res.items())
+
