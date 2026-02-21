1bb3f79435c1deb28013ff3f57977c4a70bb0bea
aiden cullo <aidencullo@gmail.com>
2026-02-21 16:48:32 -0500

    Solve 1232. Check If It Is a Straight Line

--- 1232/solution.py
+++ 1232/solution.py
@@ -3,4 +3,21 @@ from typing import List
 
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        pass
+        def calc_slope(p1, p2):
+            x1, y1 = p1
+            x2, y2 = p2
+
+            if x1 == x2:
+                return math.inf
+
+            return (y2 - y1) / (x2 - x1)
+
+
+        slope = calc_slope(coordinates[0], coordinates[1])
+
+        for a, b in pairwise(coordinates):
+            if slope != calc_slope(a, b):
+                return False
+
+        return True
+        
