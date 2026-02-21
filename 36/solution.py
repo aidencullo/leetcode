f90d7f7aa5255f8c9cd1f7711bb31f5a7187edb6
aiden cullo <aidencullo@gmail.com>
2026-02-21 17:30:09 -0500

    feat: Solve problem 36 (Valid Sudoku) - add 3x3 box validation

--- 36/solution.py
+++ 36/solution.py
@@ -3,6 +3,11 @@ from typing import List
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
 
+        def valid_mat(submat):
+            seq = [x for row in submat for x in row]
+            nums = list(filter(str.isdigit, seq))
+            return len(set(nums)) == len(nums)
+
         def valid_seq(seq):
             nums = list(filter(str.isdigit, seq))
             return len(set(nums)) == len(nums)
@@ -15,6 +20,12 @@ class Solution:
         for col in cols:
             if not valid_seq(col):
                 return False
+
+        for i in range(3):
+            for j in range(3):
+                submatrix = [row[j * 3: j * 3 + 3] for row in board[i * 3: i * 3 + 3]]
+                if not valid_mat(submatrix):
+                    return False
             
         
         return True
