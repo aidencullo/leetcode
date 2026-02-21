3b36f5c8efb4b7708e7ad36c6430a52506e1c628
aiden cullo <aidencullo@gmail.com>
2026-02-21 12:55:53 -0500

    Solve 1260 - Shift 2D Grid

--- 1260/solution.py
+++ 1260/solution.py
@@ -1,18 +1,41 @@
 from typing import List
 
+def make_double_list(rows, cols):
+    return [[None for _ in range(cols)] for _ in range(rows)]
+
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         
-        def shift(grid):
-            cols = [list(c) for c in zip(*grid)]
-            last_col = cols[-1]
-            new_first_col = [last_col[-1]] + last_col[1:]
-            new_remaining_cols = cols[1:]
-            new_cols = new_first_col + new_remaining_cols
-            new = [list(r) for r in zip(*cols)]
+        def shift_first_row(old):
+            new = copy.deepcopy(old)
+            from itertools import product
+
+            rows = len(new)
+            cols = len(new[0])
+            for row in range(rows):
+                old_row = (row - 1) % rows
+                old_col = 0
+                col = 0
+                new[row][col] = old[old_row][old_col]
+
             return new
-        
+
+
+        def shift(old):
+            new = make_double_list(len(old), len(old[0]))
+            from itertools import product
+
+            rows = len(new)
+            cols = len(new[0])
+            for row, col in product(range(rows), range(cols)):
+                old_row = row
+                old_col = (col - 1) % cols
+                new[row][col] = old[old_row][old_col]
+
+            return new
+
         for i in range(k):
             grid = shift(grid)
+            grid = shift_first_row(grid)
 
         return grid
