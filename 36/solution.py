f0ad6919e75e4131a10bd002530339575c5fe018
aiden cullo <aidencullo@gmail.com>
2026-02-21 18:35:52 -0500

    Problem 36: another attempt

--- 36/solution.py
+++ 36/solution.py
@@ -2,4 +2,31 @@ from typing import List
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        pass
+        boxes = [set() for _ in range(9)]
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+
+                if not val.isdigit():
+                    continue
+
+                box = (r // 3) * 3 + (c // 3)
+
+                if val in cols[c]:
+                    return False
+
+                if val in rows[r]:
+                    return False
+
+                if val in boxes[box]:
+                    return False
+
+            cols[c].add(val)
+            rows[r].add(val)
+            boxes[box].add(val)
+
+
+        return True
