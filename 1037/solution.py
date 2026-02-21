4517b9670d3e081c43063cea1df5aebc49d16294
aiden cullo <aidencullo@gmail.com>
2026-02-21 13:55:06 -0500

    Solve 1037 - Valid Boomerang (correct)

--- 1037/solution.py
+++ 1037/solution.py
@@ -7,6 +7,9 @@ class Solution:
             x1, y1 = p1
             x2, y2 = p2
 
+            if x1 == x2:
+                return math.inf
+
             return (y2 - y1) / (x2 - x1)
 
         def is_straight_line(points):
