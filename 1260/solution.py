bf0c827b58547aca1f21bc6691f1741543571f85
aiden cullo <aidencullo@gmail.com>
2026-02-21 13:39:14 -0500

    Solve 1260 - Shift 2D Grid

--- 1260/solution.py
+++ 1260/solution.py
@@ -1,18 +1,19 @@
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
 
-        def flatten(g):
-            pass
+        def flatten(grid):
+            return [grid[r][c] for r in range(len(grid)) for c in range(len(grid[0]))]
 
-        def shift(g):
-            pass
+        def shift(lst, k):
+            return lst[-k:] + lst[:-k]
 
-        def unflatten(g):
-            pass
+        def unflatten(lst):
+            cols = len(grid[0])
+            return [[lst[r * cols + c] for c in range(cols)] for r in range(len(grid))]
 
-        
+        k %= len(grid) * len(grid[0])
         flattened = flatten(grid)
-        shifted = shift(flatten)
+        shifted = shift(flattened, k)
         unflattened = unflatten(shifted)
         return unflattened
         
