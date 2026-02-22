033a8512da542dc704bbe0c476e9000ad39b6b62
aiden cullo <aidencullo@gmail.com>
2026-02-22 11:33:52 -0500

    Solve 82: Remove Duplicates from Sorted List II

--- 82/solution.py
+++ 82/solution.py
@@ -9,4 +9,21 @@ class ListNode:
 
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        dups = set()
+
+        runner = head
+
+        while runner and runner.next:
+            if runner.val == runner.next.val:
+                dups.add(runner.val)
+            runner = runner.next
+
+        pre_head = ListNode(0, head)
+        runner = pre_head
+
+        while runner and runner.next:
+            while runner.next and runner.next.val in dups:
+                runner.next = runner.next.next
+            runner = runner.next
+
+        return pre_head.next
