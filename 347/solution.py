5b88896ccdaeb3205667f91e768e8c95b390d8e8
aiden cullo <aidencullo@gmail.com>
2026-02-22 15:05:37 -0500

    347 attempt

--- 347/solution.py
+++ 347/solution.py
@@ -1,5 +1,11 @@
 from typing import List
+from collections import Counter
+import heapq
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return [k for k, v in Counter(nums).most_common(k)]
+        count = Counter(nums)  # frequency map
+        # heap of (frequency, num)
+        heap = [(freq, num) for num, freq in count.items()]
+        # get k largest by frequency
+        return [num for freq, num in heapq.nlargest(k, heap)]
