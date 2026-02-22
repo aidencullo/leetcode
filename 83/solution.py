a90581133b7e67f726a38ff5f74b2a949548da44
aiden cullo <aidencullo@gmail.com>
2026-02-22 11:22:11 -0500

    Solve 83: Remove Duplicates from Sorted List (incorrect)

--- 83/solution.py
+++ 83/solution.py
@@ -9,4 +9,11 @@ class ListNode:
 
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        runner = head
+
+        while runner and runner.next:
+            if runner.val == runner.next.val:
+                runner.next = runner.next.next
+            runner = runner.next
+
+        return head
