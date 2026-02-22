8250e4555a0d56eb3c3924d5f3cff1aad35d6b48
aiden cullo <aidencullo@gmail.com>
2026-02-22 15:02:24 -0500

    347 attempt

--- 347/solution.py
+++ 347/solution.py
@@ -2,6 +2,4 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = Counter(nums)
-        d = dict(counter)
-        return [k for k, v in sorted(d.items(), key=lambda x: x[1])[-k:]]
+        return [k for k, v in Counter(nums).most_common(k)]
