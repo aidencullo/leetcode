0ae858bf26b1ab86695bb6f6c5b93c2d70fbbe91
aiden cullo <aidencullo@gmail.com>
2026-02-22 10:33:05 -0500

    Solve 2148: Count Elements With Strictly Smaller and Greater Elements

--- 2148/solution.py
+++ 2148/solution.py
@@ -3,4 +3,8 @@ from typing import List
 
 class Solution:
     def countElements(self, nums: List[int]) -> int:
-        pass
+        if len(set(nums)) == 1:
+            return 0
+        low = min(nums)
+        high = max(nums)
+        return len(nums) - nums.count(low) - nums.count(high)
