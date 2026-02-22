4c16a8810377085704174fab334c9d8e1e0df1b0
aiden cullo <aidencullo@gmail.com>
2026-02-22 14:14:07 -0500

    15 TLE

--- 15/solution.py
+++ 15/solution.py
@@ -1,7 +1,6 @@
-from typing import List
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         arr = nums
         res = []
         for i in range(len(arr)):
@@ -9,4 +8,7 @@ class Solution:
                 for k in range(j + 1, len(arr)):
                     if arr[i] + arr[j] + arr[k] == 0:
                         res.append([arr[i], arr[j], arr[k]])
-        return res
+
+
+                        
+        return [list(t) for t in set(tuple(l) for l in res)]
