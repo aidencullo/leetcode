630656696a13d7c926a291eae1473b2f75d63582
aiden cullo <aidencullo@gmail.com>
2026-02-22 15:16:36 -0500

    152. Maximum Product Subarray - TLE brute force O(n^2)

--- 152/solution.py
+++ 152/solution.py
@@ -1,6 +1,9 @@
-from typing import List
-
-
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        pass
+        product = -math.inf
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                product = max(product, math.prod(nums[i: j + 1]))
+        return product
+        
