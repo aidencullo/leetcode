1ad1565be7ff831020a74a797f4e9a6d2467aed2
aiden cullo <aidencullo@gmail.com>
2026-02-22 10:45:51 -0500

    Refactor 2148: simplify to one-liner

--- 2148/solution.py
+++ 2148/solution.py
@@ -1,6 +1,2 @@
-class Solution:
-    def countElements(self, nums: List[int]) -> int:
-        min_val = min(nums)
-        max_val = max(nums)
-        return sum(1 for x in nums if min_val < x < max_val)
-        
+def countElements(nums: List[int]) -> int:
+    return sum(min(nums) < x < max(nums) for x in nums)
