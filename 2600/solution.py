3f8e984a512ba9959e67915b49304258f9699929
aiden cullo <aidencullo@gmail.com>
2026-02-23 17:56:18 -0500

    2600. Solve K Items With Maximum Sum

--- 2600/solution.py
+++ 2600/solution.py
@@ -2,6 +2,7 @@ class Solution:
     def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
         if k <= numOnes:
             return k
-        if k <= numOnes + numZeros:
+        if numOnes < k <= numOnes + numZeros:
             return numOnes
-        return min(k - numOnes - numZeros, numNegOnes) * -1 + numOnes
+        if numOnes + numZeros <= k:
+            return numOnes + min(k - numOnes - numZeros, numNegOnes) * -1
