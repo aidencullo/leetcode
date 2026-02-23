632935b2d0b01ae3c071d31a2cb911d1d6aad98e
aiden cullo <aidencullo@gmail.com>
2026-02-23 10:10:57 -0500

    Add stub for 152. Maximum Product Subarray

--- 152/solution.py
+++ 152/solution.py
@@ -1,9 +1,6 @@
+from typing import List
+
+
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        product = -math.inf
-        n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                product = max(product, math.prod(nums[i: j + 1]))
-        return product
-        
+        pass
