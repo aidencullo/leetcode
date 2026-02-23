af289706bd973f9bfecd5602f227f211ff4decfd
aiden cullo <aidencullo@gmail.com>
2026-02-23 10:16:06 -0500

    152. TLE solution

--- 152/solution.py
+++ 152/solution.py
@@ -1,6 +1,12 @@
 from typing import List
-
+import math
+from itertools import islice
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        pass
+        n = len(nums)
+        max_prod = float('-inf')
+        for i in range(n):
+            for j in range(i + 1, n + 1):
+                max_prod = max(max_prod, math.prod(islice(nums, i, j)))
+        return max_prod
