ff4707b3fcf4f806b123a3e139ae08dfa5ff4e17
aiden cullo <aidencullo@gmail.com>
2026-02-23 10:33:23 -0500

    152. Wrong solution

--- 152/solution.py
+++ 152/solution.py
@@ -1,14 +1,13 @@
-from typing import List
-import math
-from itertools import islice
-
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        n = len(nums)
-        max_prod = float('-inf')
-        for i in range(n):
-            prod = 1
-            for j in range(i, n):
-                prod *= nums[j]
-                max_prod = max(max_prod, prod)
+        running_prod = 0
+        max_prod = 0
+        
+        for num in nums:
+            if running_prod <= 0:
+                running_prod = num
+            else:
+                running_prod *= num
+            max_prod = max(max_prod, running_prod)
+
         return max_prod
