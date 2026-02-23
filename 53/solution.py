f3f69184f768dc67593ffbc539e181aacfcb2710
aiden cullo <aidencullo@gmail.com>
2026-02-23 17:37:06 -0500

    fix: Clear problem 53 solution (Maximum Subarray)

--- 53/solution.py
+++ 53/solution.py
@@ -1,15 +1,3 @@
 class Solution:
     def maxSubArray(self, nums: list[int]) -> int:
-        n = len(nums)
-        l, r = 0, n - 1
-        total = sum(nums)
-        max_sub = total
-        while l <= r:
-            if nums[l] < nums[r]:
-                total -= nums[l]
-                l += 1
-            else:
-                total -= nums[r]
-                r -= 1
-            max_sub = max(max_sub, total)
-        return max_sub
+        pass
