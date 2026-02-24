acc35ca310532b618ccf400c9117d6e2f3c93ffe
aiden cullo <aidencullo@gmail.com>
2026-02-23 20:07:51 -0500

    2745. Solve Construct the Longest New String

--- 2745/solution.py
+++ 2745/solution.py
@@ -1,3 +1,8 @@
 class Solution:
     def longestString(self, x: int, y: int, z: int) -> int:
-        pass
+        if x == y:
+            return 2 * (x + y + z)
+        if x < y:
+            return 2 * (2 * x + 1 + z)
+        if x > y:
+            return 2 * (2 * y + 1 + z)
