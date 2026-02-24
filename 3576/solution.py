731a89662583a7dacbac64472f6156f7385f874d
aiden cullo <aidencullo@gmail.com>
2026-02-23 21:49:19 -0500

    Simplify 3576: extract helper function

--- 3576/solution.py
+++ 3576/solution.py
@@ -1,33 +1,22 @@
 class Solution:
     def canMakeEqual(self, nums: List[int], k: int) -> bool:
+        def makeFlips(nums, target):
+            flips = 0
+            nums_copy = nums[:]
+
+            for i in range(n - 1):
+                if nums_copy[i] != target:
+                    nums_copy[i] *= -1
+                    nums_copy[i + 1] *= -1
+                    flips += 1
+
+            if nums_copy[-1] == target:
+                return flips
+            else:
+                return math.inf
+        
         flips = math.inf
         n = len(nums)
-
-        target = 1
-        target_flips = 0
-        nums_copy = nums[:]
-
-        for i in range(n - 1):
-            if nums_copy[i] != target:
-                nums_copy[i] *= -1
-                nums_copy[i + 1] *= -1
-                target_flips += 1
-
-        if nums_copy[-1] == target:
-            flips = min(flips, target_flips)
-
-
-        target = -1
-        target_flips = 0
-        nums_copy = nums[:]
-
-        for i in range(n - 1):
-            if nums_copy[i] != target:
-                nums_copy[i] *= -1
-                nums_copy[i + 1] *= -1
-                target_flips += 1
-
-        if nums_copy[-1] == target:
-            flips = min(flips, target_flips)
-
+        flips = min(flips, makeFlips(nums, -1))
+        flips = min(flips, makeFlips(nums, 1))
         return flips <= k
