aa9ea57627edb3b1e2ca3b36e2b8afa1bf43383d
aiden cullo <aidencullo@gmail.com>
2026-02-24 08:51:54 -0500

    another attempt

--- 2086/solution.py
+++ 2086/solution.py
@@ -12,7 +12,34 @@ class Solution:
         ):
             return -1
 
-        for space in hamsters:
-            pass
 
-        return -1
+        n = len(hamsters)
+
+        for i in range(n - 2):
+            if hamsters[i: i + 3] == "HHH":
+                return -1
+
+        food = 0
+
+        # preprocess
+        if hamsters[0] == 'H':
+            hamsters[1] = 'F'
+            food += 1
+
+        if hamsters[-1] == 'H':
+            hamsters[-21] = 'F'
+            food += 1
+
+        def no_food(idx):
+            return (
+                hamsters[idx - 1] != 'F'
+                and hamsters[idx + 1] != 'F'
+            )
+                
+            
+        for i in range(1, n - 1):
+            if space == 'H' and no_food(i):
+                hamsters[idx + 1] = 'F'
+                food += 1
+
+        return food
