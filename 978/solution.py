7aa72bb33048e627048d6b1eea09cd6abc41ed32
aiden cullo <aidencullo@gmail.com>
2026-02-23 19:04:32 -0500

    978. Wrong solution

--- 978/solution.py
+++ 978/solution.py
@@ -1,6 +1,24 @@
-from typing import List
-
+def cmp(a, b):
+    return (a > b) - (a < b)
 
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
-        pass
+        max_turbulence = 0
+        running_turbulence = 0
+        prev_cmp = 0
+
+        for x, y in pairwise(arr):
+            if cmp(x, y) == -1 and prev_cmp == 1:
+                running_turbulence += 1
+            if cmp(x, y) == 1 and prev_cmp == 1:
+                running_turbulence = 1
+            if cmp(x, y) == 1 and prev_cmp == -1:
+                running_turbulence += 1
+            if cmp(x, y) == -1 and prev_cmp == -1:
+                running_turbulence = 1
+            if cmp(x, y) == 0:
+                running_turbulence = 0
+            max_turbulence = max(max_turbulence, running_turbulence)
+                
+
+        return max_turbulence
