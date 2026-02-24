02a21a7f883301020b7f6f44facdb433804b60ec
aiden cullo <aidencullo@gmail.com>
2026-02-23 21:45:55 -0500

    Solve 3576: Transform Array to All Equal Elements

--- 3576/solution.py
+++ 3576/solution.py
@@ -1,6 +1,33 @@
-from typing import List
-
-
 class Solution:
     def canMakeEqual(self, nums: List[int], k: int) -> bool:
-        pass
+        flips = math.inf
+        n = len(nums)
+
+        target = 1
+        target_flips = 0
+        nums_copy = nums[:]
+
+        for i in range(n - 1):
+            if nums_copy[i] != target:
+                nums_copy[i] *= -1
+                nums_copy[i + 1] *= -1
+                target_flips += 1
+
+        if nums_copy[-1] == target:
+            flips = min(flips, target_flips)
+
+
+        target = -1
+        target_flips = 0
+        nums_copy = nums[:]
+
+        for i in range(n - 1):
+            if nums_copy[i] != target:
+                nums_copy[i] *= -1
+                nums_copy[i + 1] *= -1
+                target_flips += 1
+
+        if nums_copy[-1] == target:
+            flips = min(flips, target_flips)
+
+        return flips <= k
